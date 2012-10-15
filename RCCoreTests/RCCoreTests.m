a0ba8f7b534c5ca47f31a12571ffd9324d4b8176
Ben Hirashima <ben@realitycap.com>
2012-10-15 13:33:10 -0700

    init

---
+++ RCCoreTests/RCCoreTests.m
@@ -0,0 +1,32 @@
+//
+//  RCCoreTests.m
+//  RCCoreTests
+//
+//  Created by Ben Hirashima on 10/10/12.
+//  Copyright (c) 2012 RealityCap. All rights reserved.
+//
+
+#import "RCCoreTests.h"
+
+@implementation RCCoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RCCoreTests");
+}
+
+@end

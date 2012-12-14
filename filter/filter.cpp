2374699850e1a2789fadd1ef085691c208b4ffe4
Eagle Jones <eagle@ambrosia.local>
2012-12-13 18:42:44 -0800

    Send packets for feature visibility and association from filter

--- filter/filter.cpp
+++ filter/filter.cpp
@@ -815,6 +815,8 @@ static int sfm_process_features(struct filter *f, uint64_t time, feature_t *feat
     if(useful_drops) {
         packet_t *sp = mapbuffer_alloc(f->output, packet_filter_reconstruction, useful_drops * 3 * sizeof(float));
         sp->header.user = useful_drops;
+        packet_filter_feature_id_association_t *association = (packet_filter_feature_id_association_t *)mapbuffer_alloc(f->output, packet_filter_feature_id_association, useful_drops * sizeof(uint64_t));
+        association->header.user = useful_drops;
         float (*world)[3] = (float (*)[3])sp->data;
         int nfeats = 0;
         for(list<state_vision_feature *>::iterator fiter = f->s.features.begin(); fiter != f->s.features.end(); ++fiter) {
@@ -823,10 +825,12 @@ static int sfm_process_features(struct filter *f, uint64_t time, feature_t *feat
                 world[nfeats][0] = i->world[0];
                 world[nfeats][1] = i->world[1];
                 world[nfeats][2] = i->world[2];
+                association->feature_id[nfeats] = i->id;
                 ++nfeats;
             }
         }
         mapbuffer_enqueue(f->output, sp, time);
+        mapbuffer_enqueue(f->output, (packet_t *)association, time);
     }
 
     int features_used = f->s.process_features(time);
@@ -1091,6 +1095,11 @@ extern "C" void sfm_vis_measurement(void *_f, packet_t *p)
         //get world
         sp = (packet_filter_current_t *)mapbuffer_alloc(f->s.mapperbuf, packet_filter_current, sizeof(packet_filter_current) - 16 + nfeats * 3 * sizeof(float));
     }
+    packet_filter_feature_id_visible_t *visible = (packet_filter_feature_id_visible_t *)mapbuffer_alloc(f->output, packet_filter_feature_id_visible, sizeof(packet_filter_feature_id_visible_t) - 16 + nfeats * sizeof(uint64_t));
+    for(int i = 0; i < 3; ++i) {
+        visible->T[i] = f->s.T.v[i];
+        visible->W[i] = f->s.W.v[i];
+    }
     int n_good_feats = 0;
     for(list<state_vision_group *>::iterator giter = f->s.groups.children.begin(); giter != f->s.groups.children.end(); ++giter) {
         state_vision_group *g = *giter;
@@ -1106,10 +1115,12 @@ extern "C" void sfm_vis_measurement(void *_f, packet_t *p)
             cp->points[n_good_feats][0] = i->world[0];
             cp->points[n_good_feats][1] = i->world[1];
             cp->points[n_good_feats][2] = i->world[2];
+            visible->feature_id[n_good_feats] = i->id;
             ++n_good_feats;
         }
     }
     cp->header.user = n_good_feats;
+    visible->header.user = n_good_feats;
     if(f->s.mapperbuf) {
         sp->header.user = n_good_feats;
         sp->reference = f->s.reference ? f->s.reference->id : f->s.last_reference;
@@ -1122,6 +1133,7 @@ extern "C" void sfm_vis_measurement(void *_f, packet_t *p)
         mapbuffer_enqueue(f->s.mapperbuf, (packet_t *)sp, time);
     }
     mapbuffer_enqueue(f->output, (packet_t*)cp, time);
+    mapbuffer_enqueue(f->output, (packet_t*)visible, time);
 }
 
 extern "C" void sfm_features_added(void *_f, packet_t *p)

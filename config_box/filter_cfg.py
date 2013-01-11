3bf7612319b0009bacbb0995b4a043aebf5b2b97
Eagle Jones <eagle@ambrosia.local>
2013-01-11 13:37:53 -0800

    Update configuration for filter

--- config_box/filter_cfg.py
+++ config_box/filter_cfg.py
@@ -9,20 +9,20 @@ import filter
 sfm = filter.filter(False)
 sfm.s.T.variance = 1.e-20
 sfm.s.W.variance = array([1., 1., 1.e-4, 0.])
-sfm.s.V.variance = 1.e-2
-sfm.s.w.variance = 1.e-1
-sfm.s.dw.variance = 1.e0
-sfm.s.a.variance = 1.e-1
-sfm.s.da.variance = 1.e0
+sfm.s.V.variance = 1.
+sfm.s.w.variance = 1.
+sfm.s.dw.variance = 10.
+sfm.s.a.variance = 10.
+sfm.s.da.variance = 10.
 sfm.s.g.variance = 1.e-3**2
-sfm.s.Wc.variance = 1.e-4**2
+sfm.s.Wc.variance = 1.e-5**2
 sfm.s.Tc.variance = 1.e-3**2
 sfm.s.a_bias.v = array([3.6e-3, -1.8e-3, -5.5e-3, 0.])
 sfm.s.a_bias.variance = 4.e-3**2
 sfm.s.w_bias.v = array([3.9e-5, 3.4e-5, 2.e-4, 0.])
 sfm.s.w_bias.variance = 1.e-3**2 #9.7e-5**2
 
-sfm.init_vis_cov = 1.e1**2
+sfm.init_vis_cov = 1.e4**2
 
 sfm.s.T.process_noise = 0.
 sfm.s.W.process_noise = 0.

761ad52afd43ef2a4691cd89561519498fe0e5cf
Ben Hirashima <ben@realitycap.com>
2013-01-15 17:40:32 -0800

    swipe to delete implemented

--- TapeMeasure/TMHistoryVC.m
+++ TapeMeasure/TMHistoryVC.m
@@ -151,6 +151,14 @@
     }
 }
 
+- (void)deleteMeasurement:(NSIndexPath*)indexPath
+{
+    NSError *error;
+    TMMeasurement *theMeasurement = [measurementsData objectAtIndex:indexPath.row];
+    [theMeasurement.managedObjectContext deleteObject:theMeasurement];
+    [theMeasurement.managedObjectContext save:&error]; //TODO: Handle save error
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -200,28 +208,30 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
+//// Override to support conditional editing of the table view.
+//- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    // Return NO if you do not want the specified item to be editable.
+//    return YES;
+//}
+
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        [tableView beginUpdates];
+        
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        
+        [self deleteMeasurement:indexPath];
+        [self loadTableData];
+        
+        [tableView endUpdates];
     }   
 }
-*/
 
 /*
 // Override to support rearranging the table view.

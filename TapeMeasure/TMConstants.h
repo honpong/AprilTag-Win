a39dcda041ea970c3e194fefe9ad4571714d7f95
Ben Hirashima <ben@realitycap.com>
2013-02-08 15:59:05 -0800

    new macros

--- TapeMeasure/TMConstants.h
+++ TapeMeasure/TMConstants.h
@@ -22,11 +22,17 @@
 #define VIDEOCAP_MANAGER [RCVideoCapManagerFactory getVideoCapManagerInstance]
 #define MOTIONCAP_MANAGER [RCMotionCapManagerFactory getMotionCapManagerInstance]
 #define LOCATION_MANAGER [RCLocationManagerFactory getLocationManagerInstance]
+#define USER_MANAGER [RCUserManagerFactory getInstance]
+#define HTTP_CLIENT [RCHttpClientFactory getInstance]
+#define MEASUREMENT_SYNCR [TMMeasurementSyncrFactory getInstance]
 
 #define DOCUMENTS_DIRECTORY [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject]
 
 #define PREF_UNITS @"units_preference"
 #define PREF_ADD_LOCATION @"addlocation_preference"
+
+#define API_BASE_URL @"http://internal.realitycap.com/"
+#define API_HEADER_ACCEPT @"application/vnd.realitycap.json; version=1.0"
 
 typedef enum {
     TypePointToPoint = 0, TypeTotalPath = 1, TypeHorizontal = 2, TypeVertical = 3

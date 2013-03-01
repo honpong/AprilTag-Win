c01d4638dc4615ce3d3988409ee9b81086acc57f
Ben Hirashima <ben@realitycap.com>
2013-03-01 12:18:55 -0800

    new tests

---
+++ TapeMeasureTests/TMLocationExtTests.h
@@ -0,0 +1,13 @@
+//
+//  TMLocationExtTests.h
+//  TapeMeasure
+//
+//  Created by Ben Hirashima on 3/1/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TMLocationExtTests : SenTestCase
+
+@end
---
+++ TapeMeasureTests/TMMeasurementExtTests.h
@@ -0,0 +1,13 @@
+//
+//  TMMeasurementExtTests.h
+//  TapeMeasure
+//
+//  Created by Ben Hirashima on 3/1/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TMMeasurementExtTests : SenTestCase
+
+@end

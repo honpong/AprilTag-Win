4c8c2000a967d0d316e943af23bd065a01ce1bd3
Ben Hirashima <ben@realitycap.com>
2013-03-08 12:55:49 -0800

    made some more methods public so they can be tested

--- TapeMeasure/TMSyncable+TMSyncableSync.h
+++ TapeMeasure/TMSyncable+TMSyncableSync.h
@@ -20,13 +20,18 @@
 + (void)syncWithServer:(int)sinceTransId onSuccess:(void (^)(int lastTransId))successBlock onFailure:(void (^)(int statusCode))failureBlock;
 + (BOOL)isSyncInProgress;
 
++ (void)downloadChanges:(int)sinceTransId withPage:(int)pageNum onSuccess:(void (^)(int lastTransId))successBlock onFailure:(void (^)(int))failureBlock;;
++ (void)uploadChanges:(void (^)())successBlock onFailure:(void (^)(int))failureBlock;
+
 - (void)postToServer:(void (^)(int transId))successBlock onFailure:(void (^)(int statusCode))failureBlock;
 - (void)postToServer:(NSDictionary*)params onSuccess:(void (^)(int transId))successBlock onFailure:(void (^)(int statusCode))failureBlock;
 
 - (void)putToServer:(void (^)(int transId))successBlock onFailure:(void (^)(int statusCode))failureBlock;
 - (void)putToServer:(NSDictionary*)params onSuccess:(void (^)(int transId))successBlock onFailure:(void (^)(int statusCode))failureBlock;
 
++ (int)saveJson:(id)jsonArray;
 + (int)getStoredTransactionId;
 + (void)saveLastTransIdIfHigher:(int)transId;
++ (int)getNextPageNumber:(id)json;
 
 @end

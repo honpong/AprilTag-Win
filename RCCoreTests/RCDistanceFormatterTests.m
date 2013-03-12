c61d14a540658032ec98f9b2d3de8cd111cc829d
Ben Hirashima <ben@realitycap.com>
2013-03-12 16:14:27 -0700

    more tests for RCDistanceFormatter

--- RCCoreTests/RCDistanceFormatterTests.m
+++ RCCoreTests/RCDistanceFormatterTests.m
@@ -11,7 +11,7 @@
 
 @implementation RCDistanceFormatterTests
 
-- (void) testDistanceFormatter
+- (void) testInchFractions
 {
     NSString *result;
     NSString *expected;
@@ -163,7 +163,102 @@
     expected = @"1mi";
     result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleMI withFractional:YES];
     STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+}
+
+- (void) testInchDecimals
+{
+    NSString *result;
+    NSString *expected;
+    float testValue;
     
+    testValue = 11 * METERS_PER_INCH;
+    expected = @"11.0\"";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleIN withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+    
+    testValue = 12 * METERS_PER_INCH;
+    expected = @"12.0\"";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleIN withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+    
+    //test that we still get inches when inches > 12 and units scale = UnitsScaleIN
+    testValue = 13 * METERS_PER_INCH;
+    expected = @"13.0\"";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleIN withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+}
+
+- (void) testFootDecimals
+{
+    NSString *result;
+    NSString *expected;
+    float testValue;
+    
+    //test that we still get feet when feet < 1
+    testValue = 0.5 * METERS_PER_FOOT;
+    expected = @"0.50\'";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleFT withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+        
+    testValue = 1 * METERS_PER_FOOT;
+    expected = @"1.00\'";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleFT withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+    
+    //test that we still get feet when feet > 1 yard
+    testValue = 4 * METERS_PER_FOOT;
+    expected = @"4.00\'";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleFT withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+}
+
+- (void) testYardDecimals
+{
+    NSString *result;
+    NSString *expected;
+    float testValue;
+    
+    //test that we still get yards when yards < 1
+    testValue = 0.5 * METERS_PER_YARD;
+    expected = @"0.500yd";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleYD withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+    
+    testValue = 1 * METERS_PER_YARD;
+    expected = @"1.000yd";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleYD withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+    
+    //test that we still get yards when yards > 1 mile
+    testValue = 1761 * METERS_PER_YARD;
+    expected = @"1,761.000yd";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleYD withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+}
+
+- (void) testMileDecimals
+{
+    NSString *result;
+    NSString *expected;
+    float testValue;
+    
+    //test that we still get miles when miles < 1
+    testValue = 0.5 * INCHES_PER_MILE * METERS_PER_INCH;
+    expected = @"0.50000mi";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleMI withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+    
+    testValue = 1 * INCHES_PER_MILE * METERS_PER_INCH;
+    expected = @"1.00000mi";
+    result = [RCDistanceFormatter getFormattedDistance:testValue withUnits:UnitsImperial withScale:UnitsScaleMI withFractional:NO];
+    STAssertTrue([result isEqualToString:expected], [NSString stringWithFormat:@"%f should be [%@], got [%@]", testValue, expected, result]);
+}
+
+- (void) testKnownExamples
+{
+    NSString *result;
+    NSString *expected;
+    float testValue;
     
     //test some known examples
     testValue = 0.330;

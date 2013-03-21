0c22758ed8a46ff0dc52b4238d44acbbb8682e41
Ben Hirashima <ben@realitycap.com>
2013-03-21 10:11:20 -0700

    reworking new measurement flow. wip.

---
+++ TapeMeasureTests/TMNewMeasurementVCTests.h
@@ -0,0 +1,13 @@
+//
+//  TMNewMeasurementVCTests.h
+//  TapeMeasure
+//
+//  Created by Ben Hirashima on 3/20/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TMNewMeasurementVCTests : SenTestCase
+
+@end
---
+++ TapeMeasureTests/TMNewMeasurementVCTests.m
@@ -0,0 +1,13 @@
+//
+//  TMNewMeasurementVCTests.m
+//  TapeMeasure
+//
+//  Created by Ben Hirashima on 3/20/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import "TMNewMeasurementVCTests.h"
+
+@implementation TMNewMeasurementVCTests
+
+@end

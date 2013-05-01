b5730890d079985019c2675389557fba52f064ca
Ben Hirashima <ben@realitycap.com>
2013-05-01 13:29:31 -0700

    added some WIP tests

---
+++ TapeMeasureTests/TMNewMeasurementTests.h
@@ -0,0 +1,17 @@
+//
+//  TMNewMeasurementTests.h
+//  TapeMeasure
+//
+//  Created by Ben Hirashima on 5/1/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "TMNewMeasurementVC.h"
+
+@interface TMNewMeasurementTests : SenTestCase
+{
+    TMNewMeasurementVC *vc;
+    UINavigationController *nav;
+}
+@end

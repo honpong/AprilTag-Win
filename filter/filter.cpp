196f819f2405ae7068bd219f38781f27f8f0df7f
Eagle Jones <eagle@ambrosia.local>
2013-05-02 07:02:49 -0700

    Turn down initial cov on Cx, Cy

--- filter/filter.cpp
+++ filter/filter.cpp
@@ -1816,8 +1816,8 @@ void filter_config(struct filter *f)
     f->s.w_bias.v = v4(f->device.w_bias[0], f->device.w_bias[1], f->device.w_bias[2], 0.);
     f->s.w_bias.variance = v4(f->device.w_bias_var[0], f->device.w_bias_var[1], f->device.w_bias_var[2], 0.);
     f->s.focal_length.variance = 10.;
-    f->s.center_x.variance = 4.;
-    f->s.center_y.variance = 4.;
+    f->s.center_x.variance = 1.;
+    f->s.center_y.variance = 1.;
     f->s.k1.variance = .1;
     f->s.k2.variance = .1;
     f->s.k3.variance = .1;

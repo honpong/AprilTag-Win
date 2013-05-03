9136d2a07082d503eeff1ebf1366cefaa0c8fbe6
Ben Hirashima <ben@realitycap.com>
2013-05-03 12:06:18 -0700

    new measurement tests WIP

--- TapeMeasureTests/TMNewMeasurementVCSyncTests.m
+++ TapeMeasureTests/TMNewMeasurementVCSyncTests.m
@@ -75,50 +75,43 @@
     id motionMan = [RCMotionCapManagerFactory getMotionCapManagerInstance];
     id corvisMan = [RCCorvisManagerFactory getCorvisManagerInstance];
     
+    [(id<RCCorvisManager>)[corvisMan expect] startPlugins];
     [(id<RCVideoCapManager>)[videoMan expect]  startVideoCap];
     [(id<RCMotionCapManager>)[motionMan expect]  startMotionCap];
     
-    [(id<RCCorvisManager>)[corvisMan expect] startPlugins];
-    
     [vc loadView];
-    [vc handleResume];
-    [vc startMeasuring];
+    [vc handleStateEvent:EV_RESUME];
     
+    [corvisMan verify];
     [videoMan verify];
     [motionMan verify];
-    [corvisMan verify];
     
-    STAssertTrue(vc.isCapturingData, nil);
-    STAssertFalse(vc.isProcessingData, nil);
-    STAssertFalse(vc.isMeasurementComplete, nil);
-    STAssertFalse(vc.isMeasurementCanceled, nil);
+    [(id<RCCorvisManager>)[corvisMan expect] startMeasurement];
+    [vc handleStateEvent:EV_TAP];
+    
+    [corvisMan verify];
 }
 
-- (void) resumeAfterPausedAndCanceledMeasurement
-{
-//    id corvisMan = [RCCorvisManagerFactory getCorvisManagerInstance];
-    
-//    CLLocation *loc = [LOCATION_MANAGER getStoredLocation];
-//    [(id<RCCorvisManager>)[corvisMan expect]
-//     setupPluginsWithFilter:false
-//     withCapture:true
-//     withReplay:false
-//     withLocationValid:loc ? true : false
-//     withLatitude:loc ? loc.coordinate.latitude : 0
-//     withLongitude:loc ? loc.coordinate.longitude : 0
-//     withAltitude:loc ? loc.altitude : 0
-//     withUpdateProgress:NULL
-//     withUpdateMeasurement:NULL
-//     withCallbackObject:NULL];
-    
-    [vc handleResume];
-//    [corvisMan verify];
-    
-    STAssertFalse(vc.isMeasurementCanceled, nil);
-    STAssertFalse(vc.isCapturingData, nil);
-    STAssertFalse(vc.isProcessingData, nil);
-    STAssertFalse(vc.isMeasurementComplete, nil);
-}
+//- (void) resumeAfterPausedAndCanceledMeasurement
+//{
+////    id corvisMan = [RCCorvisManagerFactory getCorvisManagerInstance];
+//    
+////    CLLocation *loc = [LOCATION_MANAGER getStoredLocation];
+////    [(id<RCCorvisManager>)[corvisMan expect]
+////     setupPluginsWithFilter:false
+////     withCapture:true
+////     withReplay:false
+////     withLocationValid:loc ? true : false
+////     withLatitude:loc ? loc.coordinate.latitude : 0
+////     withLongitude:loc ? loc.coordinate.longitude : 0
+////     withAltitude:loc ? loc.altitude : 0
+////     withUpdateProgress:NULL
+////     withUpdateMeasurement:NULL
+////     withCallbackObject:NULL];
+//    
+//    [vc handleResume];
+////    [corvisMan verify];
+//}
 
 - (void) testNormalMeasurement
 {
@@ -128,82 +121,65 @@
     id motionMan = [RCMotionCapManagerFactory getMotionCapManagerInstance];
     id corvisMan = [RCCorvisManagerFactory getCorvisManagerInstance];
     
-    [(id<RCVideoCapManager>)[videoMan expect]  stopVideoCap];
-    [(id<RCMotionCapManager>)[motionMan expect]  stopMotionCap];
-    [(id<RCCorvisManager>)[corvisMan expect] stopPlugins];
-    
-    [vc stopMeasuring];
-    
-    NSLog(@"started poll");
-    int pollCount = 0;
-    
-    while (vc.isMeasurementComplete == NO && pollCount < MAX_POLL_COUNT) {
-        NSLog(@"polling... %i", pollCount);
-        NSDate* untilDate = [NSDate dateWithTimeIntervalSinceNow:POLL_INTERVAL];
-        [[NSRunLoop currentRunLoop] runUntilDate:untilDate];
-        pollCount++;
-    }
-    if (pollCount == MAX_POLL_COUNT) STFail(@"polling timed out");
-    
-    [videoMan verify];
-    [motionMan verify];
-    [corvisMan verify];
-    
-    STAssertFalse(vc.isCapturingData, nil);
-    STAssertTrue(vc.isMeasurementComplete, nil);
-    STAssertFalse(vc.isMeasurementCanceled, nil);
-    
-    //test that we keep the finished measurement after pause/resume
-    [(id<RCCorvisManager>)[corvisMan expect] teardownPlugins];
-    [vc handlePause];
-    [corvisMan verify];
-    
-    [vc handleResume];
-    
-    STAssertFalse(vc.isCapturingData, nil);
-    STAssertTrue(vc.isMeasurementComplete, nil);
-    STAssertFalse(vc.isMeasurementCanceled, nil);
-}
-
-- (void) testPauseWhileCapturingThenResume
-{
-    [self measurementStart];
-    
-    id videoMan = [RCVideoCapManagerFactory getVideoCapManagerInstance];
-    id motionMan = [RCMotionCapManagerFactory getMotionCapManagerInstance];
-    id corvisMan = [RCCorvisManagerFactory getCorvisManagerInstance];
-    
+    [(id<RCCorvisManager>)[corvisMan expect] stopMeasurement];
+        
     [(id<RCVideoCapManager>)[videoMan expect]  stopVideoCap];
     [(id<RCMotionCapManager>)[motionMan expect]  stopMotionCap];
     [(id<RCCorvisManager>)[corvisMan expect] stopPlugins];
     [(id<RCCorvisManager>)[corvisMan expect] teardownPlugins];
     
-    [vc handlePause];
-    
-    NSLog(@"started poll");
-    int pollCount = 0;
-    
-    while (vc.isCapturingData == YES && pollCount < MAX_POLL_COUNT) {
-        NSLog(@"polling... %i", pollCount);
-        NSDate* untilDate = [NSDate dateWithTimeIntervalSinceNow:POLL_INTERVAL];
-        [[NSRunLoop currentRunLoop] runUntilDate:untilDate];
-        pollCount++;
-    }
-    if (pollCount == MAX_POLL_COUNT) STFail(@"polling timed out");
-    
-    STAssertFalse(vc.isCapturingData, nil);
+    [vc handleStateEvent:EV_TAP];
+        
+    //wait a bit
+    NSDate *timeoutDate = [NSDate dateWithTimeIntervalSinceNow:2];
+    do {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:timeoutDate];
+        if([timeoutDate timeIntervalSinceNow] < 0.0)
+            break;
+    } while (true);
     
     [videoMan verify];
     [motionMan verify];
     [corvisMan verify];
-    
-    STAssertTrue(vc.isMeasurementCanceled, nil);
-    STAssertFalse(vc.isCapturingData, nil);
-    
-    [self resumeAfterPausedAndCanceledMeasurement];
 }
 
-
+//- (void) testPauseWhileCapturingThenResume
+//{
+//    [self measurementStart];
+//    
+//    id videoMan = [RCVideoCapManagerFactory getVideoCapManagerInstance];
+//    id motionMan = [RCMotionCapManagerFactory getMotionCapManagerInstance];
+//    id corvisMan = [RCCorvisManagerFactory getCorvisManagerInstance];
+//    
+//    [(id<RCVideoCapManager>)[videoMan expect]  stopVideoCap];
+//    [(id<RCMotionCapManager>)[motionMan expect]  stopMotionCap];
+//    [(id<RCCorvisManager>)[corvisMan expect] stopPlugins];
+//    [(id<RCCorvisManager>)[corvisMan expect] teardownPlugins];
+//    
+//    [vc handlePause];
+//    
+//    NSLog(@"started poll");
+//    int pollCount = 0;
+//    
+//    while (vc.isCapturingData == YES && pollCount < MAX_POLL_COUNT) {
+//        NSLog(@"polling... %i", pollCount);
+//        NSDate* untilDate = [NSDate dateWithTimeIntervalSinceNow:POLL_INTERVAL];
+//        [[NSRunLoop currentRunLoop] runUntilDate:untilDate];
+//        pollCount++;
+//    }
+//    if (pollCount == MAX_POLL_COUNT) STFail(@"polling timed out");
+//    
+//    STAssertFalse(vc.isCapturingData, nil);
+//    
+//    [videoMan verify];
+//    [motionMan verify];
+//    [corvisMan verify];
+//    
+//    STAssertTrue(vc.isMeasurementCanceled, nil);
+//    STAssertFalse(vc.isCapturingData, nil);
+//    
+//    [self resumeAfterPausedAndCanceledMeasurement];
+//}
 
 - (void) testPauseCallsTeardownPlugins
 {

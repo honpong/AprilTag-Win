0714c1e4fb1d441ac62b0292df9b48466df956eb
Ben Hirashima <ben@realitycap.com>
2013-05-14 15:44:40 -0700

    moved Units and UnitsScale enums to RCConstants.h

--- RCCore/RCDistanceFormatter.h
+++ RCCore/RCDistanceFormatter.h
@@ -2,6 +2,8 @@
 //  Copyright (c) 2012 RealityCap. All rights reserved.
 //
 
+#import "RCConstants.h"
+
 #define METERS_PER_INCH 0.0254
 #define METERS_PER_FOOT 0.3048
 #define METERS_PER_YARD 0.9144
@@ -11,16 +13,6 @@
 #define INCHES_PER_YARD 36
 #define INCHES_PER_MILE 63360
 #define SIXTEENTH_INCH 0.03125
-
-typedef enum {
-    UnitsMetric = 0, UnitsImperial = 1
-} Units;
-
-//items are numbered by their order in the segmented button list. that's why there's two 0s, etc. not ideal, but convenient and workable.
-typedef enum {
-    UnitsScaleKM = 0, UnitsScaleM = 1, UnitsScaleCM = 2,
-    UnitsScaleMI = 0, UnitsScaleYD = 1, UnitsScaleFT = 2, UnitsScaleIN = 3
-} UnitsScale;
 
 @interface RCDistanceFormatter : NSObject
 

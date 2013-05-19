6647d63242d4eb3d8e608d790de7d85f35d5b538
Eagle Jones <eagle@ambrosia.local>
2013-05-18 18:24:32 -0700

    Update calibration and tuning for iphone 5

--- RCCalibration.mm
+++ RCCalibration.mm
@@ -234,9 +234,9 @@
     dc.w_bias[0] = 0.;
     dc.w_bias[1] = 0.;
     dc.w_bias[2] = 0.;
-    dc.Tc[0] = 0.;
-    dc.Tc[1] = 0.015;
-    dc.Tc[2] = 0.;
+    dc.Tc[0] = 0.000;
+    dc.Tc[1] = 0.000;
+    dc.Tc[2] = -0.008;
     dc.Wc[0] = sqrt(2.)/2. * M_PI;
     dc.Wc[1] = -sqrt(2.)/2. * M_PI;
     dc.Wc[2] = 0.;
@@ -244,9 +244,9 @@
     for(int i = 0; i < 3; ++i) dc.a_bias_var[i] = 1.e-4; //a_bias_stdev * a_bias_stdev;
     double w_bias_stdev = 10. / 180. * M_PI; //10 dps
     for(int i = 0; i < 3; ++i) dc.w_bias_var[i] = 1.e-4; //w_bias_stdev * w_bias_stdev;
-    dc.Tc_var[0] = 1.e-6;
-    dc.Tc_var[1] = 1.e-6;
-    dc.Tc_var[2] = 1.e-6;
+    dc.Tc_var[0] = 1.e-7;
+    dc.Tc_var[1] = 1.e-7;
+    dc.Tc_var[2] = 1.e-7;
     dc.Wc_var[0] = 1.e-7;
     dc.Wc_var[1] = 1.e-7;
     dc.Wc_var[2] = 1.e-7;

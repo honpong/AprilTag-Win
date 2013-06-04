ef1271cc8da0585ef97c904ae80649a5b1328160
Ben Hirashima <ben@realitycap.com>
2013-06-04 10:44:29 -0700

    Initial Commit

---
+++ IndoorNavigation/INAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  INAppDelegate.h
+//  IndoorNavigation
+//
+//  Created by Ben Hirashima on 6/4/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface INAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IndoorNavigation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IndoorNavigation
+//
+//  Created by Ben Hirashima on 6/4/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "INAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([INAppDelegate class]));
+    }
+}

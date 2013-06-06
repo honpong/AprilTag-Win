120dbddacf1f95f8430f16dfe3deb0452705d125
Brian Fulkerson <brian@realitycap.com>
2013-06-06 13:42:49 +0100

    Kill a warning about initilizing out of order in filter.h

--- filter/filter.h
+++ filter/filter.h
@@ -17,7 +17,7 @@ struct tracker {
 };
 
 struct filter {
-filter(bool estimate_calibration): min_feats_per_group(0), output(0), control(0), visbuf(0), recognition_buffer(0), last_time(0), last_packet_time(0), s(estimate_calibration), gravity_init(0), frame(0), active(0), got_accelerometer(0), got_gyroscope(0), accelerometer_max(0.), gyroscope_max(0.), got_image(0), need_reference(true), latitude(37.7750), longitude(-122.4183), altitude(0.), location_valid(false), measurement_running(false), detector_failed(false), tracker_failed(false), tracker_warned(false), speed_failed(false), speed_warning(false), speed_warning_time(0), numeric_failed(false), ignore_lateness(false) {}
+filter(bool estimate_calibration): min_feats_per_group(0), output(0), control(0), visbuf(0), last_time(0), last_packet_time(0), s(estimate_calibration), gravity_init(0), frame(0), active(0), got_accelerometer(0), got_gyroscope(0), got_image(0), need_reference(true), accelerometer_max(0.), gyroscope_max(0.), latitude(37.7750), longitude(-122.4183), altitude(0.), location_valid(false), recognition_buffer(0), measurement_running(false), detector_failed(false), tracker_failed(false), tracker_warned(false), speed_failed(false), speed_warning(false), numeric_failed(false), speed_warning_time(0), ignore_lateness(false) {}
 
     int min_feats_per_group;
     int min_group_add;

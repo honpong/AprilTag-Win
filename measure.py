bd614cf3b93754c531e24262fde8c5e15fd23926
Brian Fulkerson <brian@realitycap.com>
2013-06-12 15:17:57 +0100

    Clean up measure.py and make it into a callable function

--- measure.py
+++ measure.py
@@ -4,69 +4,85 @@
 #
 # This file is a part of the corvis framework, and is made available
 # under the BSD license; please see LICENSE file for full text
-
+import sys
+import time
 from numpy import *
 
-import sys, os
-import getopt
-import ctypes
+def measure(filename, configuration_name):
+    sys.path.extend(['cor/'])
+    sys.path.extend(['numerics/'])
+    sys.path.extend(["filter/", "filter/.libs"])
+    import cor
+    import numerics
+    import filter
 
-KB = 1024
-MB = 1024*KB
+    cor.cvar.cor_time_pb_real = False
 
-sys.path.extend(['cor/'])
-import cor
-sys.path.extend(['numerics/'])
-import numerics
+    capture = cor.mapbuffer()
+    capture.filename = filename
+    capture.replay = True
+    capture.size = 16 * 1024 * 1024
+    capture.dispatch = cor.dispatch_t()
+    capture.dispatch.reorder_depth = 20
+    capture.dispatch.max_latency = 25000
+    capture.dispatch.mb = capture;
+    cor.dispatch_init(capture.dispatch);
+    cor.plugins_register(cor.mapbuffer_open(capture))
 
-cor.cvar.cor_time_pb_real = False
+    dc = filter.corvis_device_parameters()
 
-replay_file = sys.argv[1]
+    from device_parameters import set_device_parameters
+    set_device_parameters(dc, configuration_name) 
 
-capture = cor.mapbuffer()
-capture.filename = replay_file
-capture.replay = True
-capture.size = 16 * 1024 * 1024
-capture.dispatch = cor.dispatch_t()
-capture.dispatch.reorder_depth = 20
-capture.dispatch.max_latency = 25000
-capture.dispatch.mb = capture;
-cor.dispatch_init(capture.dispatch);
-cor.plugins_register(cor.mapbuffer_open(capture))
+    outname = filename + "_solution"
+    fc = filter.filter_setup(capture.dispatch, outname, dc)
+    fc.sfm.ignore_lateness = True
 
-outname = replay_file + "_solution"
+    cor.cor_time_init()
+    cor.plugins_start()
 
-sys.path.extend(["filter/", "filter/.libs"])
-import filter
+    class monitor():
+        def __init__(self, capture):
+            self.capture = capture
+            self.bytes_dispatched = 0
+            self.done = False
+            self.percent = 0
 
-dc = filter.corvis_device_parameters()
+        def finished(self, packet):
+            if self.done: return
 
-from device_parameters import set_device_parameters
-set_device_parameters(dc, 'ipad2') 
+            self.total_bytes = self.capture.dispatch.mb.total_bytes
+            self.bytes_dispatched = self.capture.dispatch.bytes_dispatched
+            self.percent = 100. * self.bytes_dispatched / self.total_bytes
+            if self.bytes_dispatched == self.total_bytes:
+              self.done = True
 
-fc = filter.filter_setup(capture.dispatch, outname, dc)
-fc.sfm.ignore_lateness = True
+    mc = monitor(capture)
+    cor.dispatch_addpython(fc.solution.dispatch, mc.finished)
 
+    last_bytes = 0
+    last_time = 0
+    while not mc.done: 
+        if last_bytes == mc.bytes_dispatched and mc.percent > 90:
+            if last_time == 0: 
+                last_time = time.time()
+            if time.time() - last_time > 2:
+                print "No packets received recently, stopping"
+                break
+        else:
+            last_time = 0
+            
+        last_bytes = mc.bytes_dispatched
+        time.sleep(0.1)
 
-cor.cor_time_init()
-cor.plugins_start()
+    return fc.sfm.s
 
-class monitor():
-    done = False
-    def __init__(self, capture):
-        self.capture = capture
-
-    def finished(self, packet):
-        if self.done: return
-        if self.capture.dispatch.mb.total_bytes == self.capture.dispatch.bytes_dispatched:
-          self.done = True
-
-mc = monitor(capture)
-cor.dispatch_addpython(fc.solution.dispatch, mc.finished)
-
-import time
-while not mc.done:
-    time.sleep(0.1)
-
-print "Total path length (m):", fc.sfm.s.total_distance
-print "Straight line length(m):", fc.sfm.s.T.v
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print "Usage:", sys.argv[0], "<data_filename> <configuration_name>"
+        sys.exit(1)
+    state = measure(filename=sys.argv[1], configuration_name=sys.argv[2])
+    print "Filename:", sys.argv[1]
+    print "Configuration name:", sys.argv[2]
+    print "Total path length (m):", state.total_distance
+    print "Straight line length (m):", sqrt(sum(state.T.v**2))

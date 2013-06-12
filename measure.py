5131dfb49f6eb0789d8094e72331fd2fa1837809
Brian Fulkerson <brian@realitycap.com>
2013-06-12 11:01:55 +0100

    Actually, we can do replaying completely from python
    Remove the capability for running a file instead of going to the shell
    from shell/cor, and merge benchmark.py and measure.py into one file
    which can be run from the port installed version of python

--- measure.py
+++ measure.py
@@ -1,4 +1,55 @@
-# capture, cor, fc are defined in benchmark.py
+# Created by Brian Fulkerson
+# Copyright (c) 2013. RealityCap, Inc.
+# All rights reserved.
+#
+# This file is a part of the corvis framework, and is made available
+# under the BSD license; please see LICENSE file for full text
+
+from numpy import *
+
+import sys, os
+import getopt
+import ctypes
+
+KB = 1024
+MB = 1024*KB
+
+sys.path.extend(['cor/'])
+import cor
+sys.path.extend(['numerics/'])
+import numerics
+
+cor.cvar.cor_time_pb_real = False
+
+replay_file = sys.argv[1]
+
+capture = cor.mapbuffer()
+capture.filename = replay_file
+capture.replay = True
+capture.size = 16 * 1024 * 1024
+capture.dispatch = cor.dispatch_t()
+capture.dispatch.reorder_depth = 20
+capture.dispatch.max_latency = 25000
+capture.dispatch.mb = capture;
+cor.dispatch_init(capture.dispatch);
+cor.plugins_register(cor.mapbuffer_open(capture))
+
+outname = replay_file + "_solution"
+
+sys.path.extend(["filter/", "filter/.libs"])
+import filter
+
+dc = filter.corvis_device_parameters()
+
+from device_parameters import set_device_parameters
+set_device_parameters(dc, 'ipad2') 
+
+fc = filter.filter_setup(capture.dispatch, outname, dc)
+fc.sfm.ignore_lateness = True
+
+
+cor.cor_time_init()
+cor.plugins_start()
 
 class monitor():
     done = False
@@ -19,4 +70,3 @@ while not mc.done:
 
 print "Total path length (m):", fc.sfm.s.total_distance
 print "Straight line length(m):", fc.sfm.s.T.v
-

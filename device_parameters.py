d4d37e75cc304ebd8f731dcbcfc5d9094591c3a3
Brian Fulkerson <brian@realitycap.com>
2013-06-13 14:58:32 +0100

    Add a configuration for my ipad 2 starting with the default ipad 2 one

--- device_parameters.py
+++ device_parameters.py
@@ -103,6 +103,40 @@ def set_device_parameters(dc, config_name):
         dc.shutter_delay = 0;
         dc.shutter_period = 31000;
 
+    elif config_name == 'ipad2_brian':
+        dc.Fx = 790.;
+        dc.Fy = 790.;
+        dc.Cx = 319.5;
+        dc.Cy = 239.5;
+        dc.px = 0.;
+        dc.py = 0.;
+        dc.K[0] = -1.2546e-1;
+        dc.K[1] = 5.9923e-1;
+        dc.K[2] = -.9888;
+        dc.Tc[0] = -.015;
+        dc.Tc[1] = .100;
+        dc.Tc[2] = 0.;
+        dc.Wc[0] = sqrt(2.)/2. * pi;
+        dc.Wc[1] = -sqrt(2.)/2. * pi;
+        dc.Wc[2] = 0.;
+        a_bias_stdev = .02 * 9.8; # 20 mg
+        w_bias_stdev = 10. / 180. * pi; # 10 dps
+        for i in range(3):
+            dc.a_bias[i] = 0.;
+            dc.w_bias[i] = 0.;
+            dc.a_bias_var[i] = 1.e-4; #a_bias_stdev * a_bias_stdev;
+            dc.w_bias_var[i] = 1.e-4; #w_bias_stdev * w_bias_stdev;
+            dc.Tc_var[i] = 1.e-6;
+            dc.Wc_var[i] = 1.e-7;
+        w_stdev = .03 * sqrt(50.) / 180. * pi; # .03 dps / sqrt(hz) at 50 hz
+        dc.w_meas_var = w_stdev * w_stdev;
+        a_stdev = .000218 * sqrt(50.) * 9.8; # 218 ug / sqrt(hz) at 50 hz
+        dc.a_meas_var = a_stdev * a_stdev;
+        dc.image_width = 640;
+        dc.image_height = 480;
+        dc.shutter_delay = 0;
+        dc.shutter_period = 31000;
+
     elif config_name == 'ipad3':
         dc.Fx = 620.;
         dc.Fy = 620.;

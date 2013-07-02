23ae80cd870071fa1d8a6ddaa2139fa7f7d060ef
Ben Hirashima <ben@realitycap.com>
2013-07-02 16:07:05 -0700

    docs WIP. lots of documentation TODOs added.

--- RCCore/DataClasses/RCRotation.h
+++ RCCore/DataClasses/RCRotation.h
@@ -9,12 +9,35 @@
 #include "RCPoint.h"
 #include "RCTranslation.h"
 
+/** Represents the rotation of the device. */
 @interface RCRotation : RCVector
 
+/** TODO: document
+ @param matrix A matrix
+ */
 - (void) getOpenGLMatrix:(float[16])matrix;
+
+/** TODO: document
+ @param point An RCPoint object
+ @returns An RCPoint object
+ */
 - (RCPoint *) transformPoint:(RCPoint *)point;
+
+/** TODO: document
+ @param translation An RCTranslation object
+ @returns An RCTranslation object
+ */
 - (RCTranslation *) transformTranslation:(RCTranslation *)translation;
+
+/** TODO: document
+ @returns An RCRotation object
+ */
 - (RCRotation *) getInverse;
+
+/** TODO: document
+ @param other An RCRotation object
+ @returns An RCRotation object
+ */
 - (RCRotation *) composeWithRotation:(RCRotation *)other;
 
 @end
--- RCCore/DataClasses/RCTranslation.h
+++ RCCore/DataClasses/RCTranslation.h
@@ -9,11 +9,29 @@
 #import "RCPoint.h"
 #import "RCScalar.h"
 
+/** Represents the translation of the device relative to the starting point. */
 @interface RCTranslation : RCVector
 
+/** TODO: document
+ @param point An RCPoint object
+ @returns An RCPoint object
+ */
 - (RCPoint *)transformPoint:(RCPoint *)point;
+
+/** TODO: document
+ @returns An RCScalar object
+ */
 - (RCScalar *)getDistance;
+
+/** TODO: document
+ @returns An RCTranslation object
+ */
 - (RCTranslation *)getInverse;
+
+/** TODO: document
+ @param other An RCTranslation object
+ @returns An RCTranslation object
+ */
 - (RCTranslation *)composeWithTranslation:(RCTranslation *)other;
 
 @end

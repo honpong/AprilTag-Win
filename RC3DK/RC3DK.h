5f60ee2a12e781bff82be38dc1ce13b3671c224c
Ben Hirashima <ben@realitycap.com>
2013-07-16 16:52:34 -0700

    Initial Commit

---
+++ RC3DK/RC3DK.h
@@ -0,0 +1,13 @@
+//
+//  RC3DK.h
+//  RC3DK
+//
+//  Created by Ben Hirashima on 7/16/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RC3DK : NSObject
+
+@end
---
+++ RC3DKTests/RC3DKTests.h
@@ -0,0 +1,13 @@
+//
+//  RC3DKTests.h
+//  RC3DKTests
+//
+//  Created by Ben Hirashima on 7/16/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RC3DKTests : SenTestCase
+
+@end

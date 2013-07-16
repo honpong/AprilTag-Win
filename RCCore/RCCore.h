9b929f8cab42ea999d624112a0b8e08b0193e14a
Ben Hirashima <ben@realitycap.com>
2013-07-16 12:05:12 -0700

    using static RCCore lib. updated google maps sdk to 1.3.1. api refactor still WIP.

---
+++ RCCore/RCCore.h
@@ -0,0 +1,42 @@
+//
+//  RCCore.h
+//  RCCore
+//
+//  Created by Ben Hirashima on 7/16/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+
+#ifndef RCCore_RCCore_h
+#define RCCore_RCCore_h
+
+#import "RCSensorFusion.h"
+#import "RCAVSessionManager.h"
+#import "RCLocationManager.h"
+#import "RCMotionManager.h"
+#import "RCVideoManager.h"
+
+#import "RCFeaturePoint.h"
+#import "RCRotation.h"
+#import "RCTranslation.h"
+#import "RCSensorFusionData.h"
+#import "RCSensorFusionStatus.h"
+#import "RCTransformation.h"
+#import "RCScalar.h"
+#import "RCVector.h"
+#import "RCPoint.h"
+#import "RCCameraParameters.h"
+
+#import "RCDistance.h"
+#import "RCDistanceMetric.h"
+#import "RCDistanceImperial.h"
+#import "RCDistanceImperialFractional.h"
+#import "RCFraction.h"
+
+#import "RCDistanceLabel.h"
+#import "RCFractionLabel.h"
+
+#import "RCCalibration.h"
+
+#endif
+

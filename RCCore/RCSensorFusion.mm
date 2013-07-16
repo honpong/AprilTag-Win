106d2506a986491a8499c9dc5f181635a6721dd1
Ben Hirashima <ben@realitycap.com>
2013-07-16 12:38:38 -0700

    added expiration date : 9/29/13

--- RCCore/RCSensorFusion.mm
+++ RCCore/RCSensorFusion.mm
@@ -129,10 +129,35 @@ uint64_t get_timestamp()
     return self;
 }
 
+- (BOOL) isEvaluationExpired
+{
+    NSDateComponents *comps = [[NSDateComponents alloc] init];
+    [comps setDay:29];
+    [comps setMonth:9];
+    [comps setYear:2013];
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDate *expires = [gregorian dateFromComponents:comps];
+    NSDate* now = [NSDate date];
+    
+    return [now timeIntervalSinceDate:expires] > 0 ? true : false;
+}
+
 - (void) startSensorFusion:(CLLocation*)location
 {
     LOGME
-
+    
+    if ([self isEvaluationExpired])
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"RealityCap SDK evaluation expired"
+                                                        message:nil
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        
+        [alert show];
+        return;
+    }
+    
     [self
             setupPluginsWithFilter:true
             withCapture:false

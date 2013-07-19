1fd776d348fde09a62b557124bfaf24fb3c1c01a
Brian Fulkerson <bfulkerson@gmail.com>
2013-07-19 14:23:42 +0100

    Simplify the mapbuffer setup for the simulator

--- simulate.py
+++ simulate.py
@@ -14,30 +14,26 @@ from corvis import cor, filter
 
 cor.cvar.cor_time_pb_real = False
 
-KB = 1024
-MB = 1024*KB
-GB = 1024*MB
-
 capture = cor.mapbuffer()
 
 capture.filename = "test"
-capture.size = 1*GB
-capture.indexsize = 600000
-capture.ahead = 256*MB
-capture.behind = 32*MB
-capture.blocksize = 256*KB
-capture.mem_writable = True
-capture.file_writable = True
-capture.threaded = True
+capture.size = 32 * 1024 * 1024
+capture.replay = False
+capture.block_when_full = True
 capture.dispatch = cor.dispatch_t()
+capture.dispatch.reorder_depth = 20
+capture.dispatch.max_latency = 40000
+# Enabling threading segfaults
+# capture.dispatch->threaded = true;
+
 cor.plugins_register(cor.mapbuffer_open(capture))
+cor.plugins_register(cor.dispatch_init(capture.dispatch))
 
 dc = filter.corvis_device_parameters()
 
 from util.device_parameters import set_device_parameters
 set_device_parameters(dc, "simulator") 
 
-#fc = filter.filter_setup(dc)
 outname = "simulator_solution"
 fc = filter.filter_setup(capture.dispatch, outname, dc)
 fc.sfm.ignore_lateness = True
@@ -61,7 +57,6 @@ motion.color=[0.,0.,1.,1.]
 
 measurement = renderable.measurement("/Library/Fonts/Tahoma.ttf", 12.)
 measurement.color=[0.,1.,0.,1.]
-# _databuffer->block_when_full = true;
 
 filter_render = renderable.filter_state(fc.sfm)
 
@@ -69,9 +64,10 @@ myvis.frame_1.render_widget.renderables.append(structure.render)
 myvis.frame_1.render_widget.renderables.append(motion.render)
 myvis.frame_1.render_widget.renderables.append(measurement.render)
 myvis.frame_1.render_widget.renderables.append(filter_render.render)
-cor.dispatch_addclient(visbuf.dispatch, structure, renderable.structure_packet)
-cor.dispatch_addclient(visbuf.dispatch, motion, renderable.motion_packet)
-cor.dispatch_addclient(visbuf.dispatch, measurement, renderable.measurement_packet)
+cor.dispatch_addclient(fc.solution.dispatch, structure, renderable.structure_packet)
+cor.dispatch_addclient(fc.solution.dispatch, motion, renderable.motion_packet)
+cor.dispatch_addclient(fc.solution.dispatch, measurement, renderable.measurement_packet)
+
 
 fc.sfm.visbuf = visbuf
 

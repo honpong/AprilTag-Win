f365f17398825d88089eb8cb493ea5d58ae8d4aa
Ben Hirashima <ben@realitycap.com>
2013-07-19 10:21:00 -0700

    Initial Commit

---
+++ RC3DKSampleApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RC3DKSampleApp
+//
+//  Created by Ben Hirashima on 7/19/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

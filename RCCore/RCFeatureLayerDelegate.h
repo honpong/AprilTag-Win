593b1b96b119a6360261757f065411c25ab95356
Ben Hirashima <ben@realitycap.com>
2013-07-23 15:07:47 -0700

    moved TMFeatureLayerDelegate to RCCore

---
+++ RCCore/RCFeatureLayerDelegate.h
@@ -0,0 +1,16 @@
+//
+//  RCFeatureLayerDelegate.h
+//  RCCore
+//
+//  Created by Ben Hirashima on 7/23/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#define FEATURE_RADIUS 2
+#define FRAME_SIZE 8
+
+@interface RCFeatureLayerDelegate : NSObject
+
+@property (nonatomic) UIColor* color;
+
+@end

589cf446cda9514333bb8a29d5bddfabe6da2a00
Ben Hirashima <ben@realitycap.com>
2013-07-26 09:47:07 -0700

    Initial Commit

---
+++ MeasuredPhoto/MPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPAppDelegate.h
+//  MeasuredPhoto
+//
+//  Created by Ben Hirashima on 7/26/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

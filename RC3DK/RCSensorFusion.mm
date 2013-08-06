b892085c10ecc42c55429cdc0a2faa3a4744c479
Eagle Jones <eagle@seurat-2.local>
2013-08-06 14:49:45 -0700

    Ignore a nil location

--- RC3DK/RCSensorFusion.mm
+++ RC3DK/RCSensorFusion.mm
@@ -175,7 +175,10 @@ uint64_t get_timestamp()
 
 - (void) setLocation:(CLLocation*)location
 {
-    dispatch_async(queue, ^{ filter_compute_gravity(&_cor_setup->sfm, location.coordinate.latitude, location.altitude); } );
+    if(location)
+    {
+        dispatch_async(queue, ^{ filter_compute_gravity(&_cor_setup->sfm, location.coordinate.latitude, location.altitude); } );
+    }
 }
 
 - (void) startStaticCalibration

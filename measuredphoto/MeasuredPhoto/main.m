93f17fb435b783a63b9351b39aab0d1984ca2590
Eagle Jones <eagle@ambrosia.local>
2013-08-15 11:51:43 -0700

    Temporary hack to disable buggy console logs in xcode 5 / ios 7 beta

--- measuredphoto/MeasuredPhoto/main.m
+++ measuredphoto/MeasuredPhoto/main.m
@@ -10,8 +10,22 @@
 
 #import "MPAppDelegate.h"
 
+#warning "Remove the console log hack!"
+//TODO: This is a temporary hack for xcode 5 / ios 7 beta printing infinite assertmacro logs to the console
+static int (*_oldStdWrite)(void *, const char *, int);
+
+int __pyStderrWrite(void *inFD, const char *buffer, int size)
+{
+    if ( strncmp(buffer, "AssertMacros:", 13) == 0 ) {
+        return size;
+    }
+    return _oldStdWrite(inFD, buffer, size);
+}
+
 int main(int argc, char *argv[])
 {
+    _oldStdWrite = stderr->_write;
+    stderr->_write = __pyStderrWrite;
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([MPAppDelegate class]));
     }

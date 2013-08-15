293cace51b3b3b25b65200f63878bc2061c19d93
Brian Fulkerson <brian@realitycap.com>
2013-08-15 19:56:24 +0100

    remove score_match_ring and detect_threshold, make fast_9.cpp as close to the original fast_9.c as possible

--- corvis/filter/fast_detector/fast.h
+++ corvis/filter/fast_detector/fast.h
@@ -12,12 +12,10 @@ class fast_detector {
     int pixel[16];
     int xsize, ysize, stride;
     float score_match(const unsigned char *im1, const int x1, const int y1, const unsigned char *im2, const int x2, const int y2, float max_error);
-    float score_match_ring(const unsigned char *im1, const int x1, const int y1, const unsigned char *im2, const int x2, const int y2, float max_error);
 
  public:
     vector<xy> features;
     vector<xy> &detect(const unsigned char *im, const unsigned char *mask, int number_wanted, int bthresh);
-    vector<xy> &detect_threshold(const unsigned char *im, const unsigned char *mask, int b);
     xy track(const unsigned char *im1, const unsigned char *im2, int xcurrent, int ycurrent, int x1, int y1, int x2, int y2, int b);
     fast_detector(const int xsize, const int ysize, const int stride);
 };

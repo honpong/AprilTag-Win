6751230015290239b583d8b9a5623f32913d93ea
Ben Hirashima <ben@realitycap.com>
2013-08-23 15:23:23 -0700

    added AFNetworking to 3DK

---
+++ rc3dk/RC3DK/RCHTTPClient.h
@@ -0,0 +1,19 @@
+//
+//  RCHTTPClient.h
+//  RCCore
+//
+//  Created by Ben Hirashima on 2/7/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import "AFNetworking.h"
+#import "RCDeviceInfo.h"
+
+@interface RCHTTPClient : AFHTTPClient
+
+@property int apiVersion;
+
++ (void)initWithBaseUrl:(NSString*)baseUrl withAcceptHeader:(NSString*)acceptHeaderValue withApiVersion:(int)apiVersion;
++ (RCHTTPClient *) sharedInstance;
+
+@end

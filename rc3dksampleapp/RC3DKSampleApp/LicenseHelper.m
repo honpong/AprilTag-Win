664a0e3fe89b042ec31c5b47d6e156234ee01efe
Eagle Jones <eagle@seurat-2.local>
2013-09-10 11:44:31 -0700

    Better license error messages.

--- rc3dksampleapp/RC3DKSampleApp/LicenseHelper.m
+++ rc3dksampleapp/RC3DKSampleApp/LicenseHelper.m
@@ -21,7 +21,7 @@
         }
         else
         {
-            NSString * message = [NSString stringWithFormat:@"There is a problem with the status your license. The license status code is: %i", licenseStatus];
+            NSString * message = [NSString stringWithFormat:@"There is a problem with your license. Please make sure you've correctly entered your API key in LicenseHelper.h. The license status code is: %i.", licenseStatus];
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"RC3DK License Status Error"
                                                             message:message
                                                            delegate:self
@@ -30,7 +30,7 @@
             [alert show];
         }
     } withErrorBlock:^(NSError * error) {
-        NSString * message = [NSString stringWithFormat:@"There was a problem validating your license. The license error code is: %i", error.code];
+        NSString * message = [NSString stringWithFormat:@"There was a problem validating your license. The license error code is: %i.", error.code];
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"RC3DK License Error"
                                                         message:message
                                                        delegate:self

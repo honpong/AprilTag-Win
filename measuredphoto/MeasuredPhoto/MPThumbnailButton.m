f30ce3f609da92743e376e417ce4cd7331c56ea6
Ben Hirashima <ben@realitycap.com>
2014-02-28 16:28:59 -0800

    shutter and thumbnail buttons rotate themselves on iPhone

--- measuredphoto/MeasuredPhoto/MPThumbnailButton.m
+++ measuredphoto/MeasuredPhoto/MPThumbnailButton.m
@@ -7,7 +7,34 @@
 //
 
 #import "MPThumbnailButton.h"
+#import "UIView+MPCascadingRotation.h"
 
 @implementation MPThumbnailButton
+
+- (id) initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder])
+    {
+        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+        {
+            [[NSNotificationCenter defaultCenter] addObserver:self
+                                                     selector:@selector(handleOrientationChange)
+                                                         name:UIDeviceOrientationDidChangeNotification
+                                                       object:nil];
+        }
+    }
+    return self;
+}
+
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void) handleOrientationChange
+{
+    UIDeviceOrientation orientation = [[UIDevice currentDevice] orientation];
+    [self applyRotationTransformation:orientation];
+}
 
 @end

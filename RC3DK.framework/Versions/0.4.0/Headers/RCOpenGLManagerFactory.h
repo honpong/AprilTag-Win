56e47c0e5a309de63ad8858c60577404edf0e977
Ben Hirashima <ben@realitycap.com>
2014-03-31 15:42:54 -0700

    incremented 3DK version to 0.4.0. updated static framework used by sample apps.

---
+++ RC3DK.framework/Versions/0.4.0/Headers/RCOpenGLManagerFactory.h
@@ -0,0 +1,34 @@
+//
+//  RCOpenGLManagerFactory.h
+//  RCCore
+//
+//  Created by Ben Hirashima on 7/24/13.
+//  Copyright (c) 2013 RealityCap. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+#include <OpenGLES/ES2/gl.h>
+
+// the factory returns an instance of an object that implements this protocol
+@protocol RCOpenGLManager <NSObject>
+
+@property GLuint yuvTextureProgram;
+@property GLuint tapeProgram;
+@property EAGLContext* oglContext;
+
+@end
+
+// the factory methods to get/set the instance
+@interface RCOpenGLManagerFactory : NSObject
+
++ (id<RCOpenGLManager>)getInstance;
++ (void)setInstance:(id<RCOpenGLManager>)mockObject; // makes testing possible
+
+@end
+
+enum {
+    ATTRIB_VERTEX,
+    ATTRIB_TEXTUREPOSITON,
+    ATTRIB_PERPINDICULAR,
+    NUM_ATTRIBUTES
+};

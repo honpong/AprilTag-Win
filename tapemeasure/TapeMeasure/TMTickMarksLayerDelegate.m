d7588aa85c66e1fa45eee6fd4a092d91c993061d
Ben Hirashima <ben@realitycap.com>
2014-04-02 12:57:43 -0700

    moving tape visual tweaks. bigger, more visible.

--- tapemeasure/TapeMeasure/TMTickMarksLayerDelegate.m
+++ tapemeasure/TapeMeasure/TMTickMarksLayerDelegate.m
@@ -8,13 +8,15 @@
 
 #import "TMTickMarksLayerDelegate.h"
 
-#define LAYER_HEIGHT 65 //need this because we can't get an accurate layer height for some reason
+#define LAYER_HEIGHT 80 //need this because we can't get an accurate layer height for some reason
 #define SCREEN_PIXEL_WIDTH 320
+
+static const CGFloat tickScaleFactor = 2;
 
 @implementation TMTickMarksLayerDelegate
 {
-    float screenWidthMeters;
-    int screenWidthCM;
+    CGFloat screenWidthMeters;
+    CGFloat screenWidthCM;
     Units units;
 }
 
@@ -34,43 +36,50 @@
 
 - (void) drawLayer:(CALayer *)layer inContext:(CGContextRef)context
 {
+    CGContextSetShouldAntialias(context, NO);
+    
+    [self drawTapeWithColor:[UIColor colorWithRed:.855 green:.648 blue:.164 alpha:1.] withLineWidth:2 withOffset:0 inContext:context];
+    [self drawTapeWithColor:[UIColor blackColor] withLineWidth:2 withOffset:3 inContext:context];
+}
+
+- (void) drawTapeWithColor:(UIColor*)color withLineWidth:(CGFloat)lineWidth withOffset:(CGFloat)offset inContext:(CGContextRef)context
+{
     if (units == UnitsImperial)
     {
-        float screenWidthIn = screenWidthMeters * INCHES_PER_METER;
+        CGFloat screenWidthIn = screenWidthMeters * INCHES_PER_METER;
         int wholeInches = ceil(screenWidthIn) + 2;
-        float pixelsPerInch = SCREEN_PIXEL_WIDTH / screenWidthIn;
-              
-        [self drawTickSet:context withOffset:0 withTickCount:wholeInches withSpacing:pixelsPerInch withTickHeight:16];
-        [self drawTickSet:context withOffset:pixelsPerInch / 2 withTickCount:wholeInches withSpacing:pixelsPerInch withTickHeight:14];
-        [self drawTickSet:context withOffset:pixelsPerInch / 4 withTickCount:wholeInches * 2 withSpacing:pixelsPerInch / 2 withTickHeight:10];
-        [self drawTickSet:context withOffset:pixelsPerInch / 8 withTickCount:wholeInches * 4 withSpacing:pixelsPerInch / 4 withTickHeight:6];
-        [self drawTickSet:context withOffset:pixelsPerInch / 16 withTickCount:wholeInches * 8 withSpacing:pixelsPerInch / 8 withTickHeight:3];
+        CGFloat pixelsPerInch = SCREEN_PIXEL_WIDTH / screenWidthIn;
+        
+        [self drawTickSet:context withOffset:offset + 0 withTickCount:wholeInches withSpacing:pixelsPerInch withTickHeight:tickScaleFactor * 16];
+        [self drawTickSet:context withOffset:offset + pixelsPerInch / 2 withTickCount:wholeInches withSpacing:pixelsPerInch withTickHeight:tickScaleFactor * 14];
+        [self drawTickSet:context withOffset:offset + pixelsPerInch / 4 withTickCount:wholeInches * 2 withSpacing:pixelsPerInch / 2 withTickHeight:tickScaleFactor * 10];
+        [self drawTickSet:context withOffset:offset + pixelsPerInch / 8 withTickCount:wholeInches * 4 withSpacing:pixelsPerInch / 4 withTickHeight:tickScaleFactor * 6];
+        [self drawTickSet:context withOffset:offset + pixelsPerInch / 16 withTickCount:wholeInches * 8 withSpacing:pixelsPerInch / 8 withTickHeight:tickScaleFactor * 3];
     }
     else
     {
         int wholeCM = ceil(screenWidthCM) + 1;
-        float pixelsPerCM = SCREEN_PIXEL_WIDTH / screenWidthCM;
-        float pixelsPerMM = pixelsPerCM / 10;
+        CGFloat pixelsPerCM = SCREEN_PIXEL_WIDTH / screenWidthCM;
+        CGFloat pixelsPerMM = pixelsPerCM / 10;
         
-        [self drawTickSet:context withOffset:0 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:14];
-        [self drawTickSet:context withOffset:pixelsPerCM / 2 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:8];
-        [self drawTickSet:context withOffset:pixelsPerMM     withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:4];
-        [self drawTickSet:context withOffset:pixelsPerMM * 2 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:4];
-        [self drawTickSet:context withOffset:pixelsPerMM * 3 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:4];
-        [self drawTickSet:context withOffset:pixelsPerMM * 4 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:4];
-        [self drawTickSet:context withOffset:pixelsPerMM * 6 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:4];
-        [self drawTickSet:context withOffset:pixelsPerMM * 7 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:4];
-        [self drawTickSet:context withOffset:pixelsPerMM * 8 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:4];
-        [self drawTickSet:context withOffset:pixelsPerMM * 9 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:4];
+        [self drawTickSet:context withOffset:offset + 0 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 14];
+        [self drawTickSet:context withOffset:offset + pixelsPerCM / 2 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 8];
+        [self drawTickSet:context withOffset:offset + pixelsPerMM     withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 4];
+        [self drawTickSet:context withOffset:offset + pixelsPerMM * 2 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 4];
+        [self drawTickSet:context withOffset:offset + pixelsPerMM * 3 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 4];
+        [self drawTickSet:context withOffset:offset + pixelsPerMM * 4 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 4];
+        [self drawTickSet:context withOffset:offset + pixelsPerMM * 6 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 4];
+        [self drawTickSet:context withOffset:offset + pixelsPerMM * 7 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 4];
+        [self drawTickSet:context withOffset:offset + pixelsPerMM * 8 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 4];
+        [self drawTickSet:context withOffset:offset + pixelsPerMM * 9 withTickCount:wholeCM withSpacing:pixelsPerCM withTickHeight:tickScaleFactor * 4];
     }
     
-    CGContextSetShouldAntialias(context, NO);
-    CGContextSetLineWidth(context, 1);
-    CGContextSetStrokeColorWithColor(context, [[UIColor yellowColor] CGColor]);
+    CGContextSetStrokeColorWithColor(context, [color CGColor]);
+    CGContextSetLineWidth(context, lineWidth);
     CGContextStrokePath(context);
 }
 
-- (void) drawTickSet:(CGContextRef)context withOffset:(float)offsetY withTickCount:(float)count withSpacing:(float)spacing withTickHeight:(int)height
+- (void) drawTickSet:(CGContextRef)context withOffset:(CGFloat)offsetY withTickCount:(CGFloat)count withSpacing:(CGFloat)spacing withTickHeight:(CGFloat)height
 {
     for (int i = 0; i <= count; i++)
     {

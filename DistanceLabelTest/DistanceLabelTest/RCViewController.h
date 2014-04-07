d4632bd383fa3f73a16a648f506c9354ebcdeceb
Ben Hirashima <ben@realitycap.com>
2014-04-07 11:16:57 -0700

    new project DistanceLabelTest

---
+++ DistanceLabelTest/DistanceLabelTest/RCViewController.h
@@ -0,0 +1,13 @@
+//
+//  RCViewController.h
+//  DistanceLabelTest
+//
+//  Created by Ben Hirashima on 4/7/14.
+//  Copyright (c) 2014 RealityCap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCViewController : UIViewController
+
+@end
---
+++ DistanceLabelTest/DistanceLabelTest/RCViewController.m
@@ -0,0 +1,29 @@
+//
+//  RCViewController.m
+//  DistanceLabelTest
+//
+//  Created by Ben Hirashima on 4/7/14.
+//  Copyright (c) 2014 RealityCap. All rights reserved.
+//
+
+#import "RCViewController.h"
+
+@interface RCViewController ()
+
+@end
+
+@implementation RCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

5bd2eeeff3c82989014bc61273a007c4e0256efa
jordan miller <jordan@realitycap.com>
2014-07-25 12:26:31 -0700

    serperating measurement handling code into seperate script

---
+++ measuredphoto/MeasuredPhoto/measurements.js
@@ -0,0 +1,33 @@
+//Copywrite (c) 2014 by RealityCap, Inc. Written by Jordan Miller for the exclusive use of RealityCap, Inc.
+
+var rcMeasurements = {
+    measurements : []
+}
+
+rcMeasurements.new_measurement = function (){
+
+}
+
+rcMeasurements.draw_measurement = function (){
+
+}
+
+rcMeasurements.redraw_measurement = function () {
+
+}
+
+rcMeasurements.to_json = function () {
+
+}
+
+rcMeasurements.measurement_to_json  = function () {
+
+}
+
+rcMeasurements.delete_measurement  = function () {
+
+}
+
+rcMeasurements.load_json  = function () {
+
+}

c8322f45c7d5d0a422430eccf05874a9f61d43b3
Eagle Jones <eagle@seurat>
2014-10-30 17:34:27 -0700

    Initial lighting working. Need to clean up.

--- RC3DKSampleAR/RC3DKSampleAR/ARDelegate.m
+++ RC3DKSampleAR/RC3DKSampleAR/ARDelegate.m
@@ -21,19 +21,24 @@
 
 enum {
     ATTRIB_VERTEX,
+    ATTRIB_NORMAL,
     NUM_ATTRIBUTES
 };
 
 const static GLint attribLocation[NUM_ATTRIBUTES] = {
-    ATTRIB_VERTEX
+    ATTRIB_VERTEX,
+    ATTRIB_NORMAL
 };
 
 const static GLchar * attribName[NUM_ATTRIBUTES] = {
-    "position"
+    "position",
+    "normal"
 };
 
 enum {
-    UNIFORM_COLOR,
+    //UNIFORM_COLOR,
+    UNIFORM_DIRECTIONAL,
+    UNIFORM_MATERIAL,
     UNIFORM_PROJECTION,
     UNIFORM_CAMERA,
     UNIFORM_MODEL,
@@ -44,7 +49,9 @@ static int uniformLocation[NUM_UNIFORMS];
 
 const static GLchar *uniformName[NUM_UNIFORMS] =
 {
-    "color",
+    //"color",
+    "directional",
+    "mat",
     "projection_matrix",
     "camera_matrix",
     "model_matrix"
@@ -84,20 +91,141 @@ const static GLchar *uniformName[NUM_UNIFORMS] =
 
     glUniformMatrix4fv(uniformLocation[UNIFORM_PROJECTION], 1, false, projection.m);
     glUniformMatrix4fv(uniformLocation[UNIFORM_CAMERA], 1, false, camera.m);
-    glUniformMatrix4fv(uniformLocation[UNIFORM_MODEL], 1, false, model.m);
 
-    GLfloat vertices[] = {
-        -.05, -.05, 1,
-        .05, -.05, 1,
-        -.05, .05, 1,
-        .05, .05, 1
-    };
-    glVertexAttribPointer(ATTRIB_VERTEX, 3, GL_FLOAT, 0, 0, vertices);
     glEnableVertexAttribArray(ATTRIB_VERTEX);
-    glUniform4f(uniformLocation[UNIFORM_COLOR], 0, 0, 1, 1);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    
+    glUniform3f(glGetUniformLocation(myProgram, "directional.direction"), 0, 0, -1);
+    glUniform3f(glGetUniformLocation(myProgram, "directional.halfplane"), 0, 0, -1);
+    glUniform4f(glGetUniformLocation(myProgram, "directional.ambientColor"), .8, .8, .8, 1);
+    glUniform4f(glGetUniformLocation(myProgram, "directional.diffuseColor"), .8, .8, .8, 1);
+    glUniform4f(glGetUniformLocation(myProgram, "directional.specularColor"), .8, .8, .8, 1);
+    
+    glUniform4f(glGetUniformLocation(myProgram, "mat.ambientFactor"), 0., 0., 1., 1);
+    glUniform4f(glGetUniformLocation(myProgram, "mat.diffuseFactor"), 0., 0.5, 1., 1);
+    glUniform4f(glGetUniformLocation(myProgram, "mat.specularFactor"), 1., 1., 1., 1);
+    glUniform1f(glGetUniformLocation(myProgram, "mat.shininess"), 0.2);
 
-    static const GLfloat cubeVerticesStrip[] = {
+    model = GLKMatrix4Translate(model, 0, 0, 1);
+    model = GLKMatrix4Scale(model, .1, .1, .1);
+    glUniformMatrix4fv(uniformLocation[UNIFORM_MODEL], 1, false, model.m);
+    
+    {
+        GLfloat face[] = {
+            -1,-1,1, -1,1,1, 1,-1,1,
+            -1,1,1, 1,-1,1, 1,1,1
+        };
+        GLfloat normal[] = {
+            0,0,1, 0,0,1, 0,0,1,
+            0,0,1, 0,0,1, 0,0,1
+        };
+        
+        glVertexAttribPointer(ATTRIB_VERTEX, 3, GL_FLOAT, 0, 0, face);
+        glEnableVertexAttribArray(ATTRIB_VERTEX);
+        
+        glVertexAttribPointer(ATTRIB_NORMAL, 3, GL_FLOAT, 0, 0, normal);
+        glEnableVertexAttribArray(ATTRIB_NORMAL);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+    
+    {
+        GLfloat face[] = {
+            -1,-1,-1, -1,1,-1, 1,-1,-1,
+            -1,1,-1, 1,-1,-1, 1,1,-1
+        };
+        GLfloat normal[] = {
+            0,0,-1, 0,0,-1, 0,0,-1,
+            0,0,-1, 0,0,-1, 0,0,-1
+        };
+        
+        glVertexAttribPointer(ATTRIB_VERTEX, 3, GL_FLOAT, 0, 0, face);
+        glEnableVertexAttribArray(ATTRIB_VERTEX);
+        
+        glVertexAttribPointer(ATTRIB_NORMAL, 3, GL_FLOAT, 0, 0, normal);
+        glEnableVertexAttribArray(ATTRIB_NORMAL);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+    
+    {
+        GLfloat face[] = {
+            1,-1,-1, 1,1,-1, 1,-1,1,
+            1,1,-1, 1,-1,1, 1,1,1
+        };
+        GLfloat normal[] = {
+            1,0,0, 1,0,0, 1,0,0,
+            1,0,0, 1,0,0, 1,0,0
+        };
+        
+        glVertexAttribPointer(ATTRIB_VERTEX, 3, GL_FLOAT, 0, 0, face);
+        glEnableVertexAttribArray(ATTRIB_VERTEX);
+        
+        glVertexAttribPointer(ATTRIB_NORMAL, 3, GL_FLOAT, 0, 0, normal);
+        glEnableVertexAttribArray(ATTRIB_NORMAL);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+
+    {
+        GLfloat face[] = {
+            -1,-1,-1, -1,1,-1, -1,-1,1,
+            -1,1,-1, -1,-1,1, -1,1,1
+        };
+        GLfloat normal[] = {
+            -1,0,0, -1,0,0, -1,0,0,
+            -1,0,0, -1,0,0, -1,0,0
+        };
+        
+        glVertexAttribPointer(ATTRIB_VERTEX, 3, GL_FLOAT, 0, 0, face);
+        glEnableVertexAttribArray(ATTRIB_VERTEX);
+        
+        glVertexAttribPointer(ATTRIB_NORMAL, 3, GL_FLOAT, 0, 0, normal);
+        glEnableVertexAttribArray(ATTRIB_NORMAL);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+    
+    {
+        GLfloat face[] = {
+            -1,1,-1, 1,1,-1, -1,1,1,
+            1,1,-1, -1,1,1, 1,1,1
+        };
+        GLfloat normal[] = {
+            0,1,0, 0,1,0, 0,1,0,
+            0,1,0, 0,1,0, 0,1,0
+        };
+        
+        glVertexAttribPointer(ATTRIB_VERTEX, 3, GL_FLOAT, 0, 0, face);
+        glEnableVertexAttribArray(ATTRIB_VERTEX);
+        
+        glVertexAttribPointer(ATTRIB_NORMAL, 3, GL_FLOAT, 0, 0, normal);
+        glEnableVertexAttribArray(ATTRIB_NORMAL);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+    
+    {
+        GLfloat face[] = {
+            -1,-1,-1, 1,-1,-1, -1,-1,1,
+            1,-1,-1, -1,-1,1, 1,-1,1
+        };
+        GLfloat normal[] = {
+            0,-1,0, 0,-1,0, 0,-1,0,
+            0,-1,0, 0,-1,0, 0,-1,0
+        };
+        
+        glVertexAttribPointer(ATTRIB_VERTEX, 3, GL_FLOAT, 0, 0, face);
+        glEnableVertexAttribArray(ATTRIB_VERTEX);
+        
+        glVertexAttribPointer(ATTRIB_NORMAL, 3, GL_FLOAT, 0, 0, normal);
+        glEnableVertexAttribArray(ATTRIB_NORMAL);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+
+
+/*    static const GLfloat cubeVerticesStrip[] = {
+        
         // Front face
         -1,-1,1, 1,-1,1, -1,1,1, 1,1,1,
         // Right face
@@ -114,16 +242,10 @@ const static GLchar *uniformName[NUM_UNIFORMS] =
         
         // Top Face
         -1,1,1, 1,1,1, -1,1,-1, 1,1,-1
-    };
-    glVertexAttribPointer(ATTRIB_VERTEX, 3, GL_FLOAT, 0, 0, cubeVerticesStrip);
-    glEnableVertexAttribArray(ATTRIB_VERTEX);
-    glUniform4f(uniformLocation[UNIFORM_COLOR], 0, 1, 0, 1);
+    };*/
+    //glUniform4f(uniformLocation[UNIFORM_COLOR], 0, 1, 0, 1);
 
-    model = GLKMatrix4Translate(model, 0, 0, 2);
-    model = GLKMatrix4Scale(model, .1, .1, .1);
-    glUniformMatrix4fv(uniformLocation[UNIFORM_MODEL], 1, false, model.m);
 
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 26);
 
     // Validate program before drawing. This is a good check, but only really necessary in a debug build.
     // DEBUG macro must be defined in your debug configurations if that's not already the case.
--- RC3DKSampleAR/RC3DKSampleAR/flat.vsh
+++ RC3DKSampleAR/RC3DKSampleAR/flat.vsh
@@ -6,11 +6,16 @@
 //
 
 attribute vec4 position;
+attribute vec3 normal;
 uniform mat4 projection_matrix;
 uniform mat4 camera_matrix;
 uniform mat4 model_matrix;
 
+varying vec3 v_normal;
+
 void main()
 {
     gl_Position = projection_matrix * camera_matrix * model_matrix * position;
+    vec3 norm_calc = vec3(camera_matrix * model_matrix * vec4(normal, 0.));
+    v_normal = norm_calc / length(norm_calc);
 }

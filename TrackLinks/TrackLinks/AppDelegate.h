763cfcbd6b2bca80e093911697d7d1cb0d1b43a7
Ben Hirashima <ben@realitycap.com>
2014-11-25 14:02:15 -0800

    created TrackLinks project

---
+++ TrackLinks/TrackLinks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrackLinks
+//
+//  Created by Ben Hirashima on 11/25/14.
+//  Copyright (c) 2014 Caterpillar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

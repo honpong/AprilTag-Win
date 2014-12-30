b1231d86c6d73cccde110987d69ab5da34715de4
Ben Hirashima <ben@realitycap.com>
2014-12-29 15:56:11 -1000

    initial commit for Sunlayar

---
+++ Sunlayar/Sunlayar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sunlayar
+//
+//  Created by Ben Hirashima on 12/29/14.
+//  Copyright (c) 2014 Sunlayar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sunlayar/Sunlayar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sunlayar
+//
+//  Created by Ben Hirashima on 12/29/14.
+//  Copyright (c) 2014 Sunlayar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

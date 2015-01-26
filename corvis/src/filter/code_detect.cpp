fc14bbc694560c2477e75f074edd2401c37efee1
Brian Fulkerson <brian@realitycap.com>
2015-01-26 07:50:41 +0000

    Remove a bunch of unused includes in code_detect

--- corvis/src/filter/code_detect.cpp
+++ corvis/src/filter/code_detect.cpp
@@ -6,27 +6,14 @@
 //  Adapted from /cpp/cli/src/main.cpp
 
 #include "code_detect.h"
-#include <stdio.h>
 
-#include <zxing/common/Counted.h>
-#include <zxing/Binarizer.h>
-#include <zxing/MultiFormatReader.h>
 #include <zxing/Result.h>
 #include <zxing/ReaderException.h>
-//#include <zxing/common/GlobalHistogramBinarizer.h>
-#include <zxing/common/HybridBinarizer.h>
-#include <zxing/common/GreyscaleLuminanceSource.h>
-#include <exception>
-#include <zxing/Exception.h>
 #include <zxing/common/IllegalArgumentException.h>
 #include <zxing/BinaryBitmap.h>
 #include <zxing/DecodeHints.h>
-
 #include <zxing/qrcode/QRCodeReader.h>
 #include <zxing/multi/qrcode/QRCodeMultiReader.h>
-#include <zxing/multi/ByQuadrantReader.h>
-#include <zxing/multi/MultipleBarcodeReader.h>
-#include <zxing/multi/GenericMultipleBarcodeReader.h>
 #include <zxing/common/HybridFastBinarizer.h>
 
 using namespace std;

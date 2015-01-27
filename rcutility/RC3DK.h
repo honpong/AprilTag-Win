49f4c45fb9a7e5cd18edd53984c53a2881226f43
Brian Fulkerson <brian@realitycap.com>
2015-01-27 12:26:21 +0000

    Add RC3DK.h to RCUtility to allow shared files to import 3dk

---
+++ rcutility/RC3DK.h
@@ -0,0 +1,15 @@
+//
+//  RC3DK.h
+//  RCUtility
+//
+//  Created by Brian on 1/27/15.
+//  Copyright (c) 2015 Realitycap. All rights reserved.
+//
+
+#ifndef RCUtility_RC3DK_h
+#define RCUtility_RC3DK_h
+
+// this file exists so that shared source files outside this project can import 3DK using the quoted style, like #import "RC3DK.h"
+#import <RC3DKPlus/RC3DKPlus.h>
+
+#endif

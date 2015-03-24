ed3e5b7e910d71e504cea9df4b58ce8fdc7abc32
Jim Radford <jim@realitycap.com>
2015-03-24 15:50:03 -0700

    initial_orientation: test for facing along +y

--- corvis/test/filter/utils.cpp
+++ corvis/test/filter/utils.cpp
@@ -6,14 +6,18 @@
 
 static void check_initial_orientation_from_gravity(v4 gravity)
 {
-    v4 camera(0,0,1,0), z(0,0,1,0), x(1,0,0,0);
+    v4 facing(0,1,0,0);
+    v4 camera(0,0,-1,0), z(0,0,1,0), facing_perp(-facing[1], facing[0], 0, 0);
     quaternion q = initial_orientation_from_gravity(gravity);
 
     // gravity should point in the z direction
     test_v4_near(normalize(quaternion_rotate(q, gravity)), z, 2*F_T_EPS);
 
-    // the camera should be aligned with y (i.e. have no conponent in the x direction)
-    EXPECT_NEAR(dot(quaternion_rotate(q, camera), x), 0, 2*F_T_EPS);
+    // the camera should be aligned with facing
+    EXPECT_GE(dot(quaternion_rotate(q, camera), facing), 0);
+
+    // and have no conponent in the facing_perp direction
+    EXPECT_NEAR(dot(quaternion_rotate(q, camera), facing_perp), 0, 2*F_T_EPS);
 }
 
 TEST(Filter, InitialOrientation)

3295f45ac838a751d128ca6c6242368e7e689e15
Eagle Jones <eagle@seurat>
2015-03-27 12:38:23 -0700

    Give the VideoPreview delegate two methods, one called during rendering, and one called after rendering, to make both ar approaches work

--- VideoPreview/RCVideoPreview.m
+++ VideoPreview/RCVideoPreview.m
@@ -174,10 +174,14 @@ void main()
                         //call AR delegate
                         GLKMatrix4 perspective = [weakSelf getPerspectiveMatrixWithCameraParameters:localData.cameraParameters withNear:.01 withFar:100.];
                         GLKMatrix4 camera_screen = [weakSelf getScreenRotationForOrientation:self.orientation];
-                        [weakSelf endFrame];
                         
                         if([weakSelf.delegate respondsToSelector:@selector(renderWithSensorFusionData:withCameraToScreenMatrix:)])
                             [weakSelf.delegate renderWithSensorFusionData:localData withCameraToScreenMatrix:GLKMatrix4Multiply(camera_screen, perspective)];
+                        [weakSelf endFrame];
+                        if([weakSelf.delegate respondsToSelector:@selector(didRenderWithSensorFusionData:withCameraToScreenMatrix:)])
+                            [weakSelf.delegate didRenderWithSensorFusionData:localData withCameraToScreenMatrix:GLKMatrix4Multiply(camera_screen, perspective)];
+
+
                     }
                 }
                 CFRelease(pixBuf);

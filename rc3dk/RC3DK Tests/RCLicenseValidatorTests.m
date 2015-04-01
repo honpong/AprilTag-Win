f70cd77abce9f8a0a2ee5079c390eb3e4fc28138
Ben Hirashima <ben@realitycap.com>
2015-03-31 19:12:05 -0700

    added Nocilla library to help test HTTP requests

--- rc3dk/RC3DK Tests/RCLicenseValidatorTests.m
+++ rc3dk/RC3DK Tests/RCLicenseValidatorTests.m
@@ -12,7 +12,10 @@
 #import "RCPrivateHTTPClient.h"
 #import "RCConstants.h"
 #import "RCLicenseError.h"
+#import "Nocilla.h"
 
+#define TIMEOUT 10.
+#define URL_LICENSING @"https://app.realitycap.com/api/v1/licensing/"
 #define VALID_LICENSE_KEY @"D3bed93A58f8A25FDF7Cbc4da0634D" // Ben's key
 #define BUNDLE_ID @"com.realitycap.tapemeasure"
 #define VENDOR_ID @"E621E1F8-C36C-495A-93FC-0C247A3E6E5F" // random example
@@ -24,6 +27,7 @@
 @implementation RCLicenseValidatorTests
 {
     BOOL done;
+    XCTestExpectation* responseArrived;
 }
 
 - (void)setUp
@@ -31,11 +35,15 @@
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
     [RCPrivateHTTPClient initWithBaseUrl:API_BASE_URL withAcceptHeader:API_HEADER_ACCEPT withApiVersion:API_VERSION];
+    [[LSNocilla sharedInstance] start];
+    responseArrived = [self expectationWithDescription:@"HTTP response arrived"];
 }
 
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [[LSNocilla sharedInstance] stop];
+    [[LSNocilla sharedInstance] clearStubs];
     [super tearDown];
 }
 
@@ -52,107 +60,95 @@
     return done;
 }
 
-- (void)testLiveServer
+- (void)testLicenseValid
 {
+    stubRequest(@"POST", URL_LICENSING).
+    andReturn(200).
+    withHeaders(@{@"Content-Type": @"application/json"}).
+    withBody(@"{\"license_type\":0,\"license_status\":0}");
+    
     RCLicenseValidator* validator = [RCLicenseValidator initWithBundleId:BUNDLE_ID withVendorId:VENDOR_ID withHTTPClient:HTTP_CLIENT withUserDefaults:NSUserDefaults.standardUserDefaults];
     
     [validator validateLicense:VALID_LICENSE_KEY withCompletionBlock:^(int licenseType, int licenseStatus) {
-        DLog(@"License type: %i, status: %i", licenseType, licenseStatus);
-        done = YES;
+        [responseArrived fulfill];
     } withErrorBlock:^(NSError* error) {
+        [responseArrived fulfill];
         XCTFail(@"%@", error.description);
-        done = YES;
     }];
     
-    XCTAssertTrue([self waitForCompletion:10.0], @"Request timed out");
+    [self waitForExpectationsWithTimeout:TIMEOUT handler:nil];
 }
 
 - (void) testMissingKey
 {
-    BOOL __block wasErrorBlockCalled = NO;
-    
     RCLicenseValidator* validator = [RCLicenseValidator initWithBundleId:BUNDLE_ID withVendorId:VENDOR_ID withHTTPClient:HTTP_CLIENT withUserDefaults:NSUserDefaults.standardUserDefaults];
     
     [validator
      validateLicense:nil
      withCompletionBlock:^(int licenseType, int licenseStatus){
+         [responseArrived fulfill];
          XCTFail(@"Completion block should not be called");
-         done = YES;
      }
      withErrorBlock:^(NSError* error){
+         [responseArrived fulfill];
          XCTAssertEqual(error.code, RCLicenseErrorMissingKey, @"Error code should be RCLicenseErrorMissingKey");
-         wasErrorBlockCalled = YES;
-         done = YES;
      }];
     
-    XCTAssertTrue([self waitForCompletion:10.0], @"Request timed out");
-    XCTAssertTrue(wasErrorBlockCalled, @"Error block wasn't called");
+    [self waitForExpectationsWithTimeout:TIMEOUT handler:nil];
 }
 
 - (void) testMalformedKey
 {
-    BOOL __block wasErrorBlockCalled = NO;
-    
     RCLicenseValidator* validator = [RCLicenseValidator initWithBundleId:BUNDLE_ID withVendorId:VENDOR_ID withHTTPClient:HTTP_CLIENT withUserDefaults:NSUserDefaults.standardUserDefaults];
     
     [validator
      validateLicense:@"D3bed93A58f8A25FDF7Cbc4da0634D_too_long"
      withCompletionBlock:^(int licenseType, int licenseStatus){
+         [responseArrived fulfill];
          XCTFail(@"Completion block should not be called");
-         done = YES;
      }
      withErrorBlock:^(NSError* error){
+         [responseArrived fulfill];
          XCTAssertEqual(error.code, RCLicenseErrorMalformedKey, @"Error code should be RCLicenseErrorMalformedKey");
-         wasErrorBlockCalled = YES;
-         done = YES;
      }];
     
-    XCTAssertTrue([self waitForCompletion:10.0], @"Request timed out");
-    XCTAssertTrue(wasErrorBlockCalled, @"Error block wasn't called");
+    [self waitForExpectationsWithTimeout:TIMEOUT handler:nil];
 }
 
 - (void) testMissingBundleId
 {
-    BOOL __block wasErrorBlockCalled = NO;
-    
     RCLicenseValidator* validator = [RCLicenseValidator initWithBundleId:nil withVendorId:VENDOR_ID withHTTPClient:HTTP_CLIENT withUserDefaults:NSUserDefaults.standardUserDefaults];
     
     [validator
      validateLicense:VALID_LICENSE_KEY
      withCompletionBlock:^(int licenseType, int licenseStatus){
+         [responseArrived fulfill];
          XCTFail(@"Completion block should not be called");
-         done = YES;
      }
      withErrorBlock:^(NSError* error){
+         [responseArrived fulfill];
          XCTAssertEqual(error.code, RCLicenseErrorBundleIdMissing, @"Error code should be RCLicenseErrorBundleIdMissing");
-         wasErrorBlockCalled = YES;
-         done = YES;
      }];
     
-    XCTAssertTrue([self waitForCompletion:10.0], @"Request timed out");
-    XCTAssertTrue(wasErrorBlockCalled, @"Error block wasn't called");
+    [self waitForExpectationsWithTimeout:TIMEOUT handler:nil];
 }
 
 - (void) testMissingVendorId
 {
-    BOOL __block wasErrorBlockCalled = NO;
-    
     RCLicenseValidator* validator = [RCLicenseValidator initWithBundleId:BUNDLE_ID withVendorId:nil withHTTPClient:HTTP_CLIENT withUserDefaults:NSUserDefaults.standardUserDefaults];
     
     [validator
      validateLicense:VALID_LICENSE_KEY
      withCompletionBlock:^(int licenseType, int licenseStatus){
+         [responseArrived fulfill];
          XCTFail(@"Completion block should not be called");
-         done = YES;
      }
      withErrorBlock:^(NSError* error){
+         [responseArrived fulfill];
          XCTAssertEqual(error.code, RCLicenseErrorVendorIdMissing, @"Error code should be RCLicenseErrorVendorIdMissing");
-         wasErrorBlockCalled = YES;
-         done = YES;
      }];
     
-    XCTAssertTrue([self waitForCompletion:10.0], @"Request timed out");
-    XCTAssertTrue(wasErrorBlockCalled, @"Error block wasn't called");
+    [self waitForExpectationsWithTimeout:TIMEOUT handler:nil];
 }
 
 

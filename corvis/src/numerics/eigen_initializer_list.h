d50cfd09b7012ef0bbf9e19c846d4d943bcd746a
Eagle Jones <eagle@seurat>
2015-04-10 15:38:03 -0700

    plugin for eigen to accept c++ initializer lists for construction

---
+++ corvis/src/numerics/eigen_initializer_list.h
@@ -0,0 +1,53 @@
+//
+//  eigen_initializer_list.h
+//  RC3DK
+//
+//  Created by Eagle Jones on 4/2/15.
+//  Copyright (c) 2015 RealityCap. All rights reserved.
+//
+
+#ifndef RC3DK_eigen_initializer_list_h
+#define RC3DK_eigen_initializer_list_h
+
+#include <initializer_list>
+
+EIGEN_STRONG_INLINE Matrix(std::initializer_list<const Scalar> initlist): Base()
+{
+    Base::_check_template_params();
+    int dim = initlist.size();
+    eigen_assert(SizeAtCompileTime == Dynamic || SizeAtCompileTime == dim);
+    if(Base::size() == 0) Base::resize(dim);
+    eigen_assert(Base::size() == dim);
+    int i = 0;
+    for(auto x: initlist)
+    {
+        (*this)[i] = x;
+        ++i;
+    }
+}
+
+
+EIGEN_STRONG_INLINE Matrix(std::initializer_list<std::initializer_list<const Scalar> > initlist): Base()
+{
+    Base::_check_template_params();
+    int rows = initlist.size();
+    int cols = initlist.begin()->size();
+    eigen_assert(SizeAtCompileTime == Dynamic || (RowsAtCompileTime == rows && ColsAtCompileTime == cols));
+    if(Base::size() == 0) Base::resize(rows, cols);
+    eigen_assert(Base::rows() == rows && Base::cols() == cols);
+
+    int i = 0;
+    for(auto x: initlist)
+    {
+        eigen_assert(x.size() == cols);
+        int j = 0;
+        for(auto y: x)
+        {
+            (*this)(i, j) = y;
+            ++j;
+        }
+        ++i;
+    }
+}
+
+#endif

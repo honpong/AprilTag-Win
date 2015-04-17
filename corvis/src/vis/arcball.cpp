9edf8b1ec2c2683898757a84b479ada22d38958c
Brian Fulkerson <brian@realitycap.com>
2015-04-17 10:46:26 +0100

    Use new * operator for quaternions

--- corvis/src/vis/arcball.cpp
+++ corvis/src/vis/arcball.cpp
@@ -11,12 +11,12 @@ void arcball::rotate_quaternion_with_delta(float dx, float dy)
     v4 right(-1,0,0,0);
 
     // up and right now rotation_vectors
-    up = quaternion_rotate(conjugate(q), up)*dx*radians_per_pixel;
-    right = quaternion_rotate(conjugate(q), right)*dy*radians_per_pixel;
+    up = conjugate(q)*up*dx*radians_per_pixel;
+    right = conjugate(q)*right*dy*radians_per_pixel;
 
     quaternion dqx = to_quaternion(rotation_vector(up[0], up[1], up[2]));
     quaternion dqy = to_quaternion(rotation_vector(right[0], right[1], right[2]));
-    q = quaternion_product(q, quaternion_product(dqx, dqy));
+    q = q*dqx*dqy;
 }
 
 // Rotates by clicking and dragging
@@ -43,13 +43,13 @@ void arcball::start_view_rotation(float rotation)
 void arcball::continue_view_rotation(float rotation)
 {
     v4 axis(0, 0, -1, 0);
-    axis = quaternion_rotate(conjugate(q), axis);
+    axis = conjugate(q)*axis;
 
     float delta = rotation - last_view_rotation;
     // axis is now a rotation vector
     axis = axis*delta;
     quaternion delta_q = to_quaternion(rotation_vector(axis[0], axis[1], axis[2]));
-    q = quaternion_product(q, delta_q);
+    q = q*delta_q;
 
     last_view_rotation = rotation;
 }

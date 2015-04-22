b431446ab603717f846b8b873d5295dbd35e1d6e
Eagle Jones <eagle@seurat-win>
2015-04-22 16:38:51 -0700

    First attempt at glue for windows -> sensor_data

--- corvis/src/cor/platform/win/sensor_data.cpp
+++ corvis/src/cor/platform/win/sensor_data.cpp
@@ -7,11 +7,35 @@
 //
 
 #include "../sensor_data.h"
+#include "pxcsensemanager.h"
+#include "pxcmetadata.h"
+#include <stdexcept>
+#include <iostream>
 
-camera_data::camera_data(void *h): image_handle(h, [](void *h) {})
+typedef std::pair<PXCImage *, PXCImage::ImageData> handle_type;
+
+camera_data::camera_data(void *h) : image_handle(h, [](void *h) {})
 {
+    PXCImage *pxci = (PXCImage *)h;
+    PXCImage::ImageData data;
+    auto result = pxci->AcquireAccess(PXCImage::ACCESS_READ, PXCImage::PIXEL_FORMAT_Y8, &data);
+    if(result != PXC_STATUS_NO_ERROR) throw std::runtime_error("PXCImage->AcquireAccess failed!");
+    image_handle.reset(new handle_type(pxci, data)); //TODO avoid allocation here?
+ 
+    image = data.planes[0];
+    stride = data.pitches[0];
+    auto info = pxci->QueryInfo();
+    width = info.width;
+    height = info.height;
+    auto time = pxci->QueryTimeStamp();
+    std::cerr << "camera timestamp: " << time << "\n";
+    //TODO: timestamp
+    timestamp = sensor_clock::now();
 }
 
 camera_data::~camera_data()
 {
+    auto h = (handle_type *)image_handle.get();
+    h->first->ReleaseAccess(&(h->second));
+    delete h;
 }
--- windows/RCUtility/RCUtility/CaptureManager.cpp
+++ windows/RCUtility/RCUtility/CaptureManager.cpp
@@ -1,4 +1,8 @@
 #include "stdafx.h"
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include "CaptureManager.h"
 #include "Debug.h"
 #include <string>
@@ -64,18 +68,32 @@ void CaptureManager::StopCapture()
 
 void CaptureManager::OnAmeterSample(AccelerometerReading^ sample)
 {
-	cp.receive_accelerometer(accelerometer_data(sample));
+    accelerometer_data data;
+    //windows gives acceleration in g-units, so multiply by standard gravity in m/s^2
+    data.accel_m__s2[0] = sample->AccelerationX * 9.80665;
+    data.accel_m__s2[1] = sample->AccelerationY * 9.80665;
+    data.accel_m__s2[2] = sample->AccelerationZ * 9.80665;
+    data.timestamp = sensor_clock::now();
+    cp.receive_accelerometer(std::move(data));
 
-	long long millisec = sample->Timestamp.UniversalTime / 10000;
-	Debug::Log(L"%lld\taccel\tx: %1.3f\ty: %1.3f\tz: %1.3f", millisec, sample->AccelerationX, sample->AccelerationY, sample->AccelerationZ);
+    std::cerr << "accel timestamp: " << sample->Timestamp.UniversalTime << "\n";
+    //long long millisec = sample->Timestamp.UniversalTime / 10000;
+//	Debug::Log(L"%lld\taccel\tx: %1.3f\ty: %1.3f\tz: %1.3f", millisec, sample->AccelerationX, sample->AccelerationY, sample->AccelerationZ);
 }
 
 void CaptureManager::OnGyroSample(GyrometerReading^ sample)
 {
-	cp.receive_gyro(gyro_data(sample));
+    gyro_data data;
+    //windows gives angular velocity in degrees per second
+    data.angvel_rad__s[0] = sample->AngularVelocityX * M_PI / 180.;
+    data.angvel_rad__s[1] = sample->AngularVelocityY * M_PI / 180.;
+    data.angvel_rad__s[2] = sample->AngularVelocityZ * M_PI / 180.;
+    data.timestamp = sensor_clock::now();
+	cp.receive_gyro(std::move(data));
 
-	long long millisec = sample->Timestamp.UniversalTime / 10000;
-	Debug::Log(L"%lld\tgyro\tx: %3.3f\ty: %3.3f\tz: %3.3f", millisec, sample->AngularVelocityX, sample->AngularVelocityY, sample->AngularVelocityZ); // gyro data is in degrees/sec
+    std::cerr << "gyro timestamp: " << sample->Timestamp.UniversalTime << "\n";
+    //long long millisec = sample->Timestamp.UniversalTime / 10000;
+    //Debug::Log(L"%lld\tgyro\tx: %3.3f\ty: %3.3f\tz: %3.3f", millisec, sample->AngularVelocityX, sample->AngularVelocityY, sample->AngularVelocityZ); // gyro data is in degrees/sec
 }
 
 void CaptureManager::OnVideoFrame(PXCImage* colorSample)

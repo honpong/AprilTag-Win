6873e0fe3c8cb1d50196128bf2edfb857d6c0f4b
Brian Fulkerson <brian@realitycap.com>
2015-04-29 16:31:49 +0100

    Add rotation and zoom
    Rotate by clicking and dragging
    Zoom using either +/- or w/s

--- corvis/src/vis/gui.cpp
+++ corvis/src/vis/gui.cpp
@@ -12,19 +12,61 @@ gui * gui::static_gui;
 #include <GLUT/glut.h>
 #endif
 
+const static float initial_scale = 5;
+
 void gui::configure_view()
 {
+    float aspect = 1.f*width/height;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    // Note miny and maxy and minz and maxz are reversed to get the
-    // deisred viewpoint. TODO: set up an actual projection matrix
-    // here
-    glOrtho(-2, 2, 2, -2, 10, -10);
-
-    glGetFloatv(GL_PROJECTION_MATRIX, _modelViewProjectionMatrix);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    gluPerspective(65, aspect, 0.1f, 100.0f);
+    glTranslatef(0, 0, -scale);
+    rotation_vector r = to_rotation_vector(arc.get_quaternion());
+    //glScalef(scale, scale, scale);
+    v4 axis = v4(r.x(), r.y(), r.z(), 0);
+    float angle = 0;
+    if(axis.norm() != 0) {
+        angle = axis.norm()*180/M_PI;
+        axis = axis / axis.norm();
+    }
+    glRotatef(angle, axis[0], axis[1], axis[2]);
+    glGetFloatv(GL_MODELVIEW, _modelViewProjectionMatrix);
+}
+
+void gui::reshape(int w, int h)
+{
+    width = w;
+    height = h;
+    glViewport(0, 0, width, height);
+}
+
+void gui::mouse_move(int x, int y)
+{
+    if(is_rotating)
+        arc.continue_rotation(x, y);
+}
+
+void gui::mouse(int button, int state, int x, int y)
+{
+    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        arc.start_rotation(x, y);
+        is_rotating = true;
+    }
+    else if(button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+        is_rotating = false;
+    }
+}
+void gui::keyboard(unsigned char key, int x, int y)
+{
+    if(key == '0')
+        scale = initial_scale;
+    if(key == '+' || key == '=' || key == 'w')
+        scale *= 1/1.1;
+    if(key == '-' || key == 's')
+        scale *= 1.1;
 }
 
 void gui::init_gl()
@@ -66,23 +108,27 @@ void gui::init_glut()
     char ** argv = NULL;
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);   // display mode
-    glutInitWindowSize(512, 512);              // window size
-    glutInitWindowPosition(100, 100);                           // window location
+    glutInitWindowSize(width, height);              // window size
     window_id = glutCreateWindow("Replay");     // param is the title of window
     // Configure glut callbacks
     glutDisplayFunc(gui::render_callback);
     glutIdleFunc(gui::render_callback);
+    glutReshapeFunc(gui::reshape_callback);
+    glutMouseFunc(gui::mouse_callback);
+    glutMotionFunc(gui::move_callback);
+    glutKeyboardFunc(gui::keyboard_callback);
 }
 
 void gui::start()
 {
+    arc.reset();
     init_glut();
     init_gl();
     world_state_render_init();
     glutMainLoop();
 }
 
-gui::gui(world_state * world) : state(world)
+gui::gui(world_state * world) : state(world), scale(initial_scale), width(512), height(512)
 {
     static_gui = this;
 }
--- corvis/src/vis/gui.h
+++ corvis/src/vis/gui.h
@@ -2,12 +2,23 @@
 #define __CORVIS_GUI__
 
 #include "world_state.h"
+#include "arcball.h"
 
 class gui
 {
 private:
     static gui * static_gui;
     static void render_callback() { gui::static_gui->render(); };
+    static void reshape_callback(int w, int h) { gui::static_gui->reshape(w, h); };
+    static void mouse_callback(int button, int state, int x, int y) {
+        gui::static_gui->mouse(button, state, x, y);
+    };
+    static void move_callback(int x, int y) {
+        gui::static_gui->mouse_move(x, y);
+    };
+    static void keyboard_callback(unsigned char key, int x, int y) {
+        gui::static_gui->keyboard(key, x, y);
+    };
 
     float _modelViewProjectionMatrix[16]; // 4x4
     float _normalMatrix[9]; // 3x3
@@ -16,8 +27,19 @@ private:
     void init_glut();
     void init_gl();
     void configure_view();
+    void reshape(int width, int height);
+    void mouse(int button, int state, int x, int y);
+    void mouse_move(int x, int y);
+    void keyboard(unsigned char key, int x, int y);
     void render();
     world_state * state;
+
+    float scale;
+    int width, height;
+
+    // Mouse related
+    arcball arc;
+    bool is_rotating;
 
 public:
     gui(world_state * render_state);

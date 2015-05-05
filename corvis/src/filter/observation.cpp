9176268278e1ef2222b99104430a23d5c642ce1e
Jim Radford <jim@realitycap.com>
2015-05-04 18:32:58 -0700

    Fixup the size of the sub blocks of res_cov and LC in compute_prediction_covariance()

--- corvis/src/filter/observation.cpp
+++ corvis/src/filter/observation.cpp
@@ -59,7 +59,7 @@ void observation_queue::compute_prediction_covariance(const state &s, int meas_s
     int index = 0;
     for(auto &o : observations) {
         if(o->size) {
-            matrix dst(&LC(index, 0), o->size, statesize, statesize, LC.get_stride());
+            matrix dst(&LC(index, 0), o->size, statesize, meas_size-index, LC.get_stride());
             o->cache_jacobians();
             o->project_covariance(dst, s.cov.cov);
             index += o->size;
@@ -70,7 +70,7 @@ void observation_queue::compute_prediction_covariance(const state &s, int meas_s
     index = 0;
     for(auto &o : observations) {
         if(o->size) {
-            matrix dst(&res_cov(index, 0), o->size, meas_size, meas_size, res_cov.get_stride());
+            matrix dst(&res_cov(index, 0), o->size, meas_size, meas_size-index, res_cov.get_stride());
             o->project_covariance(dst, LC);
             index += o->size;
         }

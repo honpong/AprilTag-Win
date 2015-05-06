32c7477a2de18b0bd0a0b1fbeb9f60a952b428ef
Ben Hirashima <ben@realitycap.com>
2015-05-06 11:10:45 -0700

    added CalibrationSample project

---
+++ CalibrationSample/CalibrationSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalibrationSample
+//
+//  Created by Ben Hirashima on 5/6/15.
+//  Copyright (c) 2015 RealityCap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CalibrationSample/CalibrationSampleTests/CalibrationSampleTests.m
@@ -0,0 +1,40 @@
+//
+//  CalibrationSampleTests.m
+//  CalibrationSampleTests
+//
+//  Created by Ben Hirashima on 5/6/15.
+//  Copyright (c) 2015 RealityCap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CalibrationSampleTests : XCTestCase
+
+@end
+
+@implementation CalibrationSampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

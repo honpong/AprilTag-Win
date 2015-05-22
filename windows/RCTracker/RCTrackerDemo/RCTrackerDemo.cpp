0435187ea9c4a0181a2e6baef4ca61c46b5dcf1b
Ben Hirashima <ben@realitycap.com>
2015-05-22 13:50:59 -0700

    parse filename argument

--- windows/RCTracker/RCTrackerDemo/RCTrackerDemo.cpp
+++ windows/RCTracker/RCTrackerDemo/RCTrackerDemo.cpp
@@ -2,12 +2,21 @@
 //
 
 #include "stdafx.h"
-#include <filter/replay.h>
+#include <iostream>
+#include <filter/rc_intel_interface.h>
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    const _TCHAR* fileName = nullptr;
+    if (argc > 1) fileName = (const _TCHAR*)argv[1];
+
+    if (fileName)
+    {
+        wcout << "Filename: " << fileName << endl;
+    }
+
     cout << "Press return to exit." << endl;
     cin.get();
 	return 0;

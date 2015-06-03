e226cc14a60edc036d590bce55ae04d97ebf0944
Ben Hirashima <ben@realitycap.com>
2015-06-02 19:54:56 -0700

    rc_intel_interface_tests passing

--- corvis/test/filter/rc_intel_interface_tests.cpp
+++ corvis/test/filter/rc_intel_interface_tests.cpp
@@ -2,8 +2,10 @@
 #include "gtest/gtest.h"
 #include "rc_intel_interface.h"
 #include <memory>
+#include "calibration_json_store.h"
 
 using namespace std;
+using namespace RealityCap;
 
 TEST(rc_intel_interface_tests, rc_getCalibration)
 {
@@ -11,6 +13,17 @@ TEST(rc_intel_interface_tests, rc_getCalibration)
     const wchar_t* buffer;
     size_t size = rc_getCalibration(tracker, &buffer);
     EXPECT_TRUE(size);
+}
 
-    EXPECT_TRUE(rc_setCalibration(tracker, buffer));
+TEST(rc_intel_interface_tests, rc_setCalibration)
+{
+    unique_ptr<calibration_data_store> store = calibration_data_store::GetStore();
+    corvis_device_parameters cal;
+    EXPECT_TRUE(store->LoadCalibrationDefaults(DEVICE_TYPE_GIGABYTE_S11, cal));
+
+    wstring jsonString;
+    EXPECT_TRUE(calibration_json_store::SerializeCalibration(cal, jsonString));
+
+    rc_Tracker *tracker = rc_create();
+    EXPECT_TRUE(rc_setCalibration(tracker, jsonString.c_str()));
 }

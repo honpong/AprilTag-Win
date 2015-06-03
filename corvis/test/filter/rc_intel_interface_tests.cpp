286bf7471bb42c5f7a845a91f5efceeb88e1bbb3
Ben Hirashima <ben@realitycap.com>
2015-06-02 17:26:45 -0700

    added rc_intel_interface_tests.cpp to spec, via cmake

---
+++ corvis/test/filter/rc_intel_interface_tests.cpp
@@ -0,0 +1,14 @@
+
+#include "gtest/gtest.h"
+#include "rc_intel_interface.h"
+#include <memory>
+
+using namespace std;
+
+TEST(rc_intel_interface_tests, rc_getCalibration)
+{
+    rc_Tracker *tracker = rc_create();
+    wchar_t* buffer;
+    size_t size = rc_getCalibration(tracker, &buffer);
+    EXPECT_TRUE(size);
+}

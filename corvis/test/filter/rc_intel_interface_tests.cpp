d3e49c775927b593101255f8cd4ea2f2e18beabc
Ben Hirashima <ben@realitycap.com>
2015-06-02 21:50:23 -0700

    don't compare json strings at this time

--- corvis/test/filter/rc_intel_interface_tests.cpp
+++ corvis/test/filter/rc_intel_interface_tests.cpp
@@ -7,23 +7,25 @@
 using namespace std;
 using namespace RealityCap;
 
-TEST(rc_intel_interface_tests, rc_getCalibration)
-{
-    rc_Tracker *tracker = rc_create();
-    const wchar_t* buffer;
-    size_t size = rc_getCalibration(tracker, &buffer);
-    EXPECT_TRUE(size);
-}
-
 TEST(rc_intel_interface_tests, rc_setCalibration)
 {
+    // load default cal
     unique_ptr<calibration_data_store> store = calibration_data_store::GetStore();
     corvis_device_parameters cal;
     EXPECT_TRUE(store->LoadCalibrationDefaults(DEVICE_TYPE_GIGABYTE_S11, cal));
 
+    // set cal
     wstring jsonString;
     EXPECT_TRUE(calibration_json_store::SerializeCalibration(cal, jsonString));
 
     rc_Tracker *tracker = rc_create();
     EXPECT_TRUE(rc_setCalibration(tracker, jsonString.c_str()));
+    
+    // now read cal back out and compare
+    const wchar_t* buffer;
+    size_t size = rc_getCalibration(tracker, &buffer);
+    EXPECT_TRUE(size);
+
+    // this doesn't work because not all all fields are being extracted from filter at the moment.
+    //EXPECT_STREQ(jsonString.c_str(), buffer);
 }

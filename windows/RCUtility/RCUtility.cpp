e492edd7d02229d748f78a9b5d772f442abc103c
Ben Hirashima <ben@realitycap.com>
2015-06-05 10:06:20 -0700

    moved file picker code into it's own file

--- windows/RCUtility/RCUtility.cpp
+++ windows/RCUtility/RCUtility.cpp
@@ -8,11 +8,11 @@
 #include <shellapi.h>
 #include "TrackerManager.h"
 #include <shlobj.h>
-#include <shlwapi.h>
 #include "RCFactory.h"
 #include <wchar.h>
 #include "visualization.h"
 #include "render_data.h"
+#include "FilePicker.h"
 
 using namespace RealityCap;
 
@@ -262,58 +262,6 @@ void ExitReplayingState()
     SetWindowText(hLabel, TEXT("Replay stopped."));
     appState = Idle;
 }
-
-
-///////////////////////////////////////// File Picker ///////////////////////////////////////////
-
-class CDialogEventHandler : public IFileDialogEvents, public IFileDialogControlEvents
-{
-public:
-    // IUnknown methods
-    IFACEMETHODIMP QueryInterface(REFIID riid, void** ppv)
-    {
-        static const QITAB qit[] = {
-            QITABENT(CDialogEventHandler, IFileDialogEvents),
-            QITABENT(CDialogEventHandler, IFileDialogControlEvents),
-            { 0 },
-        };
-        return QISearch(this, qit, riid, ppv);
-    }
-
-    IFACEMETHODIMP_(ULONG) AddRef()
-    {
-        return InterlockedIncrement(&_cRef);
-    }
-
-    IFACEMETHODIMP_(ULONG) Release()
-    {
-        long cRef = InterlockedDecrement(&_cRef);
-        if (!cRef)
-            delete this;
-        return cRef;
-    }
-
-    // IFileDialogEvents methods
-    IFACEMETHODIMP OnFileOk(IFileDialog *) { return S_OK; };
-    IFACEMETHODIMP OnFolderChange(IFileDialog *) { return S_OK; };
-    IFACEMETHODIMP OnFolderChanging(IFileDialog *, IShellItem *) { return S_OK; };
-    IFACEMETHODIMP OnHelp(IFileDialog *) { return S_OK; };
-    IFACEMETHODIMP OnSelectionChange(IFileDialog *) { return S_OK; };
-    IFACEMETHODIMP OnShareViolation(IFileDialog *, IShellItem *, FDE_SHAREVIOLATION_RESPONSE *) { return S_OK; };
-    IFACEMETHODIMP OnTypeChange(IFileDialog *pfd) { return S_OK; };
-    IFACEMETHODIMP OnOverwrite(IFileDialog *, IShellItem *, FDE_OVERWRITE_RESPONSE *) { return S_OK; };
-
-    // IFileDialogControlEvents methods
-    IFACEMETHODIMP OnItemSelected(IFileDialogCustomize *pfdc, DWORD dwIDCtl, DWORD dwIDItem) { return S_OK; };
-    IFACEMETHODIMP OnButtonClicked(IFileDialogCustomize *, DWORD) { return S_OK; };
-    IFACEMETHODIMP OnCheckButtonToggled(IFileDialogCustomize *, DWORD, BOOL) { return S_OK; };
-    IFACEMETHODIMP OnControlActivating(IFileDialogCustomize *, DWORD) { return S_OK; };
-
-    CDialogEventHandler() : _cRef(1) { };
-private:
-    ~CDialogEventHandler() { };
-    long _cRef;
-};
 
 HRESULT CDialogEventHandler_CreateInstance(REFIID riid, void **ppv)
 {

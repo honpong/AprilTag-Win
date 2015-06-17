a54ef50739711dc7f12e0d6997a13b5d83b5d9d7
Jim Radford <jim@realitycap.com>
2015-06-17 15:28:52 -0700

    benchmark: robustly get the config name from the directory name

--- corvis/scripts/benchmark.py
+++ corvis/scripts/benchmark.py
@@ -13,7 +13,7 @@ def scan_tests(folder_name):
     configurations = []
     for dirname, dirnames, filenames in os.walk(folder_name, followlinks=True):
         for filename in filenames:
-            config_name = os.path.basename(dirname)
+            config_name = os.path.basename(os.path.dirname(os.path.join(dirname, filename)))
             L_match = re.search("_L([\d.]+)", filename)
             PL_match = re.search("_PL([\d.]+)", filename)
             L = None

6a467da7ccc12bd06aff64025f2e21fc9b342a0b
Eagle Jones <eagle@seurat>
2015-06-20 15:53:27 -0700

    batch file to convert rssdk clips to realitycap format

---
+++ windows/src/RCTrackerDemo/convert_clip.bat
@@ -0,0 +1,11 @@
+@echo off
+if "%2"=="" goto instr
+for %%F in (%1\*.rssdk) do (
+	echo %%~nF
+	RCTrackerDemo %%~dpnxF %2\%%~nF
+)
+goto end
+:instr
+echo Usage - "convert_clip <dir1> <dir2>"
+echo Converts all rssdk clips in dir1 to RC format clips in dir2
+:end

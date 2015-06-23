5ae7568adfe002d1c013d378550553eb7a411681
Brian Fulkerson <brian@realitycap.com>
2015-06-23 11:47:38 +0100

    Expose detector and tracker parameters in tracker struct

--- corvis/src/filter/tracker.h
+++ corvis/src/filter/tracker.h
@@ -14,6 +14,8 @@ struct tracker {
     int stride;
     const static int half_patch_width = 3;
     fast_detector_9 fast;
+    int track_threshold = 10;
+    int detect_threshold = 40;
     
     void init()
     {
@@ -22,14 +24,12 @@ struct tracker {
     
     xy track(const uint8_t * im1, const uint8_t * im2, float predx, float predy, float radius, float min_score)
     {
-        int bthresh = 10;
-        return fast.track(im1, im2, half_patch_width, half_patch_width, predx, predy, radius, bthresh, min_score);
+        return fast.track(im1, im2, half_patch_width, half_patch_width, predx, predy, radius, track_threshold, min_score);
     }
     
     vector<xy> &detect(const unsigned char *im, const scaled_mask *mask, int number_wanted, int winx, int winy, int winwidth, int winheight)
     {
-        int bthresh = 40;
-        return fast.detect(im, mask, number_wanted, bthresh, winx, winy, winwidth, winheight);
+        return fast.detect(im, mask, number_wanted, detect_threshold, winx, winy, winwidth, winheight);
     }
 };
 

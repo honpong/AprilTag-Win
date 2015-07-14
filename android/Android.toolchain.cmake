5cbaf6e754ae02e4f60267571d949140c2ee4049
Jim Radford <jim@realitycap.com>
2015-07-14 07:26:18 -0700

    Android.toolchain: Fix the handling of ANDROID_ARCH and hence 64bit support
    Use and cache ANDROID_* variables at the top level to compute the output
    CMAKE_* variables which then get passed on the command line (importantly
    without any caching) to the sub-cmake's created by the try_compile()'s
    which autodetect compiler features.

--- android/Android.toolchain.cmake
+++ android/Android.toolchain.cmake
@@ -2,21 +2,18 @@ cmake_minimum_required(VERSION 3.2.2)
 
 set(CMAKE_SYSTEM_NAME Linux)
 
-set(ANDROID_NDK "${ANDROID_NDK}" CACHE PATH "Android NDK Path")
+if (NOT CMAKE_C_COMPILER_EXTERNAL_TOOLCHAIN AND NOT CMAKE_C_COMPILER_TARGET) # When called by try_compile these are passed in from below
+  set(ANDROID_NDK "${ANDROID_NDK}" CACHE PATH "Android NDK Path")
+  set(ANDROID_PLATFORM "android-21" CACHE STRING "Android Platform: 21, 20, ...")
+  set(ANDROID_ARCH "${ANDROID_ARCH}" CACHE STRING "Android Architecture: x86, x86_64, arm64, ...")
+  message("looking for ${ANDROID_PLATFORM} on ${ANDROID_ARCH} in ${ANDROID_NDK}")
+  if(ANDROID_ARCH STREQUAL "x86")
+    set(CMAKE_SYSTEM_PROCESSOR i686)
+  else()
+    set(CMAKE_SYSTEM_PROCESSOR ${ANDROID_ARCH})
+  endif()
+  set(ANDROID_TOOLCHAIN_MACHINE_NAME "${CMAKE_SYSTEM_PROCESSOR}-linux-android" CACHE INTERNAL "Toolchain target")
 
-set(ANDROID_PLATFORM "android-21" CACHE STRING "Android Platform: 21, 20, ...")
-set(ANDROID_ARCH "x86" CACHE STRING "Android Architecture: x86, x86_64, arm64, ...")
-if(ANDROID_ARCH STREQUAL "x86")
-  set(CMAKE_SYSTEM_PROCESSOR i686)
-else()
-  set(CMAKE_SYSTEM_PROCESSOR ${ANDROID_ARCH})
-endif()
-
-set(ANDROID_TOOLCHAIN_MACHINE_NAME "${CMAKE_SYSTEM_PROCESSOR}-linux-android" CACHE INTERNAL "Toolchain target")
-
-if (EXISTS "${CMAKE_BINARY_DIR}/../../toolchain/${ANDROID_TOOLCHAIN_MACHINE_NAME}")
-  set(ANDROID_TOOLCHAIN_ROOT "${CMAKE_BINARY_DIR}/../../toolchain" CACHE PATH "Toolchain root") # Compiler detection is run in ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp
-else()
   set(ANDROID_TOOLCHAIN_ROOT "${CMAKE_BINARY_DIR}/toolchain" CACHE PATH "Toolchain root")
   if (NOT EXISTS "${ANDROID_TOOLCHAIN_ROOT}/${ANDROID_TOOLCHAIN_MACHINE_NAME}")
     file(REMOVE_RECURSE "${ANDROID_TOOLCHAIN_ROOT}")
@@ -27,12 +24,16 @@ else()
       message(ERROR "failed to build the toolchain: ${ANDROID_TOOLCHAIN_ROOT}/${ANDROID_TOOLCHAIN_MACHINE_NAME}")
     endif()
   endif()
+
+  set(CMAKE_C_COMPILER   "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_MACHINE_NAME}-clang"   CACHE PATH "C")
+  set(CMAKE_CXX_COMPILER "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_MACHINE_NAME}-clang++" CACHE PATH "C++")
+  foreach(__lang C CXX)
+    set(CMAKE_${__lang}_COMPILER_EXTERNAL_TOOLCHAIN "${ANDROID_TOOLCHAIN_ROOT}")
+    set(CMAKE_${__lang}_SYSROOT_FLAG "-isysroot")
+    set(CMAKE_${__lang}_COMPILER_TARGET "${ANDROID_TOOLCHAIN_MACHINE_NAME}")
+  endforeach(__lang)
+  set(CMAKE_SYSROOT "${ANDROID_TOOLCHAIN_ROOT}/sysroot")
 endif()
-
-set(CMAKE_FIND_ROOT_PATH "${ANDROID_STANDALONE_TOOLCHAIN}/sysroot" "${CMAKE_INSTALL_PREFIX}" "${CMAKE_INSTALL_PREFIX}/share")
-
-set(CMAKE_C_COMPILER   "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_MACHINE_NAME}-clang"   CACHE PATH "C")
-set(CMAKE_CXX_COMPILER "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_MACHINE_NAME}-clang++" CACHE PATH "C++")
 
 set(ANDROID True)
 add_definitions(-DANDROID)

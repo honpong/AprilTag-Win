aed7c984ac995ca38e54c7f6bcc38e302fa2351b
Jim Radford <jim@realitycap.com>
2015-07-14 14:27:00 -0700

    android: enable linking with the rc_intel_interface via libtracker.so

--- android/RCUtility/app/src/main/jni/sensor-fusion.cpp
+++ android/RCUtility/app/src/main/jni/sensor-fusion.cpp
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <jni.h>
 #include <android/log.h>
+#include "rc_intel_interface.h"
 
 #define TAG "RCUtility"
 #define LOGV(...) ((void)__android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__))

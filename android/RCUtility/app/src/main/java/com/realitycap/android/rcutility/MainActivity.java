0fcc4243a16a7baf65097b5b627abe7ef6c55b31
Eagle Jones <eagle@seurat>
2015-07-21 11:12:31 -0700

    If something fails in startCalibration, return rather than keep going.

--- android/RCUtility/app/src/main/java/com/realitycap/android/rcutility/MainActivity.java
+++ android/RCUtility/app/src/main/java/com/realitycap/android/rcutility/MainActivity.java
@@ -136,21 +136,29 @@ public class MainActivity extends Activity implements ITrackerReceiver
 		if (appState != AppState.Idle) return false;
         setStatusText("Starting calibration...");
 
-        if (!startSensors()) cancelCalibration();
+        if (!startSensors())
+        {
+            cancelCalibration();
+            return false;
+        }
 
         Camera.Calibration.Intrinsics intr = rsMan.getCameraIntrinsics();
-        if (intr == null) cancelCalibration();
+        if (intr == null)
+        {
+            cancelCalibration();
+            return false;
+        }
         trackerProxy.configureCamera(0, 640, 480, intr.principalPoint.x, intr.principalPoint.y, intr.focalLength.x, intr.focalLength.y, 0, false, 0);
 
-        boolean result = trackerProxy.startCalibration();
-		if (result)
+        if(!trackerProxy.startCalibration())
         {
-            appState = AppState.Calibrating;
-            setStatusText("Calibrating");
+            cancelCalibration();
+            return false;
         }
-        else cancelCalibration();
 
-		return result;
+        appState = AppState.Calibrating;
+        setStatusText("Calibrating");
+        return true;
 	}
 
     private void cancelCalibration()

719d3254fdc3f24fcf85bda9cef2edb917c9bacf
Ben Hirashima <ben@realitycap.com>
2015-08-07 16:19:02 -0700

    live view starts successfully

--- android/RCUtility/app/src/main/java/com/realitycap/android/rcutility/MainActivity.java
+++ android/RCUtility/app/src/main/java/com/realitycap/android/rcutility/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,12 +15,13 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
 import android.widget.ToggleButton;
-import android.os.Handler;
 
 import com.intel.camera.toolkit.depth.Camera;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -70,7 +72,7 @@ public class MainActivity extends Activity implements ITrackerReceiver
                 }
                 else
                 {
-                    stopCalibration();
+                    cancelCalibration();
                 }
             }
         });
@@ -180,7 +182,7 @@ public class MainActivity extends Activity implements ITrackerReceiver
             return cancelTracking("Failed to get camera intrinsics.");
         }
 
-        if (!setCalibrationFromFile(DEFAULT_CALIBRATION_FILENAME))
+        if (!setDefaultCalibrationFromFile(DEFAULT_CALIBRATION_FILENAME))
         {
             return cancelTracking("Failed to load default calibration file.");
         }
@@ -195,6 +197,20 @@ public class MainActivity extends Activity implements ITrackerReceiver
         return true;
     }
 
+    protected void cancelCalibration()
+    {
+        if (appState != AppState.Calibrating) return;
+
+        stopSensors();
+        trackerProxy.stopTracker();
+        trackerProxy.destroyTracker();
+
+        setStatusText("Calibration stopped.");
+
+        calibrationButton.setChecked(false);
+        appState = AppState.Idle;
+    }
+
     protected void stopCalibration()
     {
         if (appState != AppState.Calibrating) return;
@@ -206,16 +222,17 @@ public class MainActivity extends Activity implements ITrackerReceiver
         if (cal != null)
         {
             prefs.edit().putString(PREF_KEY_CALIBRATION, cal).apply();
-            success = writeTextToFile(cal, "calibration.json");
+            success = writeTextToFileOnSDCard(cal, "calibration.json");
         }
         else success = false;
 
         trackerProxy.stopTracker();
         trackerProxy.destroyTracker();
 
-        if (success) setStatusText("Calibration stopped.");
-        else setStatusText("Calibration not saved");
+        if (success) setStatusText("Calibration successful.");
+        else setStatusText("Calibration not saved.");
 
+        calibrationButton.setChecked(false);
         appState = AppState.Idle;
     }
 
@@ -298,11 +315,18 @@ public class MainActivity extends Activity implements ITrackerReceiver
         return false;
     }
 
-    protected boolean setCalibrationFromFile(String fileName)
+    protected boolean setDefaultCalibrationFromFile(String fileName)
     {
-        final String defaultCal = readTextFromFile(fileName);
+        final String defaultCal = readTextFromFileInAssets(fileName);
         if (defaultCal == null || defaultCal.length() <= 0) return false;
         return trackerProxy.setCalibration(defaultCal);
+    }
+
+    protected boolean setCalibrationFromFile(String fileName)
+    {
+        final String cal = readTextFromFileOnSDCard(fileName);
+        if (cal == null || cal.length() <= 0) return false;
+        return trackerProxy.setCalibration(cal);
     }
 
     protected boolean configureCamera()
@@ -318,13 +342,43 @@ public class MainActivity extends Activity implements ITrackerReceiver
         return true;
     }
 
-    protected String readTextFromFile(String filename)
+    protected String readTextFromFileInAssets(String filename)
+    {
+        InputStream is;
+        try
+        {
+            is = getAssets().open(filename);
+        }
+        catch (IOException e)
+        {
+            Log.e(MyApplication.TAG, e.getLocalizedMessage());
+            return null;
+        }
+        return readTextFromInputStream(is);
+    }
+
+    protected String readTextFromFileOnSDCard(String filename)
+    {
+        File file = new File(Environment.getExternalStorageDirectory(), filename);
+        FileInputStream is;
+        try
+        {
+            is = new FileInputStream(file);
+        }
+        catch (FileNotFoundException e)
+        {
+            Log.e(MyApplication.TAG, e.getLocalizedMessage());
+            return null;
+        }
+        return readTextFromInputStream(is);
+    }
+
+    protected String readTextFromInputStream(InputStream is)
     {
         BufferedReader in = null;
         try
         {
             StringBuilder buf = new StringBuilder();
-            InputStream is = getAssets().open(filename);
             in = new BufferedReader(new InputStreamReader(is));
 
             String str;
@@ -356,7 +410,7 @@ public class MainActivity extends Activity implements ITrackerReceiver
         return null;
     }
 
-    protected boolean writeTextToFile(String text, String filename)
+    protected boolean writeTextToFileOnSDCard(String text, String filename)
     {
         FileOutputStream outputStream;
         File file = new File(Environment.getExternalStorageDirectory(), filename);
@@ -400,6 +454,7 @@ public class MainActivity extends Activity implements ITrackerReceiver
                     {
                         case 0: // idle
                             stopCalibration();
+                            setStatusText("Calibration successful.");
                             break;
                         case 1: // static calibration
                             if (progress <= 1.) setStatusText("Place device on a flat surface. Progress: " + percentage + "%");
@@ -407,7 +462,7 @@ public class MainActivity extends Activity implements ITrackerReceiver
                         case 5: // portrait calibration
                             if (progress <= 1.) setStatusText("Hold steady in portrait orientation. Progress: " + percentage + "%");
                             break;
-                        case 6: // landscape calibraiton
+                        case 6: // landscape calibration
                             if (progress <= 1.) setStatusText("Hold steady in landscape orientation. Progress: " + percentage + "%");
                             break;
                         default:

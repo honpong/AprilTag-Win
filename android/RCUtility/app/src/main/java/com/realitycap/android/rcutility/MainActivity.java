f8b87e92e123b147b5d7d52686946ae6dc4b5586
Ben Hirashima <ben@realitycap.com>
2015-08-25 11:26:58 -0700

    removed unused menu from main activity

--- android/RCUtility/app/src/main/java/com/realitycap/android/rcutility/MainActivity.java
+++ android/RCUtility/app/src/main/java/com/realitycap/android/rcutility/MainActivity.java
@@ -203,6 +203,7 @@ public class MainActivity extends TrackerActivity
     protected void exitCaptureState()
     {
         if (appState != AppState.Capturing) return;
+        setStatusText("Stopping capture...");
         stopSensors();
         trackerProxy.destroyTracker();
         setStatusText("Capture stopped.");
@@ -311,28 +312,6 @@ public class MainActivity extends TrackerActivity
             startReplayActivity(data.getData());
         }
         else super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void log(String line)

4fcaf78fd8afe40305761123e8393e9b8c4d5cf3
Jim Radford <jim@realitycap.com>
2015-09-08 13:57:07 -0700

    rc_replay: support running multiple files (mostly useful to rest reset)

--- corvis/src/tools/rc_replay.cpp
+++ corvis/src/tools/rc_replay.cpp
@@ -7,30 +7,36 @@
 int main(int c, char **v)
 {
     if (0) { usage:
-        std::cerr << "Usage: " << v[0] << " [--drop-depth] [--output-poses] [--output-status] [--output-log] <logfile>\n";
+        std::cerr << "Usage: " << v[0] << " [--drop-depth] [--output-poses] [--output-status] [--output-log] <logfile>..\n";
         return 1;
     }
 
     rc::replay rp;
 
-    char *filename = nullptr;
+    int filenames = 0;
     for (int i=1; i<c; i++)
-        if      (v[i][0] != '-' && !filename) filename = v[i];
+        if      (v[i][0] != '-') v[1+filenames++] = v[i];
         else if (strcmp(v[i], "--drop-depth") == 0) rp.disable_depth();
         else if (strcmp(v[i], "--output-poses") == 0) rp.enable_pose_output();
         else if (strcmp(v[i], "--output-status") == 0) rp.enable_status_output();
         else if (strcmp(v[i], "--output-log") == 0) rp.enable_log_output(0, 333333);
         else goto usage;
 
-    if (!filename)
+    if (!filenames)
         goto usage;
 
-    if (!rp.open(filename) || !rp.run()) {
-        std::cerr << "error replaying " << filename << "\n";
-        return 1;
+    for (int i=0; i<filenames; i++) {
+        char *filename = v[1+i];
+        if (i) rp.reset();
+
+        if (!rp.open(filename) || !rp.run()) {
+            std::cerr << "error replaying " << filename << "\n";
+            return 1;
+        }
+
+        printf("Reference Straight-line length is %.2f cm, total path length %.2f cm\n", 100*rp.get_reference_length(), NAN);
+        printf("Computed  Straight-line length is %.2f cm, total path length %.2f cm\n", 100*rp.get_length(), NAN);
     }
 
-    printf("Reference Straight-line length is %.2f cm, total path length %.2f cm\n", 100*rp.get_reference_length(), NAN);
-    printf("Computed  Straight-line length is %.2f cm, total path length %.2f cm\n", 100*rp.get_length(), NAN);
     return 0;
 }

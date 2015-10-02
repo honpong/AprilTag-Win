84962c4271b3004b9af24f9eeff7313518a7f8af
Ben Hirashima <ben.hirashima@intel.com>
2015-10-02 12:54:10 -0700

    removed unused argument

--- corvis/src/cor/sensor_fusion_queue.h
+++ corvis/src/cor/sensor_fusion_queue.h
@@ -108,7 +108,7 @@ public:
     
 private:
     void runloop();
-    bool run_control(const std::unique_lock<std::mutex> &lock);
+    bool run_control();
     bool ok_to_dispatch(sensor_clock::time_point time);
     bool dispatch_next(std::unique_lock<std::mutex> &lock, bool force);
     void dispatch_singlethread(bool force);

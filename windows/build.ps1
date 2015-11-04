2f03f56c8fbf6ce6f2cff6b5e0dd25a55a028dfd
Jim Radford <jim.radford@intel.com>
2015-11-04 10:04:24 -0800

    build.ps1: build Release on Windows by default

--- windows/build.ps1
+++ windows/build.ps1
@@ -5,5 +5,5 @@ $dir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
 if (!(Test-Path -Path "$dir\build32")) { cd "$dir"; mkdir build32; cd build32; cmake -G "Visual Studio 14 2015"       .. } else { echo "build32 already setup" }
 if (!(Test-Path -Path "$dir\build64")) { cd "$dir"; mkdir build64; cd build64; cmake -G "Visual Studio 14 2015 Win64" .. } else { echo "build64 already setup" }
 
-cmake --build "$dir\build32"
-cmake --build "$dir\build64"
+cmake --build "$dir\build32" --config Release
+cmake --build "$dir\build64" --config Release

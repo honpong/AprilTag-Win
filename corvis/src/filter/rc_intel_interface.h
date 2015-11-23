fd7ab7bc10d7e3204eb1039ace0eda9240d53740
Eagle S Jones <esjones@mfahimal-mobl.amr.corp.intel.com>
2015-11-23 14:51:30 -0800

    Change RealityCap copyright to Intel

--- corvis/src/filter/rc_intel_interface.h
+++ corvis/src/filter/rc_intel_interface.h
@@ -2,7 +2,7 @@
 //  rc_intel_interface.h
 //
 //  Created by Eagle Jones on 1/29/15.
-//  Copyright (c) 2015 Realitycap. All rights reserved.
+//  Copyright (c) 2015 Intel. All rights reserved.
 //
 
 #ifndef rc_intel_interface_h

d3bf8baaa5538f9b451b1222df8b1aefd27e16dc
Ben Hirashima <ben.hirashima@intel.com>
2015-12-04 16:07:22 -0800

    tracking working correctly. taps not registering.

--- android/BlockStacking/src/main/assets/main_controller.js
+++ android/BlockStacking/src/main/assets/main_controller.js
@@ -7,8 +7,6 @@
 ;
 var MainController = (function ($, window, RealSense, THREE)
 {
-
-                      
     "use strict";
 
     var module = {};
@@ -17,9 +15,8 @@ var MainController = (function ($, window, RealSense, THREE)
     {
         STARTUP:        0,
         READY:          1,
-        INITIALIZING:   2,
-        AUGMENTED:      3,
-        ERROR:          4
+        TRACKING:       2,
+        ERROR:          3
     };
 
     var currentRunState = Tracker.SensorFusionRunState.Inactive;
@@ -39,52 +36,30 @@ var MainController = (function ($, window, RealSense, THREE)
         enterReadyState();
         FastClick.attach(document.body);
 
-        $("#shutterButton").on( "click", function() {
-            showMessage("Click");
-            switch (workflowState)
-            {
-                case WorkflowStates.READY:
-                    enterAugmentedState();
-                    break;
-
-                case WorkflowStates.AUGMENTED:
-                    enterReadyState();
-                    break;
-
-                case WorkflowStates.ERROR:
-                    enterReadyState();
-                    break;
-            }
-        });
-
         Tracker.onStatusUpdate(function (trackingStatus, fps)
         {
-            if (workflowState != WorkflowStates.AUGMENTED && trackingStatus != "FAILED") workflowState = WorkflowStates.AUGMENTED;
+            if (workflowState !== WorkflowStates.TRACKING && trackingStatus !== "FAILED")
+                workflowState = WorkflowStates.TRACKING;
 
-            if (workflowState == WorkflowStates.AUGMENTED)
+            if (workflowState === WorkflowStates.TRACKING)
             {
-                showMessage(trackingStatus + ", " + fps + " FPS");
+                showMessage(trackingStatus + " - " + fps + " FPS");
             }
         });
 
         Tracker.onPoseUpdate(function (projMatrix, camMatrix)
         {
-            if (workflowState == WorkflowStates.AUGMENTED)
+            if (workflowState === WorkflowStates.TRACKING)
             {
                 var projectionMatrix4 = matrix4FromPlainObject(projMatrix);
                 var cameraMatrix4 = matrix4FromPlainObject(camMatrix);
-//                var cameraMatrix4 = new THREE.Matrix4();
-//                cameraMatrix4.identity();
                 updateWebGLView(projectionMatrix4, cameraMatrix4);
             }
         });
 
         Tracker.onSceneQualityUpdate(function (quality)
         {
-            if (workflowState == WorkflowStates.READY)
-            {
-                showMessage("Scene quality: " + quality);
-            }
+            showMessage("Scene quality: " + quality);
         });
     });
 
@@ -103,53 +78,26 @@ var MainController = (function ($, window, RealSense, THREE)
     function enterReadyState()
     {
         showMessage("Press the button to start.");
-
         setupWebGLView();
-
         workflowState = WorkflowStates.READY;
     }
 
     function enterAugmentedState()
     {
         showMessage("Augmented reality!");
-        workflowState = WorkflowStates.AUGMENTED;
+        workflowState = WorkflowStates.TRACKING;
     }
 
     function enterErrorState(message)
     {
         if (!message) message = "Whoops, something went wrong.";
         showMessage(message);
-//        Tracker.stopSensorFusion();
         workflowState = WorkflowStates.ERROR;
     }
 
     function showMessage(message)
     {
         $("#message").html(message);
-    }
-
-    function handleSensorFusionError(error)
-    {
-        if (error.code > 1)
-        {
-            enterErrorState(error.class + ": " + error.code);
-        }
-    }
-
-    function handleLicenseError(error)
-    {
-        enterErrorState(error.class + ": " + error.code);
-    }
-
-var cube;
-
-    function render()
-    {
-        requestAnimationFrame(render);
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
-        renderer.render(scene, camera);
-        renderer.setClearColorHex( 0xBBBBBB, 1 );
     }
 
     function setupWebGLView()
@@ -161,151 +109,122 @@ var cube;
         camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
         var canvas = document.getElementById("webGLCanvas");
-//        renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
         renderer = new THREE.WebGLRenderer({ canvas: canvas });
         renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.setClearColor( 0xffffff, 1 );
 
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        var material = new THREE.MeshLambertMaterial( { color: 0x0000ff } );
-        cube = new THREE.Mesh( geometry, material );
-        cube.position.z = -5;
-        scene.add( cube );
+        // roll-over helpers
 
-        var light = new THREE.PointLight( 0xFFFFFF, 1, 100000 );
-        light.position.set( 10, 0, 10 );
+        rollOverGeo = new THREE.BoxGeometry( 0.05, 0.05, 0.05);
+        rollOverMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
+        rollOverMesh = new THREE.Mesh( rollOverGeo, rollOverMaterial );
+        rollOverMesh.position.set(0, 0.5, -0.5);
+        scene.add( rollOverMesh );
+
+        // cubes
+
+        cubeGeo = new THREE.BoxGeometry( 0.05, 0.05, 0.05);
+        cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xfeb74c, shading: THREE.FlatShading, map: THREE.ImageUtils.loadTexture( "square-outline-textured.png" ) } );
+
+        // grid
+
+        var size = .5, step = 0.05;
+
+        var geometry = new THREE.Geometry();
+
+        for ( var i = - size; i <= size; i += step ) {
+
+            geometry.vertices.push( new THREE.Vector3( - size, i, 0) );
+            geometry.vertices.push( new THREE.Vector3(   size, i, 0) );
+
+            geometry.vertices.push( new THREE.Vector3( i, - size, 0) );
+            geometry.vertices.push( new THREE.Vector3( i, size, 0) );
+
+        }
+
+        var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
+
+        var line = new THREE.Line( geometry, material, THREE.LinePieces );
+        line.position.set(0,0.5,-0.5);
+        scene.add( line );
+
+        //
+
+        raycaster = new THREE.Raycaster();
+        //because we're using meter grids, and many objects are sub meter, we need to increase the resolution of the raycaster
+        raycaster.near = 0.01; //assume we're at least a centameter off the object
+        raycaster.far = 500000;
+        raycaster.precision = 0.0000000000001;
+        raycaster.linePrecision = 0.0000000000001;
+
+        mouse = new THREE.Vector2();
+
+        var geometry = new THREE.PlaneBufferGeometry( 1, 1 );
+        var material = new THREE.MeshBasicMaterial( {color: 0xffffff, opacity: 0.05, side: THREE.DoubleSide} );
+
+        plane = new THREE.Mesh( geometry);
+        plane.visible = false;
+        plane.position.set(0,0.5,-0.5);
+        scene.add( plane );
+
+        objects.push( plane );
+
+
+        var light;
+
+        // top
+        light = new THREE.DirectionalLight( 0xCDEDDF );
+        light.position.set( 0, 1, 1 );
         scene.add( light );
 
         light = new THREE.AmbientLight( 0x404040 );
         scene.add( light );
 
-//        render();
 
-//        scene = new THREE.Scene();
-//        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-//
-//        var canvas = document.getElementById("webGLCanvas");
-//        renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
-//        renderer.setSize( window.innerWidth, window.innerHeight );
-//
-//        // roll-over helpers
-//
-//        rollOverGeo = new THREE.BoxGeometry( 0.05, 0.05, 0.05);
-//        rollOverMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
-//        rollOverMesh = new THREE.Mesh( rollOverGeo, rollOverMaterial );
-//        rollOverMesh.position.set(0, 0.5, -0.5);
-//        scene.add( rollOverMesh );
-//
-//        // cubes
-//
-//        cubeGeo = new THREE.BoxGeometry( 0.05, 0.05, 0.05);
-//        cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xfeb74c, shading: THREE.FlatShading, map: THREE.ImageUtils.loadTexture( "square-outline-textured.png" ) } );
-//
-//        // grid
-//
-//        var size = .5, step = 0.05;
-//
-//        var geometry = new THREE.Geometry();
-//
-//        for ( var i = - size; i <= size; i += step ) {
-//
-//            geometry.vertices.push( new THREE.Vector3( - size, i, 0) );
-//            geometry.vertices.push( new THREE.Vector3(   size, i, 0) );
-//
-//            geometry.vertices.push( new THREE.Vector3( i, - size, 0) );
-//            geometry.vertices.push( new THREE.Vector3( i, size, 0) );
-//
-//        }
-//
-//        var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
-//
-//        var line = new THREE.Line( geometry, material, THREE.LinePieces );
-//        line.position.set(0,0.5,-0.5);
-//        scene.add( line );
-//
-//        //
-//
-//        raycaster = new THREE.Raycaster();
-//        //because we're using meter grids, and many objects are sub meter, we need to increase the resolution of the raycaster
-//        raycaster.near = 0.01; //assume we're at least a centameter off the object
-//        raycaster.far = 500000;
-//        raycaster.precision = 0.0000000000001;
-//        raycaster.linePrecision = 0.0000000000001;
-//
-//        mouse = new THREE.Vector2();
-//
-//        var geometry = new THREE.PlaneBufferGeometry( 1, 1 );
-//        var material = new THREE.MeshBasicMaterial( {color: 0xffffff, opacity: 0.05, side: THREE.DoubleSide} );
-//
-//        plane = new THREE.Mesh( geometry);
-//        plane.visible = false;
-//        plane.position.set(0,0.5,-0.5);
-//        scene.add( plane );
-//
-//        objects.push( plane );
-//
-//
-//        var light;
-//
-//        // top
-//        light = new THREE.DirectionalLight( 0xCDEDDF );
-//        light.position.set( 0, 1, 1 );
-//        scene.add( light );
-//
-//        light = new THREE.AmbientLight( 0x404040 );
-//        scene.add( light );
-//
-//
-//        document.addEventListener('click', onDocumentClick, false );
+        document.addEventListener('click', onDocumentClick, false );
     }
 
-//    function onDocumentClick( event ) {
-//
-//        event.preventDefault();
-//
-//        mouse.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
-//
-//        raycaster.setFromCamera( mouse, camera );
-//        raycaster.ray
-//        //alert("camera matrixWorld [[ " + camera.matrixWorld.elements[0].toFixed(2) + ", " + camera.matrixWorld.elements[1].toFixed(2) + ", " + camera.matrixWorld.elements[2].toFixed(2) + ", " + camera.matrixWorld.elements[3].toFixed(2) + " ],[" + camera.matrixWorld.elements[4].toFixed(2) + ", " + camera.matrixWorld.elements[5].toFixed(2) + ", " + camera.matrixWorld.elements[6].toFixed(2) + ", " + camera.matrixWorld.elements[7].toFixed(2) + "],[" + camera.matrixWorld.elements[8].toFixed(2) + ", " + camera.matrixWorld.elements[9].toFixed(2) + ", " + camera.matrixWorld.elements[10].toFixed(2) + ", " + camera.matrixWorld.elements[11].toFixed(2) + "],[" + camera.matrixWorld.elements[12].toFixed(2) + ", " + camera.matrixWorld.elements[13].toFixed(2) + ", " + camera.matrixWorld.elements[14].toFixed(2) + ", " + camera.matrixWorld.elements[15].toFixed(2) + "] ], ray origin ( " + raycaster.ray.origin.x.toFixed(2) + ", " + raycaster.ray.origin.y.toFixed(2) + ", " + raycaster.ray.origin.z.toFixed(2) + " ), direction (" + raycaster.ray.direction.x.toFixed(2) + ", " + raycaster.ray.direction.y.toFixed(2) + ", " + raycaster.ray.direction.z.toFixed(2) + " )");
-//        //alert("ray origin ( " + raycaster.ray.origin.x.toFixed(2) + ", " + raycaster.ray.origin.y.toFixed(2) + ", " + raycaster.ray.origin.z.toFixed(2) + " ), direction (" + raycaster.ray.direction.x.toFixed(2) + ", " + raycaster.ray.direction.y.toFixed(2) + ", " + raycaster.ray.direction.z.toFixed(2) + " )");
-//
-//
-//
-//        var intersects = raycaster.intersectObjects( scene.children);
-//
-//        if ( intersects.length > 0 ) {
-//
-//            var intersect = intersects[ 0 ];
-//
-//            //alert("object found ( " + intersect.point.x.toString() + ", " + intersect.point.y.toString() + ", " + + intersect.point.z.toString() + " )");
-//
-//            //we need to put in a switch here
-//            if (intersect.object.id == rollOverMesh.id) {
-//                  var voxel = new THREE.Mesh( cubeGeo, cubeMaterial );
-//                  voxel.position.copy( rollOverMesh.position );
-//                  voxel.position.divideScalar( .05 ).floor().multiplyScalar( .05 ).addScalar( .025 );
-//                  scene.add( voxel );
-//
-//                  objects.push( voxel );
-//
-//            }
-//
-//            rollOverMesh.position.copy( intersect.point );
-//            //we want the selection box to apear adjacent to newly created voxels, we have to introduce a slight bias to its position so
-//                      // numerical error on the intersect doesn't push it into the newly created voxel.
-//            rollOverMesh.position.x = rollOverMesh.position.x + (raycaster.ray.origin.x - raycaster.ray.direction.x)/1000; //bais closer to camera.
-//            rollOverMesh.position.y = rollOverMesh.position.y + (raycaster.ray.origin.y - raycaster.ray.direction.y)/1000; //bais closer to camera.
-//            rollOverMesh.position.z = rollOverMesh.position.z + 0.001; //bais selection box to apear on top of objects.
-//            rollOverMesh.position.divideScalar( .05 ).floor().multiplyScalar( .05 ).addScalar( .025 );
-//
-//
-//
-//
-//        }
-//
-//        renderer.render( scene, camera );
-//
-//    }
+    function onDocumentClick( event ) {
+        alert("tap");
+        event.preventDefault();
+
+        mouse.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+
+        raycaster.setFromCamera( mouse, camera );
+        raycaster.ray
+        //alert("camera matrixWorld [[ " + camera.matrixWorld.elements[0].toFixed(2) + ", " + camera.matrixWorld.elements[1].toFixed(2) + ", " + camera.matrixWorld.elements[2].toFixed(2) + ", " + camera.matrixWorld.elements[3].toFixed(2) + " ],[" + camera.matrixWorld.elements[4].toFixed(2) + ", " + camera.matrixWorld.elements[5].toFixed(2) + ", " + camera.matrixWorld.elements[6].toFixed(2) + ", " + camera.matrixWorld.elements[7].toFixed(2) + "],[" + camera.matrixWorld.elements[8].toFixed(2) + ", " + camera.matrixWorld.elements[9].toFixed(2) + ", " + camera.matrixWorld.elements[10].toFixed(2) + ", " + camera.matrixWorld.elements[11].toFixed(2) + "],[" + camera.matrixWorld.elements[12].toFixed(2) + ", " + camera.matrixWorld.elements[13].toFixed(2) + ", " + camera.matrixWorld.elements[14].toFixed(2) + ", " + camera.matrixWorld.elements[15].toFixed(2) + "] ], ray origin ( " + raycaster.ray.origin.x.toFixed(2) + ", " + raycaster.ray.origin.y.toFixed(2) + ", " + raycaster.ray.origin.z.toFixed(2) + " ), direction (" + raycaster.ray.direction.x.toFixed(2) + ", " + raycaster.ray.direction.y.toFixed(2) + ", " + raycaster.ray.direction.z.toFixed(2) + " )");
+        //alert("ray origin ( " + raycaster.ray.origin.x.toFixed(2) + ", " + raycaster.ray.origin.y.toFixed(2) + ", " + raycaster.ray.origin.z.toFixed(2) + " ), direction (" + raycaster.ray.direction.x.toFixed(2) + ", " + raycaster.ray.direction.y.toFixed(2) + ", " + raycaster.ray.direction.z.toFixed(2) + " )");
+
+        var intersects = raycaster.intersectObjects( scene.children);
+
+        if ( intersects.length > 0 ) {
+
+            var intersect = intersects[ 0 ];
+
+            //alert("object found ( " + intersect.point.x.toString() + ", " + intersect.point.y.toString() + ", " + + intersect.point.z.toString() + " )");
+
+            //we need to put in a switch here
+            if (intersect.object.id == rollOverMesh.id) {
+                  var voxel = new THREE.Mesh( cubeGeo, cubeMaterial );
+                  voxel.position.copy( rollOverMesh.position );
+                  voxel.position.divideScalar( .05 ).floor().multiplyScalar( .05 ).addScalar( .025 );
+                  scene.add( voxel );
+
+                  objects.push( voxel );
+
+            }
+
+            rollOverMesh.position.copy( intersect.point );
+            //we want the selection box to apear adjacent to newly created voxels, we have to introduce a slight bias to its position so
+                      // numerical error on the intersect doesn't push it into the newly created voxel.
+            rollOverMesh.position.x = rollOverMesh.position.x + (raycaster.ray.origin.x - raycaster.ray.direction.x)/1000; //bais closer to camera.
+            rollOverMesh.position.y = rollOverMesh.position.y + (raycaster.ray.origin.y - raycaster.ray.direction.y)/1000; //bais closer to camera.
+            rollOverMesh.position.z = rollOverMesh.position.z + 0.001; //bais selection box to apear on top of objects.
+            rollOverMesh.position.divideScalar( .05 ).floor().multiplyScalar( .05 ).addScalar( .025 );
+        }
+
+        renderer.render( scene, camera );
+    }
 
                       
     function updateWebGLView(projectionMatrix, cameraMatrix)
@@ -313,19 +232,13 @@ var cube;
         if (!projectionMatrix) alert("no projection matrix");
         if (!cameraMatrix) alert("no camera matrix");
 
-        cameraMatrix.getInverse(cameraMatrix);
-        cameraMatrix.transpose();
-
-//        camera.projectionMatrix = projectionMatrix;
+        camera.projectionMatrix = projectionMatrix;
         camera.matrixAutoUpdate = false;
         camera.matrixWorld = cameraMatrix;
         //some three.js functionality relies on position being set independently of matrixWorld defining position.
         camera.position.x = cameraMatrix.elements[12];
         camera.position.y = cameraMatrix.elements[13];
         camera.position.z = cameraMatrix.elements[14];
-//        camera.position.x = 0;
-//        camera.position.y = 0;
-//        camera.position.z = 0;
 
         renderer.render( scene, camera );
     }
--- android/BlockStacking/src/main/java/com/intel/sample/depth/spsample/SPBasicFragment.java
+++ android/BlockStacking/src/main/java/com/intel/sample/depth/spsample/SPBasicFragment.java
@@ -197,20 +197,18 @@ public class SPBasicFragment extends Fragment implements DepthProcessModule {
 		 * IMU samples and their associated time stamps.
 		 */			
 		@Override
-		public void onSyncedFramesAvailable(SPInputStream input) {	
+		public void onSyncedFramesAvailable(SPInputStream input) {
+
+            float sceneQuality = mSPCore.getSceneQuality(input, false);
+            mIsScenePerceptionActive.set(sceneQuality >= ACCEPTABLE_INPUT_COVERAGE_PERC);
+            if (!mIsScenePerceptionActive.get()) sendSceneQualityToWebView(sceneQuality);
+
 			if (mSPStatus != Status.SP_STATUS_SUCCESS) {
 				if (mIsScenePerceptionActive.get()) {
 					setProgramStatus("ERROR - " + mSPStatus); // TODO: show in web view?
 				}
 				mCamHandler.frameProcessCompleteCallback();
 				return;
-			}		
-			
-			//checking scene quality for good initialization of SP module
-			if (!mIsScenePerceptionActive.get()) {
-				float sceneQuality = mSPCore.getSceneQuality(input, false);
-                sendSceneQualityToWebView(sceneQuality);
-				mIsScenePerceptionActive.set(sceneQuality >= ACCEPTABLE_INPUT_COVERAGE_PERC);
 			}
 
 			// if user indicates active tracking and reconstruction (pressing Play button)
@@ -872,11 +870,13 @@ public class SPBasicFragment extends Fragment implements DepthProcessModule {
 
     public void sendPoseToWebView(final CameraPose cameraPose){
         final Activity curActivity = getActivity();
-        if (curActivity != null) {
+        if (curActivity != null)
+        {
+			Matrix.rotateM(cameraPose.get(), 0, 90f, 1f, 0f, 0f); // workaround for projection matrix difficulties
+
             curActivity.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-//					String cameraPoseString = String.format("{ m00: %f, m10: %f, m20: %f, m30: %f, m01: %f, m11: %f, m21: %f, m31: %f, m02: %f, m12: %f, m22: %f, m32: %f, m03: %f, m13: %f, m23: %f, m33: %f }",
 					String cameraPoseString = String.format("{ m00: %f, m01: %f, m02: %f, m03: %f, m10: %f, m11: %f, m12: %f, m13: %f, m20: %f, m21: %f, m22: %f, m23: %f, m30: %f, m31: %f, m32: %f, m33: %f }",
                         cameraPose.get()[0], cameraPose.get()[1], cameraPose.get()[2], cameraPose.get()[3], cameraPose.get()[4], cameraPose.get()[5], cameraPose.get()[6], cameraPose.get()[7], cameraPose.get()[8], cameraPose.get()[9], cameraPose.get()[10], cameraPose.get()[11], cameraPose.get()[12], cameraPose.get()[13], cameraPose.get()[14], cameraPose.get()[15]
                     );

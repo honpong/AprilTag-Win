c2ad939d149ac2dd3356329f11007ce867dd5eab
Brian Fulkerson <brian.fulkerson@intel.com>
2016-04-22 15:45:10 +0100

    test: change tolerances on quaternion test
    Without this change, tests fail when f_t is double

--- corvis/test/numerics/quaternion.cpp
+++ corvis/test/numerics/quaternion.cpp
@@ -10,7 +10,7 @@ TEST(Quaternion, Identity)
     EXPECT_QUATERNION_NEAR(q * id, q, 0);
     EXPECT_QUATERNION_NEAR(id * q, q, 0);
     quaternion qn = normalize(q);
-    EXPECT_QUATERNION_NEAR(qn * conjugate(qn), id, F_T_EPS);
+    EXPECT_QUATERNION_NEAR(qn * conjugate(qn), id, F_T_EPS*2);
 }
 
 TEST(Quaternion, Rotation)
@@ -18,6 +18,6 @@ TEST(Quaternion, Rotation)
     quaternion rotq = normalize(q);
     v4 v(-1.5, 1.6, -.2, 0.); quaternion w(0, v[0], v[1], v[2]);
     quaternion rotv = rotq * w * conjugate(rotq); v4 rotw(rotv.x(), rotv.y(),rotv.z(), rotv.w());
-    EXPECT_V4_NEAR(rotq * v, rotw, F_T_EPS) << "rotq * v == unqaternion(rotq * quaternion(v) * conjugate(rotq))";
-    EXPECT_V4_NEAR(rotq * v, to_rotation_matrix(rotq) * v, F_T_EPS);
+    EXPECT_V4_NEAR(rotq * v, rotw, F_T_EPS*3) << "rotq * v == unqaternion(rotq * quaternion(v) * conjugate(rotq))";
+    EXPECT_V4_NEAR(rotq * v, to_rotation_matrix(rotq) * v, F_T_EPS*2);
 }

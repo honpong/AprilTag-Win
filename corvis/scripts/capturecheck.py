c3618fad4c099432a1d116923835512d59860902
Brian Fulkerson <brian.fulkerson@intel.com>
2016-06-06 14:15:20 +0100

    capturecheck: switch to argparse and accept "-e" to print exceptions

--- corvis/scripts/capturecheck.py
+++ corvis/scripts/capturecheck.py
@@ -2,18 +2,17 @@
 from struct import *
 from collections import defaultdict
 import numpy
+import argparse
+
+parser = argparse.ArgumentParser(description='Check a capture file.')
+parser.add_argument("-e", "--exceptions", action='store_true',
+        help="Print details when sample dt is more than 5%% away from the mean")
+parser.add_argument("capture_filename")
+
+args = parser.parse_args()
 
 packets = defaultdict(list)
-
-import sys
-
-if len(sys.argv) < 2:
-  print "Usage:", sys.argv[0], "<capture file>"
-  sys.exit(1)
-
-filename = sys.argv[1]
-
-f = open(filename)
+f = open(args.capture_filename)
 header_size = 16
 header_str = f.read(header_size)
 while header_str != "":
@@ -34,6 +33,7 @@ for packet_type in packets:
   print "rate (hz):", 1/(mean_delta/1e6), "mean dt (us):", mean_delta, "std dt (us):", numpy.std(deltas)
   exceptions = numpy.flatnonzero(numpy.logical_or(deltas > mean_delta*1.05, deltas < mean_delta*0.95))
   print len(exceptions), "samples are more than 5% from mean"
-  #for e in exceptions:
-  #    print "Exception: t t+1 delta", packets[packet_type][e], packets[packet_type][e+1], deltas[e]
+  if args.exceptions:
+      for e in exceptions:
+          print "Exception: t t+1 delta", packets[packet_type][e], packets[packet_type][e+1], deltas[e]
   print ""

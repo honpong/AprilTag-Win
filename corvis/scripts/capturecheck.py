9304e78f013c35e813e6b1705fa0404d2f71d451
Brian Fulkerson <brian.fulkerson@intel.com>
2016-06-06 13:51:16 +0100

    capturecheck: calculate timing stats and report deviations from mean

--- corvis/scripts/capturecheck.py
+++ corvis/scripts/capturecheck.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from struct import *
 from collections import defaultdict
+import numpy
 
 packets = defaultdict(list)
 
@@ -20,7 +21,7 @@ while header_str != "":
   if ptype == 1:
     ptime += 16667
   print pbytes, ptype, user, float(ptime)/1e6
-  packets[ptype].append(ptime)
+  packets[ptype].append(int(ptime))
   junk = f.read(pbytes-header_size)
   header_str = f.read(header_size)
 
@@ -28,3 +29,11 @@ f.close()
 
 for packet_type in packets:
   print "type:", packet_type, "number:", len(packets[packet_type])
+  deltas = numpy.array(packets[packet_type][1:]) - numpy.array(packets[packet_type][:-1])
+  mean_delta = numpy.mean(deltas)
+  print "rate (hz):", 1/(mean_delta/1e6), "mean dt (us):", mean_delta, "std dt (us):", numpy.std(deltas)
+  exceptions = numpy.flatnonzero(numpy.logical_or(deltas > mean_delta*1.05, deltas < mean_delta*0.95))
+  print len(exceptions), "samples are more than 5% from mean"
+  #for e in exceptions:
+  #    print "Exception: t t+1 delta", packets[packet_type][e], packets[packet_type][e+1], deltas[e]
+  print ""

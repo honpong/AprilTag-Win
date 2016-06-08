134babac701d99f04dd6ce62a3ffcc115959bdbe
Jim Radford <jim.radford@intel.com>
2016-06-08 16:13:49 -0700

    Remove obsolete uses of tracker.h

--- corvis/src/filter/tracker_pyramid.h
+++ corvis/src/filter/tracker_pyramid.h
@@ -5,8 +5,6 @@ extern "C" {
 #include "cor.h"
 }
 
-#include "tracker.h"
-
 void tracker_pyramid_init(int width, int height, int stride);
 feature_t tracker_pyramid_track(uint8_t * im1, uint8_t * im2, int currentx, int currenty, int x1, int y1, int x2, int y2);
 

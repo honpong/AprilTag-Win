7e1499dc4d8ff764a9d90268bb558152a0476c44
Brian Fulkerson <brian.fulkerson@intel.com>
2016-06-29 14:55:25 +0200

    calibration: Basic version 10 (multi-sensor) calibration
    Includes serialization / deserialization and conversion from old
    versions.

---
+++ corvis/src/filter/calibration.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#include "rc_tracker.h"
+
+#define CALIBRATION_VERSION 10
+
+template<class T> class sensor_calibration {
+    public:
+        std::string name;
+        uint64_t id;
+        rc_Extrinsics extrinsics;
+        T intrinsics;
+        sensor_calibration(rc_Extrinsics _extrinsics, T _intrinsics) : extrinsics(_extrinsics), intrinsics(_intrinsics) {};
+        sensor_calibration() : extrinsics({0}), intrinsics(T()) {};
+};
+
+typedef sensor_calibration<rc_CameraIntrinsics> sensor_calibration_camera;
+typedef sensor_calibration<rc_CameraIntrinsics> sensor_calibration_depth;
+
+struct imu_intrinsics {
+    rc_AccelerometerIntrinsics accelerometer;
+    rc_GyroscopeIntrinsics gyroscope;
+};
+
+typedef sensor_calibration<imu_intrinsics> sensor_calibration_imu;
+typedef sensor_calibration<rc_AccelerometerIntrinsics> sensor_calibration_accelerometer;
+typedef sensor_calibration<rc_GyroscopeIntrinsics> sensor_calibration_gyroscope;
+
+struct calibration {
+    int version;
+    std::string device_id;
+    std::string device_type;
+    std::vector<sensor_calibration_imu> imus;
+    std::vector<sensor_calibration_camera> cameras;
+    std::vector<sensor_calibration_depth> depths;
+};
+
+bool calibration_serialize(const calibration &cal, std::string &jsonString);
+bool calibration_deserialize(const std::string &jsonString, calibration &cal);

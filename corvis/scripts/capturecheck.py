honpong/AprilTag-Win

commit dc96264a19308403d92a73df46ead04862fe5a93

capturecheck.py: ptime is already an int

corvis/scripts/capturecheck.py
@@ -59,9 +59,9 @@ while header_str != "":
       ptime += exposure/2
   if packet_str == "":
       packet_str = str(ptype)
-  packets[packet_str].append(int(ptime))
+  packets[packet_str].append(ptime)
   if not (ptype == accel_type or ptype == gyro_type) and prev_packet_str == packet_str:
-      warnings[packet_str].append((int(last_time), int(ptime)))
+      warnings[packet_str].append(last_time, ptime)
   last_time = ptime
   prev_packet_str = packet_str
   header_str = f.read(header_size)

commit b38fbc89352ada43d8516b6fcb51f97c6ec99264

capturecheck.py: fix bug with not adding a tuple
Introduced in dc96264a19308403d92a73df46ead04862fe5a93 when I removed
an important ().

corvis/scripts/capturecheck.py
@@ -66,7 +66,7 @@ while header_str != "":
   latencies[packet_str].append(latest_received - ptime)
   packets[packet_str].append(ptime)
   if not (ptype == accel_type or ptype == gyro_type) and prev_packet_str == packet_str:
-      warnings[packet_str].append(last_time, ptime)
+      warnings[packet_str].append((last_time, ptime))
   last_time = ptime
   prev_packet_str = packet_str
   header_str = f.read(header_size)
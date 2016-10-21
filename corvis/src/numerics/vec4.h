4a84ca6b9b3d7dcde04060682893993cf2dfc2b0
Jim Radford <jim.radford@intel.com>
2016-10-21 14:38:58 -0700

    stdev: keep track of min as well as max

--- corvis/src/numerics/vec4.h
+++ corvis/src/numerics/vec4.h
@@ -74,9 +74,9 @@ template <int N>
 class stdev
 {
 public:
-    v<N> sum, max, mean, M2, variance, stdev_;
+    v<N> sum, min, max, mean, M2, variance, stdev_;
     uint32_t count;
-    stdev(): sum(v<N>::Zero()), mean(v<N>::Zero()), M2(v<N>::Zero()), variance(v<N>::Zero()), stdev_(v<N>::Zero()), max(v<N>::Zero()), count(0) {}
+    stdev(): sum(v<N>::Zero()), mean(v<N>::Zero()), M2(v<N>::Zero()), variance(v<N>::Zero()), stdev_(v<N>::Zero()), min({INFINITY}), max(v<N>::Zero()), count(0) {}
     bool valid() { return count >= 2; }
     void data(const v<N> &x) {
         ++count;
@@ -84,6 +84,7 @@ public:
         mean = mean + delta / (f_t)count;
         M2 = M2 + delta.cwiseProduct(x - mean);
         if(x.norm() > max.norm()) max = x;
+        if(x.norm() < min.norm()) min = x;
         variance = M2 / (f_t)(count - 1);
         stdev_ = variance.array().sqrt();
     }
@@ -92,7 +93,7 @@ public:
 template<int N>
 static inline std::ostream& operator<<(std::ostream &stream, const stdev<N> &v)
 {
-    return stream << "mean is: " << v.mean << ", stdev is: " << v.stdev_ << ", max is: " << v.max;
+    return stream << " min is " << v.min << " mean is: " << v.mean << ", stdev is: " << v.stdev_ << ", max is: " << v.max;
 }
 
 class histogram

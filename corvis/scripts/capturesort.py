e0d739b369b43a983344ae0b887cd4b6a0edf6c4
Jim Radford <jim.radford@intel.com>
2016-12-05 09:56:53 -0800

    capturesort: sort by time + exposure/2 (Closes: #203)

--- corvis/scripts/capturesort.py
+++ corvis/scripts/capturesort.py
@@ -16,15 +16,16 @@ def capture_packets(filename):
           header = f.read(16)
           if header == "": break
           (pbytes, ptype, user, ptime) = unpack('IHHQ', header)
-          yield { 'type': ptype, 'user': user, 'time': ptime, 'header': header, 'data': f.read(pbytes - 16) }
+          data = f.read(pbytes - 16)
+          yield { 'type': ptype, 'user': user, 'time': ptime, 'header': header, 'data': data, 'exposure': unpack_from('Q', data)[0] if ptype in (28,29) else 0 }
 
 packets = list(capture_packets(src))
 
 if dst is not None:
   with open(dst, "wb") as d:
-    for p in sorted(packets, None, lambda p: p['time']):
+    for p in sorted(packets, None, lambda p: p['time'] + p['exposure']/2):
         d.write(p['header'])
         d.write(p['data'])
 else:
   for p in packets:
-    print((p['time'], p['type']))
+    print((p['time'], p['type'], p['exposure']))

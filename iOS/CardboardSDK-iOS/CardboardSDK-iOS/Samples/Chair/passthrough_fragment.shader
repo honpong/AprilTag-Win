9074a1208988678eccd5b8bd66fdaf835d04a5c6
Jim Radford <jim.radford@intel.com>
2017-01-05 15:27:46 -0800

    iOS: remove obsolete iOS apps

--- iOS/CardboardSDK-iOS/CardboardSDK-iOS/Samples/Chair/passthrough_fragment.shader
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-varying vec4 v_Color;
-varying mediump vec2 coordinate;
-
-uniform sampler2D texture_value;
-
-void main() {
-    gl_FragColor = texture2D(texture_value, coordinate) + v_Color;
-}
--- iOS/RC_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-xcodebuild -workspace RC.xcworkspace -list | tail +3 | sed -e 's/^ *//' -e '/^$/d' | while read s; do
-    echo -n $'\e[33m'"$s"$'\e[m' $'\e[34mBuilding\e[m' ...
-    xcodebuild -workspace RC.xcworkspace -scheme "$s" -configuration Release \
-               'GCC_PREPROCESSOR_DEFINITIONS=${GCC_PREPROCESSOR_DEFINITIONS} SDK_LICENSE_KEY=@\"C4c5C2Ae73eD8c3EdAAFDC75df12DC\"' > "/tmp/build-$s.out" && \
-       echo $'\e[33m'"$s"$'\e[m' $'\e[32mBuilt\e[m' && rm "/tmp/build-$s.out"  || \
-       echo $'\e[33m'"$s"$'\e[m' $'\e[31mFailed\e[m'. See "/tmp/build-$s.out" for details.;
-done

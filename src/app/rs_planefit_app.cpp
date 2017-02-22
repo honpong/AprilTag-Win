5e2792c6cf27af35718ab4b19c1136083bc39e51
Hon Pong (Gary) Ho <hon.pong.ho@intel.com>
2017-02-21 16:50:38 -0800

    app: save multiple frames.

--- src/app/rs_planefit_app.cpp
+++ src/app/rs_planefit_app.cpp
@@ -5,28 +5,37 @@
 #include "json/json.h"
 #include "rs_shapefit.h"
 
-int planefit_one_frame(const std::string& path);
-int capture_one_frame(const std::string& path);
+const int image_set_size = 20;
+
+struct frame_data { cv::Mat src[2]; rs_sf_image images[2]; rs_sf_intrinsics depth_intrinsics; };
+int planefit_one_frame(rs_sf_image* images, rs_sf_intrinsics* depth_intrinsics);
+int capture_frames(const std::string& path);
+frame_data load_one_frame(const std::string& path, const int frame_num = -1);
 
 int main(int argc, char* argv[])
 {
-    std::string path = "c:\\temp\\shapefit\\a\\";
-    //capture_one_frame(path);
-    planefit_one_frame(path);
-    
-    path = "c:\\temp\\shapefit\\b\\";
-    planefit_one_frame(path);
-    
-    path = "c:\\temp\\shapefit\\c\\";
-    return planefit_one_frame(path);
+    std::string path = "c:\\temp\\shapefit\\d\\";
+    //capture_frames(path);
+    //planefit_one_frame(data.images, &data.depth_intrinsics);
+
+    rs_sf_planefit* planefitter = nullptr;
+    for (int i = 0; i < image_set_size; ++i)
+    {
+        auto data = load_one_frame(path, i);
+        if (!planefitter) planefitter = rs_sf_planefit_create(&data.depth_intrinsics);
+        rs_sf_planefit_depth_image(planefitter, data.images, RS_SF_PLANEFIT_OPTION_RESET);
+    }
+    rs_sf_planefit_delete(planefitter);
 }
 
-int planefit_one_frame(const std::string & path)
+frame_data load_one_frame(const std::string& path, const int frame_num)
 {
-    auto depth_data = cv::imread(path + "depth.png", CV_LOAD_IMAGE_UNCHANGED);
-    auto ir_data = cv::imread(path + "ir.png", CV_LOAD_IMAGE_UNCHANGED);
+    frame_data r_data;
+    auto suffix = (frame_num >= 0 ? "_" + std::to_string(frame_num) : "") + ".png";
+    auto depth_data = r_data.src[0] = cv::imread(path + "depth" + suffix, CV_LOAD_IMAGE_UNCHANGED);
+    auto ir_data = r_data.src[1] = cv::imread(path + "ir" + suffix, CV_LOAD_IMAGE_UNCHANGED);
 
-    rs_sf_image input_images[2] = {};
+    auto& input_images = r_data.images;
     input_images[0].data = depth_data.data;
     input_images[0].img_w = depth_data.cols;
     input_images[0].img_h = depth_data.rows;
@@ -43,7 +52,7 @@ int planefit_one_frame(const std::string & path)
     infile >> calibration_data;
 
     Json::Value json_depth_intrinsics = calibration_data["depth_cam"]["intrinsics"];
-    rs_sf_intrinsics depth_intrinsics = {};
+    auto& depth_intrinsics = r_data.depth_intrinsics;
     depth_intrinsics.cam_fx = json_depth_intrinsics["fx"].asFloat();
     depth_intrinsics.cam_fy = json_depth_intrinsics["fy"].asFloat();
     depth_intrinsics.cam_px = json_depth_intrinsics["ppx"].asFloat();
@@ -51,14 +60,19 @@ int planefit_one_frame(const std::string & path)
     depth_intrinsics.img_w = json_depth_intrinsics["width"].asInt();
     depth_intrinsics.img_h = json_depth_intrinsics["height"].asInt();
 
+    return r_data;
+}
+
+int planefit_one_frame(rs_sf_image* input_images, rs_sf_intrinsics* depth_intrinsics)
+{
     ////////////////////////////////////////////////////////////////////////////////
-    auto planefitter = rs_sf_planefit_create(&depth_intrinsics);
+    auto planefitter = rs_sf_planefit_create(depth_intrinsics);
     rs_sf_planefit_depth_image(planefitter, input_images);
     rs_sf_planefit_delete(planefitter);
     return 0;
 }
 
-int capture_one_frame(const std::string& path) {
+int capture_frames(const std::string& path) {
 
     rs::context ctx;
     auto list = ctx.query_devices();
@@ -77,7 +91,8 @@ int capture_one_frame(const std::string& path) {
     dev.set_option(RS_OPTION_EMITTER_ENABLED, 1);
     dev.set_option(RS_OPTION_ENABLE_AUTO_EXPOSURE, 1);
     
-    cv::Mat depth_image, raw_ir_image, displ_image;
+    struct dataset { cv::Mat depth, ir, displ; };
+    std::deque<dataset> image_set;
     while (1) {
         auto fs = syncer.wait_for_frames();
         if (fs.size() == 0) break;
@@ -89,18 +104,26 @@ int capture_one_frame(const std::string& path) {
         auto depth_data = frames[RS_STREAM_DEPTH]->get_data();
         auto img_w = frames[RS_STREAM_DEPTH]->get_width();
         auto img_h = frames[RS_STREAM_DEPTH]->get_height();
-        depth_image = cv::Mat(img_h, img_w, CV_16U, (void*)depth_data).clone();
-        raw_ir_image = cv::Mat(img_h, img_w, CV_8U, (void*)frames[RS_STREAM_INFRARED]->get_data()).clone();
+        auto depth_image = cv::Mat(img_h, img_w, CV_16U, (void*)depth_data).clone();
+        auto raw_ir_image = cv::Mat(img_h, img_w, CV_8U, (void*)frames[RS_STREAM_INFRARED]->get_data()).clone();
+        cv::Mat displ_image;
         depth_image.convertTo(displ_image, CV_8U, 255.0 / 8000.0);
         cv::hconcat(displ_image, raw_ir_image, displ_image);
         cv::imshow("DS5 depth", displ_image);
+    
+        image_set.push_back({ depth_image,raw_ir_image,displ_image });
+        while (image_set.size() > image_set_size) image_set.pop_front();
         if (cv::waitKey(1) == 'q') break;
     }
 
-    cv::imwrite(path + "depth.png", depth_image);
-    cv::imwrite(path + "ir.png", raw_ir_image);
-    cv::imwrite(path + "displ.png", displ_image);
-   
+    int frame_id = 0;
+    for (auto& dataset : image_set) {
+        std::string suffix = std::to_string(frame_id++) + ".png";
+        cv::imwrite(path + "depth_" + suffix, dataset.depth);
+        cv::imwrite(path + "ir_" + suffix, dataset.ir);
+        cv::imwrite(path + "displ_" + suffix , dataset.displ);
+    }
+
     Json::Value json_intr;
     json_intr["fx"] = intrinsics.fx;
     json_intr["fy"] = intrinsics.fy;
@@ -114,6 +137,7 @@ int capture_one_frame(const std::string& path) {
 
     Json::Value root;
     root["depth_cam"]["intrinsics"] = json_intr;
+    root["depth_cam"]["num_frame"] = image_set_size;
 
     try {
         Json::StyledStreamWriter writer;

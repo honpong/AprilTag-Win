c8978ac329c3978d8da32489b67cb873921cb054
Daniel Mirota <dmirota.mirota@intel.com>
2017-03-14 10:36:46 -0700

    remapper: Moved use of std::copy and std::fill inside of remapper.cpp to add _SCL_SECURE_NO_WARNINGS once for debug

--- corvis/src/tracker/remapper.h
+++ corvis/src/tracker/remapper.h
@@ -85,52 +85,11 @@ class remapper {
         remap_vector(size, Pn, Pn);
     }
 
-    void remap_vector(int size, matrix &Pn_to, const matrix &Pn_from) {
-        compile();
-        Pn_to.resize(std::max(Pn_from.cols(),size)); // in case &Pn_to == &Pn_from
-        for (auto &c : updates)
-            if (c.type == update::add)
-                std::memcpy(&Pn_to[c.to], &process_covariance[c.to], c.size*sizeof(process_covariance[c.to]));
-            else if (&Pn_to != &Pn_from || c.to != c.from)
-                std::memmove(&Pn_to[c.to], &Pn_from[c.from], c.size*sizeof(Pn_from[0]));
-        Pn_to.resize(size);
-    }
+    void remap_vector(int size, matrix &Pn_to, const matrix &Pn_from);
 
     void remap_matrix(int size, matrix &P) {
         remap_matrix(size, P, P);
     }
 
-    void remap_matrix(int size, matrix &P_to, const matrix &P_from) {
-        compile();
-        P_to.resize(std::max(P_from.rows(),size), std::max(P_from.cols(),size)); // in case &P_to == &P_from
-        for (auto &r : updates) {
-            if (r.from <= r.to && r.to < r.from+r.size) {
-                for (int i=r.size-1; i>=0; i--)
-                    if (r.type == update::add) {
-                        std::fill(&P_to(r.to+i,   0),&P_to(r.to+i, r.to+i), 0);
-                        P_to(r.to+i,r.to+i) = initial_covariance[r.to+i];
-                        std::fill(&P_to(r.to+i,r.to+i+1),&P_to(r.to+i,size), 0);
-                    } else {
-                        for (auto &c : updates)
-                            if (c.type == update::add)
-                                std::fill(&P_to(r.to+i,c.to),&P_to(r.to+i,c.to+c.size), 0);
-                            else if (&P_to != &P_from || r.from != r.to || c.from != c.to)
-                                std::memmove(&P_to(r.to+i,c.to), &P_from(r.from+i,c.from), c.size * sizeof(P_from(0,0)));
-                    }
-            } else {
-                for (int i=0; i<r.size; i++)
-                    if (r.type == update::add) {
-                        std::fill(&P_to(r.to+i,   0),&P_to(r.to+i, r.to+i), 0);
-                        P_to(r.to+i,r.to+i) = initial_covariance[r.to+i];
-                        std::fill(&P_to(r.to+i,r.to+i+1),&P_to(r.to+i,size), 0);
-                    } else
-                        for (auto &c : updates)
-                            if (c.type == update::add)
-                                std::fill(&P_to(r.to+i,c.to),&P_to(r.to+i,c.to+c.size), 0);
-                            else if (&P_to != &P_from || r.from != r.to || c.from != c.to)
-                                std::memmove(&P_to(r.to+i,c.to), &P_from(r.from+i,c.from), c.size * sizeof(P_from(0,0)));
-            }
-        }
-        P_to.resize(size, size);
-    }
+    void remap_matrix(int size, matrix &P_to, const matrix &P_from);
 };

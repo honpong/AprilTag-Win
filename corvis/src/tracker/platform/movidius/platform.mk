4552ce0824dccd9ee0622f6a93f7e2719e81e981
Pedro Pinies <pedro.pinies@intel.com>
2017-05-30 15:14:19 -0700

    vocabulary: Add DBoW vocabulary.

--- corvis/src/tracker/platform/movidius/platform.mk
+++ corvis/src/tracker/platform/movidius/platform.mk
@@ -27,6 +27,12 @@ SLAM_SOURCES += $(SLAM_PREFIX)/corvis/src/feature/tracker/fast_tracker.cpp
 SLAM_SOURCES += $(SLAM_PREFIX)/corvis/src/feature/descriptor/patch_descriptor.cpp
 SLAM_SOURCES += $(wildcard $(SLAM_PLATFORM_PREFIX)/leon/*.cpp)
 
+SLAM_SOURCES += $(addprefix $(SLAM_PREFIX)/ThirdParty/DBoW2/src/, \
+    BowVector.cpp \
+    FeatureVector.cpp \
+    ScoringObject.cpp \
+)
+
 SLAM_C_SOURCES := $(addprefix $(SLAM_PREFIX)/ThirdParty/vlfeat-0.9.18/vl/, \
     liop.c \
     imopv.c \
@@ -36,9 +42,12 @@ SLAM_C_SOURCES := $(addprefix $(SLAM_PREFIX)/ThirdParty/vlfeat-0.9.18/vl/, \
 )
 SLAM_C_SOURCES += $(wildcard $(SLAM_PLATFORM_PREFIX)/leon/*.c)
 
+SLAM_C_SOURCES += $(SLAM_PREFIX)/corvis/src/vocabulary/voc.c
+
 SLAM_HEADERS := \
 	$(wildcard $(SLAM_PREFIX)/corvis/src/tracker/*.h) \
 	$(wildcard $(SLAM_PREFIX)/corvis/src/tracker/fast_detector/*.h) \
+	$(wildcard $(SLAM_PREFIX)/ThirdParty/DBoW2/include/*.h) \
 	$(wildcard $(SLAM_PLATFORM_PREFIX)/leon/*.h) \
 	$(wildcard $(SLAM_PLATFORM_PREFIX)/leon/*.hpp)
 
@@ -51,6 +60,8 @@ SLAM_CCOPT   := \
 	-I$(SLAM_PREFIX)/corvis/src/feature/descriptor \
 	-I$(SLAM_PREFIX)/corvis/src/feature/detector \
 	-I$(SLAM_PREFIX)/corvis/src/feature/tracker \
+	-I$(SLAM_PREFIX)/corvis/src/vocabulary \
+	-I$(SLAM_PREFIX)/ThirdParty/DBoW2/include \
 	-I$(SLAM_PREFIX)/ThirdParty/eigen \
 	-I$(SLAM_PREFIX)/ThirdParty/spdlog/include \
 	-I$(SLAM_PREFIX)/ThirdParty/rapidjson/include/ \
@@ -59,6 +70,7 @@ SLAM_CCOPT   := \
 	-Wno-unused-parameter \
 	-Wno-sign-compare \
 	-fno-strict-aliasing \
+	-DVOC_DIR='"$(SLAM_PREFIX)/corvis/src/vocabulary/"' \
 	-DRAPIDJSON_64BIT=1 \
 	-DEIGEN_STACK_ALLOCATION_LIMIT=1024 \
 	-MD \

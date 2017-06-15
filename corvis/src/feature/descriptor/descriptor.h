52b57393cb14624571174e0e68367c1ad0a4218e
Jim Radford <jim.radford@intel.com>
2017-06-14 22:28:20 -0700

    descriptor: allow overiding DESCRIPTOR

--- corvis/src/feature/descriptor/descriptor.h
+++ corvis/src/feature/descriptor/descriptor.h
@@ -1,4 +1,9 @@
 #pragma once
-#include "patch_descriptor.h"
 
+#ifndef DESCRIPTOR
 #define DESCRIPTOR patch_descriptor
+#endif
+
+#define TO_STR(d) #d
+#define DOT_H(d) TO_STR(d.h)
+#include DOT_H(DESCRIPTOR)

1a7ab80ca45d421a5f32681bd286a16d44594c3b
Brian Fulkerson <brian.fulkerson@intel.com>
2017-07-24 14:14:36 +0100

    filter: Fix last standby_feature being duplicated
    When we terminated the loop because we had found enough features, the
    last added standby feature was not being erased.

--- corvis/src/tracker/filter.cpp
+++ corvis/src/tracker/filter.cpp
@@ -442,7 +442,7 @@ static int filter_add_detected_features(struct filter * f, state_camera &camera,
     f_t image_to_depth = 1;
     if(f->has_depth)
         image_to_depth = f_t(f->recent_depth->image.height)/image_height;
-    for(auto i = kp.begin(); i != kp.end(); i = kp.erase(i)) {
+    for(auto i = kp.begin(); i != kp.end();) {
         {
             state_vision_feature *feat = f->s.add_feature(*i, *g);
 
@@ -470,6 +470,7 @@ static int filter_add_detected_features(struct filter * f, state_camera &camera,
             g->features.children.push_back(feat);
             feat->track.feature = i->feature;
             
+            i = kp.erase(i);
             found_feats++;
             if(found_feats == newfeats) break;
         }

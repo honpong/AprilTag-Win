c3acb908af385c73a78e365417035c378e1bb422
Jim Radford <jim.radford@intel.com>
2017-10-25 16:39:00 -0700

    rc_tracker: remove unsued function

--- corvis/src/tracker/rc_tracker.cpp
+++ corvis/src/tracker/rc_tracker.cpp
@@ -24,11 +24,6 @@ const char *rc_version()
     return rc_build_;
 }
 
-static void rc_trace(const rc_Vector p)
-{
-    trace_log->info("{} {} {}", p.x, p.y, p.z);
-}
-
 static void rc_trace(const rc_Matrix p)
 {
     trace_log->info("{} {} {}; {} {} {}; {} {} {}", p.v[0][0], p.v[0][1], p.v[0][2],  p.v[1][0], p.v[1][1], p.v[1][2],  p.v[2][0], p.v[2][1], p.v[2][2]);

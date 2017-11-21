f21f1522160ed0bded9ab987252b9e3a029ff21e
Jim Radford <jim.radford@intel.com>
2017-11-21 14:52:31 -0600

    project_motion_covariance_shave: save CMX with a better estimate of the max number of groups

--- corvis/src/tracker/platform/movidius/leon/project_covariance_definitions.h
+++ corvis/src/tracker/platform/movidius/leon/project_covariance_definitions.h
@@ -4,8 +4,6 @@
 #define PROJECT_COVARIANCE_FIRST_SHAVE 0
 #define PROJECT_COVARIANE_SHAVES 4
 
-#define MAX_DATA_SIZE 96
-
 struct project_covariance_element_data{
     int index;
     float initial_covariance;
@@ -35,11 +33,11 @@ struct project_motion_covariance_data{
     project_covariance_element_data Q = {0, 0, 0};
     const float *dQp_s_dW = NULL;
     int camera_count = 0;
-    project_covariance_element_data tr[MAX_DATA_SIZE] = { {0, 0, 0} };
-    project_covariance_element_data qr[MAX_DATA_SIZE] = { {0, 0, 0} };
-    float* dTrp_dQ_s_matrix[MAX_DATA_SIZE] = {NULL};
-    float* dQrp_s_dW_matrix[MAX_DATA_SIZE] = {NULL};
-    float* dTrp_ddT_matrix[MAX_DATA_SIZE] = {NULL};
+    project_covariance_element_data tr[MAX_GROUPS] = {};
+    project_covariance_element_data qr[MAX_GROUPS] = {};
+    float* dTrp_dQ_s_matrix[MAX_GROUPS] = {};
+    float* dQrp_s_dW_matrix[MAX_GROUPS] = {};
+    float* dTrp_ddT_matrix[MAX_GROUPS] = {};
     float dt = 0.f;
 };
 

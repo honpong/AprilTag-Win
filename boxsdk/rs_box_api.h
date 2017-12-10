873f352d6efb3df5e653a52c8bf320b19ff07eea
Hon Pong (Gary) Ho <hon.pong.ho@intel.com>
2017-12-10 22:20:52 +0800

    software legal compliance headers

--- boxsdk/rs_box_api.h
+++ boxsdk/rs_box_api.h
@@ -1,5 +1,17 @@
-// License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+/*******************************************************************************
+  
+ Copyright 2015-2018 Intel Corporation.
+ 
+ This software and the related documents are Intel copyrighted materials,
+ and your use of them is governed by the express license under which
+ they were provided to you. Unless the License provides otherwise,
+ you may not use, modify, copy, publish, distribute, disclose or transmit
+ this software or the related documents without Intel's prior written permission.
+ 
+ This software and the related documents are provided as is, with no express or
+ implied warranties, other than those that are expressly stated in the License.
+ 
+ *******************************************************************************/
 
 #pragma once
 #ifndef rs_box_api_h

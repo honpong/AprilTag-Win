eb914dd5a30fa30a179d6b9c7de6664ecd4c6035
Hon Pong (Gary) Ho <hon.pong.ho@intel.com>
2017-12-11 19:24:44 -0800

    libSP: cmake option to selection particular SP version.

--- src/thirdparty/sp/win/libSPConfig.cmake
+++ src/thirdparty/sp/win/libSPConfig.cmake
@@ -1,33 +1,36 @@
 get_filename_component(libSP_CONFIG_PATH "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY CACHE)
 
-set(SP_TYPE "20171204" CACHE STRING "Choose either trunk / gold2 / 20171204")
+set(libSP_VERSION "20171211" CACHE STRING "Choose either trunk / gold2 / 20171211")
 
 if(WIN32)
-if(${SP_TYPE} MATCHES "trunk" OR ${SP_TYPE} MATCHES "gold2")
 
-	set(libSP_ROOT "${libSP_CONFIG_PATH}/${SP_TYPE}")
-	set(libSP_INCLUDE_DIR "${libSP_ROOT}/include")
-	set(libSP_LIB_DIR "${libSP_ROOT}/lib/${CMAKE_VS_PLATFORM_NAME}")
-	set(libSP_BIN_DIR "${libSP_ROOT}/bin/${CMAKE_VS_PLATFORM_NAME}")
-	set(libSP_LIB "SP_Core")
-	set(libSP_FOUND TRUE CACHE BOOL "" FORCE)
+    set(libSP_DIR "${libSP_CONFIG_PATH}/${libSP_VERSION}")
 
-	MESSAGE("SP Path ${libSP_INCLUDE_DIR}")
+    if(EXISTS ${libSP_DIR})
 
-elseif(${SP_TYPE} MATCHES "20171204")
+        set(libSP_FOUND TRUE CACHE BOOL "" FORCE)
 
-	set(libSP_ROOT "${libSP_CONFIG_PATH}/${SP_TYPE}")
-	set(libSP_INCLUDE_DIR "${libSP_ROOT}/include")
-	set(libSP_LIB_DIR "${libSP_ROOT}/lib/${CMAKE_VS_PLATFORM_NAME}")
-	set(libSP_BIN_DIR "${libSP_ROOT}/bin/${CMAKE_VS_PLATFORM_NAME}")
-	set(libSP_LIB "DenseReconstruction")
-	set(libSP_FOUND TRUE CACHE BOOL "" FORCE)
+        if(${libSP_VERSION} MATCHES "trunk" OR ${libSP_VERSION} MATCHES "gold2")
+            set(libSP_LIB "SP_Core")
+        else()
+            set(libSP_LIB "DenseReconstruction")
+        endif()
 
-	MESSAGE("SP Path ${libSP_INCLUDE_DIR}")
+        add_library(libSP SHARED IMPORTED)
+        set_target_properties(libSP PROPERTIES 
+            INCLUDE_DIRECTORIES           ${libSP_DIR}/include
+            INTERFACE_INCLUDE_DIRECTORIES ${libSP_DIR}/include
+            IMPORTED_LOCATION             ${libSP_DIR}/bin/${CMAKE_VS_PLATFORM_NAME}/${libSP_LIB}.dll
+            IMPORTED_IMPLIB               ${libSP_DIR}/lib/${CMAKE_VS_PLATFORM_NAME}/${libSP_LIB}.lib
+        ) 
+        
+        MESSAGE(STATUS "SP Path ${libSP_DIR}/include")
 
-else()
-	set(libSP_FOUND FALSE CACHE BOOL "" FORCE)
-endif()
+    else()
+        set(libSP_FOUND FALSE CACHE BOOL "" FORCE)
+        MESSAGE(STATUS "SP Path not found")
+    endif()
+    
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 	MESSAGE("Darwin SP not supported yet")
 	set(libSP_FOUND FALSE CACHE BOOL "" FORCE)
@@ -36,4 +39,3 @@ elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 	MESSAGE("Linux SP not supported yet")
 endif()
 
-MESSAGE( STATUS "libSP library name: " ${libSP_LIB} )

b111505882a74ce7bf932aa36aca67db323962b1
Brian Fulkerson <brian.fulkerson@intel.com>
2018-01-26 10:16:37 +0000

    Add stripcontrollers.py

---
+++ corvis/scripts/stripcontrollers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from packet import Packet, PacketType
+import sys
+
+if len(sys.argv) < 3:
+  print "Usage:", sys.argv[0], "<capture file> <output file>"
+  sys.exit(1)
+
+filename = sys.argv[1]
+outfilename = sys.argv[2]
+
+f = open(filename, "rb")
+fout = open(outfilename, "wb")
+
+
+p = Packet.from_file(f)
+while p is not None:
+    skip = (p.header.type == PacketType.image_raw and p.header.sensor_id > 1) or \
+           (p.header.type == PacketType.accelerometer and p.header.sensor_id > 0) or \
+           (p.header.type == PacketType.gyroscope and p.header.sensor_id > 0)
+    if not skip:
+        p.to_file(fout)
+    p = Packet.from_file(f)
+
+f.close()
+fout.close()

f557516b4600a91317f49759bd5925237e7347f6
Brian Fulkerson <brian.fulkerson@intel.com>
2018-01-30 13:33:04 +0000

    benchmark: better recall edges

--- corvis/src/measure/benchmark.cpp
+++ corvis/src/measure/benchmark.cpp
@@ -229,7 +229,7 @@ void benchmark_run(std::ostream &stream, const char *directory, int threads,
     std::vector<double> rpe_T_edges = {0, 0.02, 0.04, 0.06, 0.1, 0.15};
     std::vector<double> rpe_R_edges = {0, 0.1, 0.25, 0.5, 1, 2};
     std::vector<double> precision_edges = {0, 60, 80, 95, 99, 100};
-    std::vector<double> recall_edges = {0, 0.05, 0.1, 0.2, 1, 5};
+    std::vector<double> recall_edges = {0, 1, 5, 10, 50, 100};
     std::vector<double> reloc_rpe_T_edges = {0, 0.02, 0.04, 0.06, 0.1, 0.15};
     std::vector<double> reloc_rpe_R_edges = {0, 0.25, 0.5, 1, 2, 5};
     std::vector<double> reloc_times_edges = {0, 1, 2, 3, 5, 10};

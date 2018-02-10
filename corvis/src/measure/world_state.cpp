046a06ffbd265b3b4dd03ea88752b7d75fd727ae
Jim Radford <jim.radford@intel.com>
2018-02-09 20:57:19 -0800

    world_state: observe the actual stage name

--- corvis/src/measure/world_state.cpp
+++ corvis/src/measure/world_state.cpp
@@ -596,9 +596,9 @@ void world_state::rc_data_callback(rc_Tracker * tracker, const rc_Data * data)
     observe_position(timestamp_us, (float)G.T[0], (float)G.T[1], (float)G.T[2], (float)G.Q.w(), (float)G.Q.x(), (float)G.Q.y(), (float)G.Q.z(), data->path == rc_DATA_PATH_FAST);
     update_sensors(tracker, data);
 
-    rc_Pose stage_pose;
-    if(rc_getStaticNode(tracker, nullptr, &stage_pose))
-        observe_virtual_object(0, "", stage_pose);
+    rc_Pose stage_pose; const char *stage_name = nullptr;
+    if(rc_getStaticNode(tracker, &stage_name, &stage_pose))
+        observe_virtual_object(0, stage_name ?: "(null)", stage_pose);
 
     if(data->path == rc_DATA_PATH_FAST) return;
 

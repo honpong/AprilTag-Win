1d9ac5cd7fbb703a3c138720c83cbf0da080e0e9
Brian Fulkerson <brian.fulkerson@intel.com>
2018-03-20 09:49:18 -0400

    extractcalibration.py: Take a second argument for the output json

--- corvis/scripts/extractcalibration.py
+++ corvis/scripts/extractcalibration.py
@@ -2,18 +2,20 @@
 from packet import Packet, PacketType
 import sys
 
-if len(sys.argv) < 2:
-    print "Usage:", sys.argv[0], "<capture file>"
+if len(sys.argv) != 3:
+    print "Usage:", sys.argv[0], "<capture file> <output json file>"
     sys.exit(1)
 
 filename = sys.argv[1]
+output_filename = sys.argv[2]
 
 f = open(filename, "rb")
 
 p = Packet.from_file(f)
 while p is not None:
     if p.header.type == PacketType.calibration_json:
-        print p.data
+        with open(output_filename, "w") as fout:
+            fout.write(p.data)
         f.close()
         sys.exit(0)
 

09e4fa826a378b7945784d13a8bb910f7d4d6999
Jim Radford <jim.radford@intel.com>
2018-06-05 22:00:33 -0700

    extractcalibration: support calibration_bin packets

--- scripts/extractcalibration.py
+++ scripts/extractcalibration.py
@@ -2,20 +2,33 @@
 from packet import Packet, PacketType
 import sys
 
-if len(sys.argv) != 3:
-    print "Usage:", sys.argv[0], "<capture file> <output json file>"
+if len(sys.argv) not in (2,3):
+    print "Usage:", sys.argv[0], "<capture file> [<output-file.(json|bin)>]"
     sys.exit(1)
 
 filename = sys.argv[1]
-output_filename = sys.argv[2]
+output_filename = sys.argv[2] if len(sys.argv) > 2 else None
 
 f = open(filename, "rb")
 
 p = Packet.from_file(f)
 while p is not None:
     if p.header.type == PacketType.calibration_json:
+        if output_filename:
+            assert(re.match(r'.*\.json',output_filename))
+        else:
+            output_filename = filename + ".json"
         with open(output_filename, "w") as fout:
             fout.write(p.data)
+        f.close()
+        sys.exit(0)
+    elif p.header.type == PacketType.calibration_bin:
+        if output_filename:
+            assert(re.match(r'.*\.bin',output_filename))
+        else:
+            output_filename = filename + ".bin"
+        with open(output_filename, "w") as bout:
+            bout.write(p.data)
         f.close()
         sys.exit(0)
 

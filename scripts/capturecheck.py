da402438a817b6b7b86f043f8c25edbfcf13f307
Mirota, Daniel <daniel.mirota@intel.com>
2018-08-06 12:32:17 -0700

    Scripts: Added python 3.6 support to capturecheck (#998)
    preserving python 2.7 support

--- scripts/capturecheck.py
+++ scripts/capturecheck.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 from struct import *
 from collections import defaultdict
 import numpy
@@ -30,7 +31,7 @@ parser.add_argument("capture_filename")
 
 args = parser.parse_args()
 if args.max_packets > 0:
-    print "Inspecting the first ", args.max_packets, " packets"
+    print("Inspecting the first ", args.max_packets, " packets")
 
 accel_type = 20
 gyro_type = 21
@@ -66,24 +67,24 @@ last_ptype = None
 packet_count = 0;
 bytes_read = 0
 total_bytes_read = 0
-while header_str != "":
+while len(header_str) > 0:
   (pbytes, ptype, sensor_id, ptime) = unpack('IHHQ', header_str)
   if pbytes < header_size:
-    print "Error: packet at", total_bytes_read, "bytes, (time,type,id) (", ptime, ",", ptype, ",", sensor_id,") has size ", pbytes, "bytes but minimum packet size is", header_size
+    print("Error: packet at", total_bytes_read, "bytes, (time,type,id) (", ptime, ",", ptype, ",", sensor_id,") has size ", pbytes, "bytes but minimum packet size is", header_size)
     sys.exit(1)
   got_types[ptype] += 1
   packet_str = packet_types[ptype] + "_" + str(sensor_id)
   if ptype == 1:
     ptime += 16667
   if args.verbose:
-      print packet_str, pbytes, ptype, sensor_id, ptime,
+      print(packet_str, pbytes, ptype, sensor_id, ptime),
   data = f.read(pbytes-header_size)
   packet_count += 1
   bytes_read += pbytes;
   total_bytes_read += pbytes
   if bytes_read > 1024*1024*100:
     bytes_read -= 1024*1024*100
-    if args.progress: 
+    if args.progress:
         sys.stdout.write('.')
         sys.stdout.flush()
 
@@ -91,11 +92,11 @@ while header_str != "":
       break
 
   if ptype == arrival_time_type and last_ptype == arrival_time_type:
-      if args.verbose: print "Warning: Unconsumed arrival time"
+      if args.verbose: print("Warning: Unconsumed arrival time")
       unconsumed_arrival_time_warnings.append((ptime))
   if last_ptype is not None and ptype != arrival_time_type and last_ptype != arrival_time_type:
       missing_arrival_time_warnings.append((ptime))
-      if args.verbose: print "warning: missing data packet arrival_time"
+      if args.verbose: print("warning: missing data packet arrival_time")
   last_ptype = ptype
 
   if ptype == accel_type or ptype == gyro_type:
@@ -103,7 +104,7 @@ while header_str != "":
       (x, y, z) = unpack('fff', data[:12])
       current_data = numpy.array([x, y, z])
       if args.verbose:
-          print "\t", sensor_id, x, y, z, sqrt(x*x + y*y + z*z)
+          print("\t", sensor_id, x, y, z, sqrt(x*x + y*y + z*z))
       if packet_str in last_data:
           norm = numpy.linalg.norm(last_data[packet_str] - current_data)
           if ptype == gyro_type and norm > 6:
@@ -119,7 +120,7 @@ while header_str != "":
       ptime += exposure/2
       if args.verbose:
           camera_str = "%d %dx%d grey, %dx%d depth, %d exposure, %d adjusted time" % (sensor_id, width, height, depth_w, depth_h, exposure, ptime)
-          print "\t", camera_str
+          print("\t", camera_str)
 
   elif ptype == image_raw_type or ptype == stereo_raw_type:
       if ptype == image_raw_type:
@@ -133,37 +134,37 @@ while header_str != "":
       ptime += exposure/2
       if args.verbose:
           camera_str = "%s %d (%d) %dx%d, %d stride, %d exposure, %d adjusted time" % (type_str, sensor_id, camera_format, width, height, stride, exposure, ptime)
-          print "\t", camera_str
+          print("\t", camera_str)
 
   elif ptype == thermometer_type:
       if args.verbose:
           (temp_C) = unpack('f', data[:4])
-          print "\t%.2fC" % (temp_C)
+          print("\t%.2fC" % (temp_C))
 
   elif ptype == arrival_time_type:
       if last_arrival_time > ptime:
           out_of_order_warnings.append((ptime, last_arrival_time))
       last_arrival_time = ptime
       if args.verbose:
-          print "\t %d" % ptime
+          print("\t %d" % ptime)
 
   elif ptype == odometry_type:
       (x, y) = unpack('ff', data[:8])
       if args.verbose:
-          print "\t", x, y
+          print("\t", x, y)
 
   elif ptype == velocimeter_type:
       (x, y, z) = unpack('fff', data[:12])
       if args.verbose:
-          print "\t", sensor_id, x, y, z
+          print("\t", sensor_id, x, y, z)
 
   elif ptype == calibration_type:
       if args.verbose:
-          print "\t", data
+          print("\t", data)
 
   else:
       if args.verbose:
-          print ""
+          print("")
 
   if packet_str == "":
       packet_str = str(ptype)
@@ -181,7 +182,7 @@ while header_str != "":
 
 f.close()
 if args.progress:
-    print ''
+    print('')
 
 error_text = ""
 total_warnings = 0
@@ -191,7 +192,7 @@ def compress_warnings(warning_list):
     i = 0
     current_list = []
     while i < len(warning_list)-1:
-        print warning_list[i], i
+        print(warning_list[i], i)
         adjacent = warning_list[i][1] == warning_list[i+1][0]
         current_list.append(warning_list[i][0])
         if not adjacent:
@@ -211,9 +212,9 @@ for packet_type in sorted(packets.keys()):
   if packet_type == arrival_time_type: continue
   timestamps = numpy.array(packets[packet_type])
   platencies = numpy.array(latencies[packet_type])
-  print packet_type, len(packets[packet_type]), "packets"
+  print(packet_type, len(packets[packet_type]), "packets")
   if len(packets[packet_type]) == 1:
-      print ""
+      print("")
       continue
   deltas = timestamps[1:] - timestamps[:-1]
   if len(arrivals[packet_type]) > 0:
@@ -222,37 +223,37 @@ for packet_type in sorted(packets.keys()):
   median_delta = numpy.median(deltas)
   start_s[packet_type] = numpy.min(timestamps)/1e6
   end_s[packet_type] = numpy.max(timestamps)/1e6
-  print "\tRate:", numpy.float64(1)/((median_delta+1)/1e6), "Hz"
-  print "\tmedian dt (us):", median_delta
-  print "\tstd dt (us):", numpy.std(deltas)
-  print "\trelative latency (us): %.3f min, %.3f median, %.3f max, %.3f std" % (numpy.min(platencies), numpy.median(platencies), numpy.max(platencies), numpy.std(platencies))
-  print "\tstart (s) finish (s):", start_s[packet_type], end_s[packet_type]
-  print "\tlength (s):", end_s[packet_type] - start_s[packet_type]
+  print("\tRate:", numpy.float64(1)/((median_delta+1)/1e6), "Hz")
+  print("\tmedian dt (us):", median_delta)
+  print("\tstd dt (us):", numpy.std(deltas))
+  print("\trelative latency (us): %.3f min, %.3f median, %.3f max, %.3f std" % (numpy.min(platencies), numpy.median(platencies), numpy.max(platencies), numpy.std(platencies)))
+  print("\tstart (s) finish (s):", start_s[packet_type], end_s[packet_type])
+  print("\tlength (s):", end_s[packet_type] - start_s[packet_type])
   if len(arrivals[packet_type]) > 0:
-    print "\tarrival (us): min: %.0f median: %.0f max: %.0f std: %.0f" % (numpy.min(arrivals_deltas), numpy.median(arrivals_deltas), numpy.max(arrivals_deltas), numpy.std(arrivals_deltas))
+    print("\tarrival (us): min: %.0f median: %.0f max: %.0f std: %.0f" % (numpy.min(arrivals_deltas), numpy.median(arrivals_deltas), numpy.max(arrivals_deltas), numpy.std(arrivals_deltas)))
   else:
-    print "\tNo arrival time data"
+    print("\tNo arrival time data")
 
   duplicate_timestamps = numpy.flatnonzero(deltas == 0)
   if len(duplicate_timestamps):
       error_text += "%d duplicate timestamps for %s\n" % (len(duplicate_timestamps), packet_type)
 
   if packet_type.startswith("thermometer"):
-      print "Skipping packet frequency analysis for this sensor\n"
+      print("Skipping packet frequency analysis for this sensor\n")
       continue
 
   exceptions = numpy.flatnonzero(numpy.logical_or(deltas > median_delta*1.05, deltas < median_delta*0.95))
   latency_warnings = numpy.flatnonzero(platencies > 33333)
 
-  print len(exceptions), "samples are more than 5% from median"
+  print(len(exceptions), "samples are more than 5% from median")
   if len(latency_warnings):
-      print len(latency_warnings), "packet latency warnings"
+      print(len(latency_warnings), "packet latency warnings")
   if len(warnings[packet_type]):
-      print len(warnings[packet_type]), "latency warnings"
+      print(len(warnings[packet_type]), "latency warnings")
   if len(exposure_warnings[packet_type]):
-      print len(exposure_warnings[packet_type]), "exposure warnings"
+      print(len(exposure_warnings[packet_type]), "exposure warnings")
   if len(imu_warnings[packet_type]):
-      print len(imu_warnings[packet_type]), "IMU warnings"
+      print(len(imu_warnings[packet_type]), "IMU warnings")
 
   total_warnings += len(exceptions) + len(latency_warnings) + len(warnings[packet_type]) + len(exposure_warnings[packet_type]) + len(imu_warnings[packet_type])
 
@@ -262,28 +263,28 @@ for packet_type in sorted(packets.keys()):
           break;
   for l in latency_warnings:
       if args.latency_warnings or platencies[l] > 50000:
-          print "High latency:", platencies[l], "us at", timestamps[l]
+          print("High latency:", platencies[l], "us at", timestamps[l])
   for e in exceptions:
       if deltas[e] > median_delta * 2:
           error_text += "Missing " + packet_type + " data (deltas too large)\n"
           break
   for e in exceptions:
       if args.exceptions or deltas[e] > median_delta * 2:
-          print "Exception: t t+1 delta", timestamps[e], timestamps[e+1], deltas[e]
+          print("Exception: t t+1 delta", timestamps[e], timestamps[e+1], deltas[e])
   if args.warnings:
       compressed_warnings = compress_warnings(warnings[packet_type])
       for w in compressed_warnings:
-          print "Warning: ", len(w), "images at timestamps:", w
+          print("Warning: ", len(w), "images at timestamps:", w)
   if len(exposure_warnings[packet_type]):
       error_text += "Error: Exposure problems with " + packet_type + "\n"
   if args.exposure_warnings:
       for w in exposure_warnings[packet_type]:
-          print "Warning: Image at", w[0], "had exposure of", w[1], "microseconds"
+          print("Warning: Image at", w[0], "had exposure of", w[1], "microseconds")
   if args.imu_warnings:
       numpy.set_printoptions(precision=2)
       for w in imu_warnings[packet_type]:
-          print "Warning:", packet_type, "at", w[0], "changed by ", w[1], "current: ", w[2], "last:", w[3]
-  print ""
+          print("Warning:", packet_type, "at", w[0], "changed by ", w[1], "current: ", w[2], "last:", w[3])
+  print("")
 
 start_times_s = numpy.array([start_s[key] for key in start_s])
 if numpy.max(start_times_s) - numpy.min(start_times_s) > 5:
@@ -294,7 +295,7 @@ if numpy.max(end_times_s) - numpy.min(end_times_s) > 5:
     error_text += "Error: Sensor end times differed by more than 5 seconds\n"
 
 total_time_s = numpy.max(end_times_s) - numpy.min(start_times_s)
-print "Total capture time: %.2fs" % total_time_s
+print("Total capture time: %.2fs" % total_time_s)
 
 for sensor_id in [1,2]:
     required_packets = [packet_types[p] + "_" + str(sensor_id) for p in [gyro_type, accel_type, controller_physical_info_type]]
@@ -309,15 +310,15 @@ for sensor_id in [1,2]:
         optional_packets = [packet_types[thermometer_type] + "_" + str(sensor_id)]
         missing = [c for c in optional_packets if c not in packets]
         for m in missing:
-            print "Warning: Controller %d is missing %s\n" % (sensor_id, m)
+            print("Warning: Controller %d is missing %s\n" % (sensor_id, m))
 
 if got_types[calibration_type] == 0 and got_types[calibration_bin_type] == 0:
-    print "Warning: Never received calibration packet"
+    print("Warning: Never received calibration packet")
     total_warnings += 1
 elif got_types[calibration_type] + got_types[calibration_bin_type] > 1:
     error_text += "Too many calibration packets\n"
 if got_types[arrival_time_type] == 0:
-    print "Warning: Never received arrival_time packet"
+    print("Warning: Never received arrival_time packet")
     total_warnings += 1
 else:
     if len(out_of_order_warnings) > 0 :
@@ -328,7 +329,7 @@ else:
         error_text += "Error: %d unconsumed arrival-time packets\n" % len(unconsumed_arrival_time_warnings)
 
 if got_types[thermometer_type] == 0:
-    print "Warning: Never received any thermometer data\n"
+    print("Warning: Never received any thermometer data\n")
     total_warnings += 1
 
 if got_types[accel_type] == 0:
@@ -343,13 +344,13 @@ if got_types[image_raw_type] == 0 and \
 max_warnings = max(10*total_time_s/60, 10) # 10 warnings per minute
 exit_code=0
 if len(error_text) > 0:
-    print "Failing errors:\n" + error_text
-    print "capturecheck result: Failed!"
+    print("Failing errors:\n" + error_text)
+    print("capturecheck result: Failed!")
     exit_code = 1
 elif total_warnings > max_warnings:
-    print "capturecheck result: Failed, with", total_warnings, "warnings!"
+    print("capturecheck result: Failed, with", total_warnings, "warnings!")
     exit_code = 2
 else:
-    print "capturecheck result: Passed, with", total_warnings, "warnings"
+    print("capturecheck result: Passed, with", total_warnings, "warnings")
 sys.exit(exit_code)
 

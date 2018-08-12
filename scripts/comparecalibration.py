df254c47477723843bc000ca3b9da6c4efbee531
Jim Radford <jim.radford@intel.com>
2018-08-11 17:10:55 -0700

    comparecalibration: 2to3

--- scripts/comparecalibration.py
+++ scripts/comparecalibration.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+from __future__ import print_function
 from packet import Packet, PacketType
 import sys
 
 if len(sys.argv) != 3:
-    print "Usage:", sys.argv[0], "<capture file> <json file>"
+    print("Usage:", sys.argv[0], "<capture file> <json file>")
     sys.exit(1)
 
 filename = sys.argv[1]
@@ -15,14 +16,14 @@ p = Packet.from_file(f)
 while p is not None:
     if p.header.type == PacketType.calibration_json:
         if p.data == json_cal:
-            print "Calibrations are the same"
+            print("Calibrations are the same")
         else:
-            print "Calibrations are different"
+            print("Calibrations are different")
         sys.exit(0)
 
     p = Packet.from_file(f)
 
 f.close()
 
-print "Error: No packet_calibration_json found!"
+print("Error: No packet_calibration_json found!")
 sys.exit(1)

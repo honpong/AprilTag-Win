8778039bd1ca38a952fc1d8d7de3f63ee7164ebf
Mirota, Daniel <daniel.mirota@intel.com>
2018-08-30 12:47:44 +0100

    compare_poses: ate registration

--- src/compare_poses/compare_poses.cpp
+++ src/compare_poses/compare_poses.cpp
@@ -29,6 +29,23 @@ int main(int argc, char ** argv)
             cerr << "There was a problem calculating the ATE, exiting\n";
             return 1;
         }
+
+        //save the ground truth after ate registration
+        tpose_sequence gt_with_current_ate_sequence;
+        gt_with_current_ate_sequence.tposes = gt_sequence.tposes;
+
+        tpose ate_registration{ gt_sequence.tposes.at(0).t, transformation(result.errors.ate_s.R, result.errors.ate_s.T)*gt_sequence.tposes.at(0).G };
+
+
+        gt_with_current_ate_sequence.set_relative_pose(gt_sequence.tposes.at(0).t, ate_registration);
+
+        {
+            ofstream pose_st(std::string(argv[i]) + "_gt_ate.tum");
+            for (auto &pose : gt_with_current_ate_sequence.tposes) {
+                pose_st << tpose_tum(sensor_clock::tp_to_micros(pose.t) / 1e6, pose.G, 0, 3);
+            }
+        }
+
         if(!result.errors.calculate_rpe_600ms()) {
             cerr << "There was a problem calculating the RPE, exiting\n";
             return 1;

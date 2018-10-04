07c863f79455b2f17a06362fabf59710b85414a7
Chen, Chuan <chuan.chen@intel.com>
2018-10-03 21:33:04 -0700

    state_vision: use 1.01 instead of 1.1 when perturbing newly brought back group covariances (#1046)
    This leads to a decidedly non-random benchmark improvement.

--- src/tracker/state_vision.cpp
+++ src/tracker/state_vision.cpp
@@ -322,13 +322,13 @@ void state_vision::update_map(mapper *map, const std::vector<triangulated_track>
 
 void state_vision::project_new_group_covariance(const state_vision_group &g)
 {
-    cov.cov.map()(g.Tr.index + 0, g.Tr.index + 0) *= 1.1;
-    cov.cov.map()(g.Tr.index + 1, g.Tr.index + 1) *= 1.1;
-    cov.cov.map()(g.Tr.index + 2, g.Tr.index + 2) *= 1.1;
+    cov.cov.map()(g.Tr.index + 0, g.Tr.index + 0) *= 1.01;
+    cov.cov.map()(g.Tr.index + 1, g.Tr.index + 1) *= 1.01;
+    cov.cov.map()(g.Tr.index + 2, g.Tr.index + 2) *= 1.01;
 
-    cov.cov.map()(g.Qr.index + 0, g.Qr.index + 0) *= 1.1;
-    cov.cov.map()(g.Qr.index + 1, g.Qr.index + 1) *= 1.1;
-    cov.cov.map()(g.Qr.index + 2, g.Qr.index + 2) *= 1.1;
+    cov.cov.map()(g.Qr.index + 0, g.Qr.index + 0) *= 1.01;
+    cov.cov.map()(g.Qr.index + 1, g.Qr.index + 1) *= 1.01;
+    cov.cov.map()(g.Qr.index + 2, g.Qr.index + 2) *= 1.01;
 }
 
 state_vision_group * state_vision::add_group(const rc_Sensor camera_id, mapper *map)

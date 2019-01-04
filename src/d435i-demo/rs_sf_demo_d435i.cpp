da8a90810768f7086e3ce6c88eba602b9a426b67
Hon Pong (Gary) Ho <hon.pong.ho@intel.com>
2019-01-04 12:30:33 +0800

    no need for extra space

--- src/d435i-demo/rs_sf_demo_d435i.cpp
+++ src/d435i-demo/rs_sf_demo_d435i.cpp
@@ -383,7 +383,7 @@ struct d435i_exec_pipeline
                     _tracker->process(_src.data_vec_with_stereo(_tracker->require_laser_off()));
                     _app_hint = _tracker->prefix() + ": ";
                     switch (_tracker->wait_for_image_pose(images)) {
-                    case rs2::camera_imu_tracker::HIGH:   _app_hint += "High Confidence  "; break;
+                    case rs2::camera_imu_tracker::HIGH:   _app_hint += "High Confidence"; break;
                     case rs2::camera_imu_tracker::MEDIUM: _app_hint += "Medium Confidence"; break;
                     default:                              _app_hint  = "Move Around / Reset"; break;
                     }

bd6a842e8be0f45f288d39c74dcf5342efe85a3b
Hon Pong (Gary) Ho <hon.pong.ho@intel.com>
2019-03-19 16:44:54 +0800

    for future use of jpeg processing binary

--- src/t265-demo/t265-insight.bat
+++ src/t265-demo/t265-insight.bat
@@ -4,6 +4,9 @@ echo %1
 REM just do something here
 cd %1
 python translatev1.py
+REM call dist\translatev1\translatev1.exe
 mkdir output
 python csvrwv1.py
+REM call dist\csvrwv1\csvrwv1.exe
+MSG * "TAGGED JPEGS AT FOLDER %1"
 EXIT

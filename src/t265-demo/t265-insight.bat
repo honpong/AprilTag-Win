1ff9840977cac929c21fcc3ac09b5cc8581ccc91
Hon Pong (Gary) Ho <hon.pong.ho@intel.com>
2019-03-20 13:17:16 +0800

    dos script to handle path changes

--- src/t265-demo/t265-insight.bat
+++ src/t265-demo/t265-insight.bat
@@ -1,12 +1,24 @@
 @echo off
-REM %1 is the folder path of captured dataset
+:: %1 is the folder path of captured dataset
 echo %1
-REM just do something here
-cd %1
-python translatev1.py
-REM call dist\translatev1\translatev1.exe
+
+:: set default path
+if [%1]==[] (set SRC=".\capture") else (set SRC=%1) 
+
+:: call first script
+:: python translatev1.py
+call translatev1.exe %SRC%
+
+:: make output directory
+RMDIR /S /Q output
 mkdir output
-python csvrwv1.py
-REM call dist\csvrwv1\csvrwv1.exe
-MSG * "TAGGED JPEGS AT FOLDER %1"
-EXIT
+
+:: call second script
+:: python csvrwv1.py
+call csvrwv1.exe %SRC%
+
+:: remind output directory
+set DST=%cd%
+MSG * "SOURCE JPEGS FROM %SRC%, TAGGED JPEGS TO FOLDER %DST%\output"
+
+EXIT 0

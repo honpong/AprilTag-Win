a2c1c0391d40dc25040f56522eaa778102948ae9
Hon Pong (Gary) Ho <hon.pong.ho@intel.com>
2019-03-25 15:36:57 +0800

    win script fix

--- src/t265-demo/t265-insight.bat
+++ src/t265-demo/t265-insight.bat
@@ -6,17 +6,17 @@ ECHO %1
 if [%1]==[] (SET SRC=".\capture") else (SET SRC=%1) 
 if [%2]==[] (SET PY=0) else (SET PY=1)
 
-if PY==0 (PRINT "Use executables.") else (PRINT "Use python scripts in py\.")
+if %PY%==0 (PRINT "Use executables.") else (PRINT "Use python scripts in py\.")
 
 :: call first script
-if PY==0 (CALL translatev1.exe %SRC%) else (python py\translatev1.py %SRC%)
+if %PY%==0 (CALL translatev1.exe %SRC%) else (python py\translatev1.py %SRC%)
 
 :: make output directory
 RMDIR /S /Q output 2> NUL
 MKDIR output
 
 :: call second script
-if PY==0 (CALL csvrwv1.exe %SRC%) else (python py\csvrwv1.py %SRC%)
+if %PY%==0 (CALL csvrwv1.exe %SRC%) else (python py\csvrwv1.py %SRC%)
 
 DEL /F /Q outputllh.csv
 

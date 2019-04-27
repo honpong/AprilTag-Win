e94eefc4a40e00214a9413063df4488428fa71e3
Hon Pong (Gary) Ho <hon.pong.ho@intel.com>
2019-04-27 23:02:32 +0800

    add capture abstraction interface

--- src/t265-demo/rs_sf_demo_t265.hpp
+++ src/t265-demo/rs_sf_demo_t265.hpp
@@ -26,4 +26,13 @@
 
 #include "json/json.h"
 
+struct rgb_cam
+{
+    virtual bool isOpened() { return false; }
+    virtual bool get_image(cv::Mat& dst) = 0;
+    virtual std::string name() { return "cam id unknown"; }
+    virtual ~rgb_cam() {}
+    static std::unique_ptr<rgb_cam> create();
+};
+
 #endif /* rs_sf_demo_t265_h */
